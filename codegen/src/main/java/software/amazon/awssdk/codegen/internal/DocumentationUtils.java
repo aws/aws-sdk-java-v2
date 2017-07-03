d5050c23dfa9a1c8447c047a4ec5c4169d99fa5f
Matthew Miller <millem@amazon.com>
2017-07-03 15:16:46 -0700

    Addressed initial comments.

--- codegen/src/main/java/software/amazon/awssdk/codegen/internal/DocumentationUtils.java
+++ codegen/src/main/java/software/amazon/awssdk/codegen/internal/DocumentationUtils.java
@@ -44,10 +44,6 @@ public class DocumentationUtils {
     public static final String DEFAULT_FLUENT_RETURN =
             "Returns a reference to this object so that method calls can be chained together.";
 
-    public static final String CONSTRUCTOR_DOC =
-            "Constructs a new %s object. Callers should use the setter or fluent setter (with...) methods to initialize any " +
-            "additional object members.";
-
     //TODO kylthoms@: probably should move this to a custom config in each service
     private static final Set<String> SERVICES_EXCLUDED_FROM_CROSS_LINKING = new HashSet<>(Arrays.asList(
             "apigateway", "budgets", "cloudsearch", "cloudsearchdomain",
