4143bd75bb8d33a6c38bddc09eab7fa4cffaa4b2
Kyle Thomson <kylthoms@amazon.com>
2017-10-20 17:03:45 -0700

    Temporarily disabling epoll so that we can get a build out. Need to understand why this is causing a problem.

--- http-clients/netty-nio-client/src/main/java/software/amazon/awssdk/http/nio/netty/DefaultEventLoopGroupFactory.java
+++ http-clients/netty-nio-client/src/main/java/software/amazon/awssdk/http/nio/netty/DefaultEventLoopGroupFactory.java
@@ -18,7 +18,6 @@ package software.amazon.awssdk.http.nio.netty;
 import static software.amazon.awssdk.utils.StringUtils.isBlank;
 
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.epoll.Epoll;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import java.util.Optional;
@@ -60,11 +59,16 @@ public final class DefaultEventLoopGroupFactory
     @Override
     public EventLoopGroup create() {
         int numThreads = numberOfThreads == null ? 0 : numberOfThreads;
+        return new NioEventLoopGroup(numThreads, resolveThreadFactory());
+        /*
+        Need to investigate why epoll is raising channel inactive after succesful response that causes
+        problems with retries.
+
         if (Epoll.isAvailable() && isNotAwsLambda()) {
             return new EpollEventLoopGroup(numThreads, resolveThreadFactory());
         } else {
-            return new NioEventLoopGroup(numThreads, resolveThreadFactory());
-        }
+
+        }*/
     }
 
     @ReviewBeforeRelease("We should work with Lambda to get Epoll opened up")
