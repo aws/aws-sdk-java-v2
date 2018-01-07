10ccc93ca806abcb983195ed1a03df3702554551
Kyle Thomson <kylthoms@amazon.com>
2018-01-06 21:03:43 -0800

    Make codegen.config an optional configuration.

--- codegen-maven-plugin/src/main/java/software/amazon/awssdk/codegen/maven/plugin/GenerationMojo.java
+++ codegen-maven-plugin/src/main/java/software/amazon/awssdk/codegen/maven/plugin/GenerationMojo.java
@@ -73,7 +73,7 @@ public class GenerationMojo extends AbstractMojo {
             try {
                 getLog().info("Loading from: " + p.toString());
                 generateCode(C2jModels.builder()
-                                      .codeGenConfig(loadCodeGenConfig(p))
+                                      .apply(b -> loadCodeGenConfig(p).ifPresent(b::codeGenConfig))
                                       .customizationConfig(loadCustomizationConfig(p))
                                       .serviceModel(loadServiceModel(p))
                                       .waitersModel(loadWaiterModel(p))
@@ -116,8 +116,8 @@ public class GenerationMojo extends AbstractMojo {
                      .execute();
     }
 
-    private BasicCodeGenConfig loadCodeGenConfig(Path root) throws MojoExecutionException {
-        return loadRequiredModel(BasicCodeGenConfig.class, root.resolve(CODE_GEN_CONFIG_FILE));
+    private Optional<BasicCodeGenConfig> loadCodeGenConfig(Path root) {
+        return loadOptionalModel(BasicCodeGenConfig.class, root.resolve(CODE_GEN_CONFIG_FILE));
     }
 
     private CustomizationConfig loadCustomizationConfig(Path root) {
