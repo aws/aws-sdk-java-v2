92a31f3e150e9399a00367b800d2493866950a98
shorea <shorea@amazon.com>
2018-04-09 19:34:17 -0700

    Syncing retry conditions with V1 and a bit of cleanup, reduce duplication

--- core/src/main/java/software/amazon/awssdk/core/retry/conditions/RetryCondition.java
+++ core/src/main/java/software/amazon/awssdk/core/retry/conditions/RetryCondition.java
@@ -17,16 +17,19 @@ package software.amazon.awssdk.core.retry.conditions;
 
 import software.amazon.awssdk.annotations.SdkPublicApi;
 import software.amazon.awssdk.core.retry.RetryPolicyContext;
+import software.amazon.awssdk.core.retry.RetryUtils;
 import software.amazon.awssdk.core.retry.SdkDefaultRetrySettings;
 
 @SdkPublicApi
 @FunctionalInterface
 public interface RetryCondition {
 
-    RetryCondition DEFAULT = new OrRetryCondition(new RetryOnStatusCodeCondition(SdkDefaultRetrySettings.RETRYABLE_STATUS_CODES),
-                                                  new RetryOnExceptionsCondition(SdkDefaultRetrySettings.RETRYABLE_EXCEPTIONS),
-                                                  new RetryOnErrorCodeCondition(SdkDefaultRetrySettings.RETRYABLE_ERROR_CODES));
-
+    RetryCondition DEFAULT = new OrRetryCondition(
+        new RetryOnErrorCodeCondition(SdkDefaultRetrySettings.RETRYABLE_ERROR_CODES),
+        new RetryOnStatusCodeCondition(SdkDefaultRetrySettings.RETRYABLE_STATUS_CODES),
+        new RetryOnExceptionsCondition(SdkDefaultRetrySettings.RETRYABLE_EXCEPTIONS),
+        c -> RetryUtils.isThrottlingException(c.exception()),
+        c -> RetryUtils.isClockSkewError(c.exception()));
     RetryCondition NONE = new MaxNumberOfRetriesCondition(0);
 
     default OrRetryCondition or(RetryCondition other) {
