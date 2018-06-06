713e3a5d806011d9951fa68f46f90703e2245a49
Zoe Wang <zoewang@amazon.com>
2018-06-06 11:42:26 -0700

    Split service packages to differnt modules for kinesis, route53, iot, cloudsearch

--- services/iotdata/src/it/java/software/amazon/awssdk/services/iotdata/ServiceIntegrationTest.java
+++ services/iotdata/src/it/java/software/amazon/awssdk/services/iotdata/ServiceIntegrationTest.java
@@ -13,7 +13,7 @@
  * permissions and limitations under the License.
  */
 
-package software.amazon.awssdk.services.iot;
+package software.amazon.awssdk.services.iotdata;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertEquals;
