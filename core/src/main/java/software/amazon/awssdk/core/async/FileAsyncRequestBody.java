aws/aws-sdk-java-v2

commit 12aaaae874f158e03255c5304b813cd8234c638d

Add TCK tests for reactive streams impls
Fixes #405
Fixes #406

core/src/main/java/software/amazon/awssdk/core/async/FileAsyncRequestBody.java
@@ -16,17 +16,16 @@
 package software.amazon.awssdk.core.async;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.CompletionHandler;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.atomic.AtomicLong;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import software.amazon.awssdk.annotations.SdkInternalApi;
+import software.amazon.awssdk.core.util.async.NoopSubscription;
 import software.amazon.awssdk.utils.builder.SdkBuilder;
 
 /**
@@ -43,7 +42,7 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
     /**
      * File to read.
      */
-    private final Path file;
+    private final Path path;
 
     /**
      * Size (in bytes) of ByteBuffer chunks read from the file and delivered to the subscriber.
@@ -52,18 +51,33 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
 
 
     private FileAsyncRequestBody(DefaultBuilder builder) {
-        this.file = builder.path;
+        this.path = builder.path;
         this.chunkSizeInBytes = builder.chunkSizeInBytes == null ? DEFAULT_CHUNK_SIZE : builder.chunkSizeInBytes;
     }
 
     @Override
     public long contentLength() {
-        return file.toFile().length();
+        return path.toFile().length();
     }
 
     @Override
     public void subscribe(Subscriber<? super ByteBuffer> s) {
-        s.onSubscribe(new FileSubscription(file, s, chunkSizeInBytes));
+        try {
+            AsynchronousFileChannel channel = openInputChannel(this.path);
+
+            // We need to synchronize here because the subscriber could call
+            // request() from within onSubscribe which would potentially
+            // trigger onNext before onSubscribe is finished.
+            Subscription subscription = new FileSubscription(channel, s, chunkSizeInBytes);
+            synchronized (subscription) {
+                s.onSubscribe(subscription);
+            }
+        } catch (IOException e) {
+            // subscribe() must return normally, so we need to signal the
+            // failure to open via onError() once onSubscribe() is signaled.
+            s.onSubscribe(new NoopSubscription(s));
+            s.onError(e);
+        }
     }
 
     /**
@@ -135,7 +149,6 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
      * Reads the file for one subscriber.
      */
     private static class FileSubscription implements Subscription {
-
         private final AsynchronousFileChannel inputChannel;
         private final Subscriber<? super ByteBuffer> subscriber;
         private final int chunkSize;
@@ -143,29 +156,36 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
         private long position = 0;
         private AtomicLong outstandingDemand = new AtomicLong(0);
         private boolean writeInProgress = false;
+        private volatile boolean done = false;
 
-        private FileSubscription(Path file, Subscriber<? super ByteBuffer> subscriber, int chunkSize) {
-            this.inputChannel = openInputChannel(file);
+        private FileSubscription(AsynchronousFileChannel inputChannel, Subscriber<? super ByteBuffer> subscriber, int chunkSize) {
+            this.inputChannel = inputChannel;
             this.subscriber = subscriber;
             this.chunkSize = chunkSize;
         }
 
         @Override
         public void request(long n) {
+            if (done) {
+                return;
+            }
+
             if (n < 1) {
                 IllegalArgumentException ex =
-                    new IllegalArgumentException(subscriber + " violated the Reactive Streams rule 3.9 by requesting a non-positive number of elements.");
-                subscriber.onError(ex);
-            } else
+                    new IllegalArgumentException(subscriber + " violated the Reactive Streams rule 3.9 by requesting a "
+                            + "non-positive number of elements.");
+                signalOnError(ex);
+            } else {
                 try {
-                    long initialDemand = outstandingDemand.get();
-                    long newDemand = initialDemand + n;
-                    if (newDemand < 1) {
-                        // As governed by rule 3.17, when demand overflows `Long.MAX_VALUE` we treat the signalled demand as "effectively unbounded"
-                        outstandingDemand.set(Long.MAX_VALUE);
-                    } else {
-                        outstandingDemand.set(newDemand);
-                    }
+                    // As governed by rule 3.17, when demand overflows `Long.MAX_VALUE` we treat the signalled demand as
+                    // "effectively unbounded"
+                    outstandingDemand.getAndUpdate(initialDemand -> {
+                        if (Long.MAX_VALUE - initialDemand < n) {
+                            return Long.MAX_VALUE;
+                        } else {
+                            return initialDemand + n;
+                        }
+                    });
 
                     synchronized (this) {
                         if (!writeInProgress) {
@@ -174,13 +194,19 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
                         }
                     }
                 } catch (Exception e) {
-                    subscriber.onError(e);
+                    signalOnError(e);
                 }
+            }
         }
 
         @Override
         public void cancel() {
-            closeFile();
+            synchronized (this) {
+                if (!done) {
+                    done = true;
+                    closeFile();
+                }
+            }
         }
 
         private void readData() {
@@ -188,6 +214,7 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
             if (!inputChannel.isOpen()) {
                 return;
             }
+
             final ByteBuffer buffer = ByteBuffer.allocate(chunkSize);
             inputChannel.read(buffer, position, buffer, new CompletionHandler<Integer, ByteBuffer>() {
                 @Override
@@ -195,7 +222,7 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
                     if (result > 0) {
                         attachment.flip();
                         position += attachment.remaining();
-                        subscriber.onNext(attachment);
+                        signalOnNext(attachment);
                         // If we have more permits, queue up another read.
                         if (outstandingDemand.decrementAndGet() > 0) {
                             readData();
@@ -203,18 +230,18 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
                         }
                     } else {
                         // Reached the end of the file, notify the subscriber and cleanup
-                        subscriber.onComplete();
+                        signalOnComplete();
                         closeFile();
                     }
 
-                    synchronized (FileSubscription.this) {
+                    synchronized (this) {
                         writeInProgress = false;
                     }
                 }
 
                 @Override
                 public void failed(Throwable exc, ByteBuffer attachment) {
-                    subscriber.onError(exc);
+                    signalOnError(exc);
                     closeFile();
                 }
             });
@@ -224,20 +251,38 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
             try {
                 inputChannel.close();
             } catch (IOException e) {
-                throw new UncheckedIOException(e);
+                signalOnError(e);
             }
         }
 
-    }
+        private void signalOnNext(ByteBuffer bb) {
+            synchronized (this) {
+                if (!done) {
+                    subscriber.onNext(bb);
+                }
+            }
+        }
 
-    private static AsynchronousFileChannel openInputChannel(Path path) {
-        try {
-            if (!Files.exists(path)) Files.createFile(path);
-            return AsynchronousFileChannel.open(path, StandardOpenOption.READ);
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+        private void signalOnComplete() {
+            synchronized (this) {
+                if (!done) {
+                    subscriber.onComplete();
+                    done = true;
+                }
+            }
+        }
+
+        private void signalOnError(Throwable t) {
+            synchronized (this) {
+                if (!done) {
+                    subscriber.onError(t);
+                    done = true;
+                }
+            }
         }
     }
 
-
+    private static AsynchronousFileChannel openInputChannel(Path path) throws IOException {
+        return AsynchronousFileChannel.open(path, StandardOpenOption.READ);
+    }
 }

core/pom.xml
@@ -118,7 +118,6 @@
         <dependency>
             <groupId>org.reactivestreams</groupId>
             <artifactId>reactive-streams</artifactId>
-            <version>${reactive-streams.version}</version>
         </dependency>
 
         <dependency>
@@ -135,13 +134,11 @@
         <dependency>
             <groupId>org.reactivestreams</groupId>
             <artifactId>reactive-streams-tck</artifactId>
-            <version>${reactive-streams.version}</version>
             <scope>test</scope>
         </dependency>
         <dependency>
             <groupId>com.google.jimfs</groupId>
             <artifactId>jimfs</artifactId>
-            <version>1.1</version>
             <scope>test</scope>
         </dependency>
     </dependencies>

core/src/main/java/software/amazon/awssdk/core/async/AsyncResponseTransformer.java
@@ -21,6 +21,8 @@ import java.nio.file.Path;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscription;
 import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.internal.async.ByteArrayAsyncResponseTransformer;
+import software.amazon.awssdk.core.internal.async.FileAsyncResponseTransformer;
 
 /**
  * Callback interface to handle a streaming asynchronous response.

core/src/main/java/software/amazon/awssdk/core/async/ByteArrayAsyncRequestBody.java
@@ -19,6 +19,7 @@ import java.nio.ByteBuffer;
 
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+
 import software.amazon.awssdk.annotations.SdkInternalApi;
 
 /**
@@ -32,51 +33,54 @@ import software.amazon.awssdk.annotations.SdkInternalApi;
 @SdkInternalApi
 final class ByteArrayAsyncRequestBody implements AsyncRequestBody {
 
-  private final byte[] bytes;
+    private final byte[] bytes;
 
     ByteArrayAsyncRequestBody(byte[] bytes) {
         this.bytes = bytes.clone();
     }
 
-  @Override
-  public long contentLength() {
-    return bytes.length;
-  }
-
-  @Override
-  public void subscribe(Subscriber<? super ByteBuffer> s) {
-    // As per rule 2.13, we need to throw a `java.lang.NullPointerException` if the `Subscription` is `null`
-    if (s == null) throw new NullPointerException("Subscription MUST NOT be null.");
-
-    // As per 2.13, this method must return normally (i.e. not throw).
-    try {
-      s.onSubscribe(
-          new Subscription() {
-            boolean done = false;
-            @Override
-            public void request(long n) {
-              if (n > 0) {
-                if (!done) {
-                  s.onNext(ByteBuffer.wrap(bytes));
-                  done = true;
-                  s.onComplete();
-                }
-              } else {
-                s.onError(new IllegalArgumentException("§3.9: non-positive requests are not allowed!"));
-              }
-            }
-
-            @Override
-            public void cancel() {
-            }
-          }
-      );
-    } catch (Throwable ex) {
-      new IllegalStateException(s + " violated the Reactive Streams rule 2.13 " +
-          "by throwing an exception from onSubscribe.", ex)
-          // When onSubscribe fails this way, we don't know what state the
-          // s is thus calling onError may cause more crashes.
-          .printStackTrace();
+    @Override
+    public long contentLength() {
+        return bytes.length;
     }
-  }
+
+    @Override
+    public void subscribe(Subscriber<? super ByteBuffer> s) {
+        // As per rule 1.9 we must throw NullPointerException if the subscriber parameter is null
+        if (s == null) {
+            throw new NullPointerException("Subscription MUST NOT be null.");
+        }
+
+        // As per 2.13, this method must return normally (i.e. not throw).
+        try {
+            s.onSubscribe(
+                    new Subscription() {
+                        boolean done = false;
+
+                        @Override
+                        public void request(long n) {
+                            if (n > 0) {
+                                if (!done) {
+                                    s.onNext(ByteBuffer.wrap(bytes));
+                                    done = true;
+                                    s.onComplete();
+                                }
+                            } else {
+                                s.onError(new IllegalArgumentException("§3.9: non-positive requests are not allowed!"));
+                            }
+                        }
+
+                        @Override
+                        public void cancel() {
+                        }
+                    }
+            );
+        } catch (Throwable ex) {
+            new IllegalStateException(s + " violated the Reactive Streams rule 2.13 " +
+                    "by throwing an exception from onSubscribe.", ex)
+                    // When onSubscribe fails this way, we don't know what state the
+                    // s is thus calling onError may cause more crashes.
+                    .printStackTrace();
+        }
+    }
 }

core/src/main/java/software/amazon/awssdk/core/internal/async/ByteArrayAsyncResponseTransformer.java
@@ -13,7 +13,7 @@
  * permissions and limitations under the License.
  */
 
-package software.amazon.awssdk.core.async;
+package software.amazon.awssdk.core.internal.async;
 
 import static software.amazon.awssdk.utils.FunctionalUtils.invokeSafely;
 
@@ -24,6 +24,7 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import software.amazon.awssdk.annotations.SdkInternalApi;
 import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.async.AsyncResponseTransformer;
 import software.amazon.awssdk.utils.BinaryUtils;
 
 /**
@@ -36,7 +37,8 @@ import software.amazon.awssdk.utils.BinaryUtils;
  * @see AsyncResponseTransformer#toBytes()
  */
 @SdkInternalApi
-class ByteArrayAsyncResponseTransformer<ResponseT> implements AsyncResponseTransformer<ResponseT, ResponseBytes<ResponseT>> {
+public class ByteArrayAsyncResponseTransformer<ResponseT> implements
+        AsyncResponseTransformer<ResponseT, ResponseBytes<ResponseT>> {
 
     private ResponseT response;
     private ByteArrayOutputStream baos;
@@ -49,7 +51,7 @@ class ByteArrayAsyncResponseTransformer<ResponseT> implements AsyncResponseTrans
     @Override
     public void onStream(Publisher<ByteBuffer> publisher) {
         baos = new ByteArrayOutputStream();
-        publisher.subscribe(new BaosSubscriber());
+        publisher.subscribe(new BaosSubscriber(baos));
     }
 
     @Override
@@ -66,16 +68,21 @@ class ByteArrayAsyncResponseTransformer<ResponseT> implements AsyncResponseTrans
         }
     }
 
-    /**
-     * Requests chunks sequentially and dumps them into a {@link ByteArrayOutputStream}.
-     */
-    // TODO cover with Reactive Streams TCK, it's mostly ok, just a few edge cases / sanity checks should be covered AFAICS
-    private class BaosSubscriber implements Subscriber<ByteBuffer> {
+    static class BaosSubscriber implements Subscriber<ByteBuffer> {
+        private final ByteArrayOutputStream baos;
 
         private Subscription subscription;
 
+        BaosSubscriber(ByteArrayOutputStream baos) {
+            this.baos = baos;
+        }
+
         @Override
         public void onSubscribe(Subscription s) {
+            if (this.subscription != null) {
+                s.cancel();
+                return;
+            }
             this.subscription = s;
             subscription.request(1);
         }

core/src/main/java/software/amazon/awssdk/core/internal/async/FileAsyncResponseTransformer.java
@@ -13,7 +13,7 @@
  * permissions and limitations under the License.
  */
 
-package software.amazon.awssdk.core.async;
+package software.amazon.awssdk.core.internal.async;
 
 import static software.amazon.awssdk.utils.FunctionalUtils.invokeSafely;
 
@@ -29,6 +29,7 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import software.amazon.awssdk.annotations.SdkInternalApi;
+import software.amazon.awssdk.core.async.AsyncResponseTransformer;
 
 /**
  * {@link AsyncResponseTransformer} that writes the data to the specified file.
@@ -36,13 +37,13 @@ import software.amazon.awssdk.annotations.SdkInternalApi;
  * @param <ResponseT> Response POJO type. Returned on {@link #complete()}.
  */
 @SdkInternalApi
-final class FileAsyncResponseTransformer<ResponseT> implements AsyncResponseTransformer<ResponseT, ResponseT> {
+public final class FileAsyncResponseTransformer<ResponseT> implements AsyncResponseTransformer<ResponseT, ResponseT> {
 
     private final Path path;
     private AsynchronousFileChannel fileChannel;
     private volatile ResponseT response;
 
-    FileAsyncResponseTransformer(Path path) {
+    public FileAsyncResponseTransformer(Path path) {
         this.path = path;
     }
 
@@ -59,7 +60,7 @@ final class FileAsyncResponseTransformer<ResponseT> implements AsyncResponseTran
     public void onStream(Publisher<ByteBuffer> publisher) {
         // onStream may be called multiple times so reset the file channel every time
         this.fileChannel = invokeSafely(() -> createChannel(path));
-        publisher.subscribe(new FileSubscriber());
+        publisher.subscribe(new FileSubscriber(this.fileChannel, path));
     }
 
     @Override
@@ -82,16 +83,27 @@ final class FileAsyncResponseTransformer<ResponseT> implements AsyncResponseTran
     /**
      * {@link Subscriber} implementation that writes chunks to a file.
      */
-    // FIXME cover with Reactive Streams TCK, looks ok from a first brief look, but could be missing some edge cases
-    private class FileSubscriber implements Subscriber<ByteBuffer> {
+    static class FileSubscriber implements Subscriber<ByteBuffer> {
+        private final AtomicLong position = new AtomicLong();
+
+        private final AsynchronousFileChannel fileChannel;
+        private final Path path;
 
         private volatile boolean writeInProgress = false;
         private volatile boolean closeOnLastWrite = false;
-        private final AtomicLong position = new AtomicLong();
         private Subscription subscription;
+
+        FileSubscriber(AsynchronousFileChannel fileChannel, Path path) {
+            this.fileChannel = fileChannel;
+            this.path = path;
+        }
 
         @Override
         public void onSubscribe(Subscription s) {
+            if (this.subscription != null) {
+                s.cancel();
+                return;
+            }
             this.subscription = s;
             // Request the first chunk to start producing content
             s.request(1);
@@ -99,6 +111,10 @@ final class FileAsyncResponseTransformer<ResponseT> implements AsyncResponseTran
 
         @Override
         public void onNext(ByteBuffer byteBuffer) {
+            if (byteBuffer == null) {
+                throw new NullPointerException("Element must not be null");
+            }
+
             writeInProgress = true;
             fileChannel.write(byteBuffer, position.get(), byteBuffer, new CompletionHandler<Integer, ByteBuffer>() {
                 @Override

core/src/main/java/software/amazon/awssdk/core/internal/http/async/SimpleRequestProvider.java
@@ -18,6 +18,7 @@ package software.amazon.awssdk.core.internal.http.async;
 import static software.amazon.awssdk.utils.FunctionalUtils.invokeSafely;
 
 import java.nio.ByteBuffer;
+
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import software.amazon.awssdk.annotations.ReviewBeforeRelease;
@@ -62,16 +63,31 @@ public final class SimpleRequestProvider implements SdkHttpRequestProvider {
 
     @Override
     public void subscribe(Subscriber<? super ByteBuffer> s) {
-        s.onSubscribe(new Subscription() {
-            @Override
-            public void request(long n) {
+        s.onSubscribe(new SubscriptionImpl(s));
+    }
+
+    private class SubscriptionImpl implements Subscription {
+        private boolean running = true;
+        private final Subscriber<? super ByteBuffer> s;
+
+        private SubscriptionImpl(Subscriber<? super ByteBuffer> s) {
+            this.s = s;
+        }
+
+        @Override
+        public void request(long n) {
+            if (n <= 0) {
+                s.onError(new IllegalArgumentException("Demand must be positive"));
+            } else if (running) {
                 s.onNext(ByteBuffer.wrap(content));
                 s.onComplete();
+                running = false;
             }
+        }
 
-            @Override
-            public void cancel() {
-            }
-        });
+        @Override
+        public void cancel() {
+            running = false;
+        }
     }
 }

core/src/main/java/software/amazon/awssdk/core/pagination/async/PaginatedItemsPublisher.java
@@ -45,6 +45,6 @@ public class PaginatedItemsPublisher<ResponseT, ItemT> implements SdkPublisher<I
     @Override
     public void subscribe(Subscriber<? super ItemT> subscriber) {
         subscriber.onSubscribe(isLastPage ? new EmptySubscription(subscriber)
-                                          : new ItemsSubscription(subscriber, nextPageFetcher, getIteratorFunction));
+                                          : new ItemsSubscription<>(subscriber, nextPageFetcher, getIteratorFunction));
     }
 }

commit 221ec5c6b354c172e02f77b78aad42a71cd32cd0

Synchronize on FileSubscription.this
Regression from #519

core/src/main/java/software/amazon/awssdk/core/async/FileAsyncRequestBody.java
@@ -234,7 +234,7 @@ final class FileAsyncRequestBody implements AsyncRequestBody {
                         closeFile();
                     }
 
-                    synchronized (this) {
+                    synchronized (FileSubscription.this) {
                         writeInProgress = false;
                     }
                 }