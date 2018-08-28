ee6a9cf447727cee9f243a6170751c7906db6d0f
Varun Nandi <varunkn@amazon.com>
2018-08-28 15:18:42 -0700

    Remove SimpleDB

--- core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/pipeline/stages/MoveParametersToBodyStage.java
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/pipeline/stages/MoveParametersToBodyStage.java
@@ -23,7 +23,6 @@ import java.nio.charset.StandardCharsets;
 import software.amazon.awssdk.annotations.ReviewBeforeRelease;
 import software.amazon.awssdk.annotations.SdkInternalApi;
 import software.amazon.awssdk.annotations.SdkProtectedApi;
-import software.amazon.awssdk.core.interceptor.SdkExecutionAttribute;
 import software.amazon.awssdk.core.internal.http.RequestExecutionContext;
 import software.amazon.awssdk.core.internal.http.pipeline.MutableRequestToRequestPipeline;
 import software.amazon.awssdk.http.SdkHttpFullRequest;
@@ -45,20 +44,9 @@ public final class MoveParametersToBodyStage implements MutableRequestToRequestP
 
     private boolean shouldPutParamsInBody(SdkHttpFullRequest.Builder input,
                                           RequestExecutionContext context) {
-        return notSimpleDb(context) &&
-               input.method() == SdkHttpMethod.POST &&
+        return input.method() == SdkHttpMethod.POST &&
                input.content() == null &&
                !CollectionUtils.isNullOrEmpty(input.rawQueryParameters());
-    }
-
-    // TODO FIXME
-    @ReviewBeforeRelease("SigV2 expects query params to be signed as query params despite being in the body. Moving" +
-                         " before signing breaks SimpleDB which still uses SigV2. Probably the best thing to do is" +
-                         " make the SigV2 signer be aware that params are being moved into the body and move them back out" +
-                         " and unencode and sign them as query params. We did a similar thing in the V4 signer in 1.11.x" +
-                         " but I'd rather have the grossness in the legacy signer implementation")
-    private boolean notSimpleDb(RequestExecutionContext context) {
-        return !"SimpleDbClient".equals(context.executionAttributes().getAttribute(SdkExecutionAttribute.SERVICE_NAME));
     }
 
     @SdkProtectedApi
