d1ef15f47de07340ac7c7f850d59399d737b8c26
Zoe Wang <zoewang@amazon.com>
2018-10-24 12:25:15 -0700

    Add mock and integration tests to test using SDK on module path with Java 9+

---
+++ test/module-path-tests/src/main/java/software/amazon/awssdk/modulepath/tests/MockTestsRunner.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.modulepath.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+import software.amazon.awssdk.modulepath.tests.mocktests.BaseMockApiCall;
+import software.amazon.awssdk.modulepath.tests.mocktests.JsonProtocolApiCall;
+import software.amazon.awssdk.modulepath.tests.mocktests.XmlProtocolApiCall;
+
+/**
+ * Executor to run mock tests on module path.
+ */
+public class MockTestsRunner {
+
+    private MockTestsRunner() {
+    }
+
+    public static void main(String... args) {
+        List<BaseMockApiCall> tests = new ArrayList<>();
+        tests.add(new XmlProtocolApiCall());
+        tests.add(new JsonProtocolApiCall());
+
+        tests.forEach(t -> {
+            t.successfulApiCall();
+            t.failedApiCall();
+
+            t.successfulAsyncApiCall();
+            t.failedAsyncApiCall();
+        });
+    }
+}
---
+++ test/module-path-tests/src/main/java/software/amazon/awssdk/modulepath/tests/mocktests/BaseMockApiCall.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.modulepath.tests.mocktests;
+
+import java.io.ByteArrayInputStream;
+import java.util.concurrent.CompletionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.http.AbortableInputStream;
+import software.amazon.awssdk.http.SdkHttpFullResponse;
+
+/**
+ * Base classes to mock sync and async api calls.
+ */
+public abstract class BaseMockApiCall {
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseMockApiCall.class);
+
+    protected final MockHttpClient mockHttpClient;
+    protected final MockAyncHttpClient mockAyncHttpClient;
+    private final String protocol;
+
+    public BaseMockApiCall(String protocol) {
+        this.protocol = protocol;
+        this.mockHttpClient = new MockHttpClient();
+        this.mockAyncHttpClient = new MockAyncHttpClient();
+    }
+
+    public void successfulApiCall() {
+        logger.info("stubing successful api call for {} protocol", protocol);
+        mockHttpClient.stubNextResponse(successResponse(protocol));
+        runnable().run();
+        mockHttpClient.reset();
+    }
+
+    public void failedApiCall() {
+        logger.info("stubing failed api call for {} protocol", protocol);
+        mockHttpClient.stubNextResponse(errorResponse(protocol));
+
+        try {
+            runnable().run();
+        } catch (AwsServiceException e) {
+            logger.info("Received expected service exception", e.getMessage());
+        }
+
+        mockHttpClient.reset();
+    }
+
+    public void successfulAsyncApiCall() {
+        logger.info("stubing successful async api call for {} protocol", protocol);
+        mockAyncHttpClient.stubNextResponse(successResponse(protocol));
+        asyncRunnable().run();
+        mockAyncHttpClient.reset();
+    }
+
+    public void failedAsyncApiCall() {
+        logger.info("stubing failed async api call for {} protocol", protocol);
+        mockAyncHttpClient.stubNextResponse(errorResponse(protocol));
+
+        try {
+            asyncRunnable().run();
+        } catch (CompletionException e) {
+            if (e.getCause() instanceof AwsServiceException) {
+                logger.info("expected service exception {}", e.getMessage());
+            } else {
+                throw new RuntimeException("Unexpected exception is thrown");
+            }
+        }
+
+        mockAyncHttpClient.reset();
+    }
+
+    abstract Runnable runnable();
+
+    abstract Runnable asyncRunnable();
+
+    private SdkHttpFullResponse successResponse(String protocol) {
+        return SdkHttpFullResponse.builder()
+                                  .statusCode(200)
+                                  .content(generateContent(protocol))
+                                  .build();
+    }
+
+    private SdkHttpFullResponse errorResponse(String protocol) {
+        return SdkHttpFullResponse.builder()
+                                  .statusCode(500)
+                                  .content(generateContent(protocol))
+                                  .build();
+    }
+
+    private AbortableInputStream generateContent(String protocol) {
+        String content;
+        switch (protocol) {
+            case "xml":
+                content = "<foo></foo>";
+                break;
+            default:
+            case "json":
+                content = "{}";
+        }
+
+        return AbortableInputStream.create(new ByteArrayInputStream(content.getBytes()));
+    }
+}
