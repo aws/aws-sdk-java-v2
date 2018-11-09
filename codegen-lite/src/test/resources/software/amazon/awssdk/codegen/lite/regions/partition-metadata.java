655d7a7182b6143ebdcc9286f149ffc07a6dd27e
Fink <finks@amazon.com>
2018-11-09 10:18:18 -0800

    Updating tests to have annotation

--- codegen-lite/src/test/resources/software/amazon/awssdk/codegen/lite/regions/partition-metadata.java
+++ codegen-lite/src/test/resources/software/amazon/awssdk/codegen/lite/regions/partition-metadata.java
@@ -1,21 +1,10 @@
-/*
- * Copyright 2013-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
- * the License. A copy of the License is located at
- *
- * http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
- * and limitations under the License.
- */
-
 package software.amazon.awssdk.regions.partitionmetadata;
 
 import software.amazon.awssdk.annotations.Generated;
+import software.amazon.awssdk.annotations.SdkPublicApi;
 import software.amazon.awssdk.regions.PartitionMetadata;
 
+@SdkPublicApi
 @Generated("software.amazon.awssdk:codegen")
 public final class AwsPartitionMetadata implements PartitionMetadata {
     private static final String DNS_SUFFIX = "amazonaws.com";
