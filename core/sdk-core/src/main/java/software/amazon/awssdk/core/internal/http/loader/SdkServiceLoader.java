87fe163d3b563c3160161cd451f25b52d67c5b4e
Zoe Wang <zoewang@amazon.com>
2018-11-30 13:17:37 -0800

    Update and `ClassLoaderHelper#classLoader` to load class using the class loader of that class and fallback to context class loader and system class loader. See #56
    Address comments and make classLoader fallback to contextClassLoader

--- core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/loader/SdkServiceLoader.java
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/loader/SdkServiceLoader.java
@@ -18,9 +18,10 @@ package software.amazon.awssdk.core.internal.http.loader;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 import software.amazon.awssdk.annotations.SdkInternalApi;
+import software.amazon.awssdk.core.internal.util.ClassLoaderHelper;
 
 /**
- * Thin layer over {@link ServiceLoader} to allow mocking in tests.
+ * Thin layer over {@link ServiceLoader}.
  */
 @SdkInternalApi
 class SdkServiceLoader {
@@ -28,6 +29,6 @@ class SdkServiceLoader {
     public static final SdkServiceLoader INSTANCE = new SdkServiceLoader();
 
     <T> Iterator<T> loadServices(Class<T> clzz) {
-        return ServiceLoader.load(clzz).iterator();
+        return ServiceLoader.load(clzz, ClassLoaderHelper.classLoader(SdkServiceLoader.class)).iterator();
     }
 }
