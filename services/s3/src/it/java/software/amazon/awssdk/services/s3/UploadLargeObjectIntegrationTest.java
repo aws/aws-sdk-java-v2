03a661043e38987262231f6ea3dfe62cdf4d968e
Zoe Wang <zoewang@amazon.com>
2019-01-14 17:47:34 -0800

    add OneTimeReadTimeoutHandler to requests with expect continue header to avoid unexpected ReadTimeoutException

---
+++ services/s3/src/it/java/software/amazon/awssdk/services/s3/UploadLargeObjectIntegrationTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.services.s3;
+
+import static software.amazon.awssdk.testutils.service.S3BucketUtils.temporaryBucketName;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.testutils.RandomTempFile;
+import software.amazon.awssdk.utils.IoUtils;
+
+@Ignore("The tests are intended to run manually as it can take a relatively long time")
+public class UploadLargeObjectIntegrationTest extends S3IntegrationTestBase {
+
+    private static final String BUCKET = temporaryBucketName(UploadLargeObjectIntegrationTest.class);
+
+    private static final String ASYNC_KEY = "async-key";
+    private static final String SYNC_KEY = "sync-key";
+
+    private static File file;
+
+    @BeforeClass
+    public static void setupFixture() throws IOException {
+        createBucket(BUCKET);
+        file = new RandomTempFile(500 * 1024 * 1024);
+    }
+
+    @AfterClass
+    public static void tearDownFixture() {
+        deleteBucketAndAllContents(BUCKET);
+        file.delete();
+    }
+
+    @Test
+    public void syncPutLargeObject() throws Exception {
+        s3.putObject(b -> b.bucket(BUCKET).key(SYNC_KEY), file.toPath());
+        verifyResponse(SYNC_KEY);
+    }
+
+    @Test
+    public void asyncPutLargeObject() throws Exception {
+        s3Async.putObject(b -> b.bucket(BUCKET).key(ASYNC_KEY), file.toPath()).join();
+        verifyResponse(ASYNC_KEY);
+    }
+
+    private void verifyResponse(String key) {
+        ResponseInputStream<GetObjectResponse> responseInputStream = s3.getObject(b -> b.bucket(BUCKET).key(key));
+        IoUtils.drainInputStream(responseInputStream);
+    }
+}
