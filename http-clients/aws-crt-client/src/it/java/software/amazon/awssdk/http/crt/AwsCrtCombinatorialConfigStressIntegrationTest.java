c94aaaa691a150b1183d356ce9581ca8282bb526
Alex Weibel <aweibel@amazon.com>
2019-08-30 13:13:23 -0700

    Rename to AwsCrtCombinatorialConfigStressIntegrationTest

--- http-clients/aws-crt-client/src/it/java/software/amazon/awssdk/http/crt/AwsCrtCombinatorialConfigStressIntegrationTest.java
+++ http-clients/aws-crt-client/src/it/java/software/amazon/awssdk/http/crt/AwsCrtCombinatorialConfigStressIntegrationTest.java
@@ -45,7 +45,7 @@ import software.amazon.awssdk.services.kms.model.GenerateRandomResponse;
 import software.amazon.awssdk.utils.AttributeMap;
 
 @RunWith(Theories.class)
-public class AwsCrtCombinatorialConfigStressTest {
+public class AwsCrtCombinatorialConfigStressIntegrationTest {
     private final static String KEY_ALIAS = "alias/aws-sdk-java-v2-integ-test";
     private final static Region REGION = Region.US_EAST_1;
     private final static List<SdkAsyncHttpClient> awsCrtHttpClients = new ArrayList<>();
