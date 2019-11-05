0438595ae7800404402312438973f418d9ed7f89
Jonathan M. Henson <henso@amazon.com>
2019-11-04 20:05:03 -0800

    Fixed benchmark builds.

---
+++ test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/apicall/httpclient/async/AwsCrtClientNonTLSBenchmark.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.benchmark.apicall.httpclient.async;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.profile.StackProfiler;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import software.amazon.awssdk.benchmark.apicall.httpclient.SdkHttpClientBenchmark;
+import software.amazon.awssdk.benchmark.utils.MockServer;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
+import software.amazon.awssdk.services.protocolrestjson.ProtocolRestJsonAsyncClient;
+
+import java.util.Collection;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static software.amazon.awssdk.benchmark.utils.BenchmarkConstant.CONCURRENT_CALLS;
+import static software.amazon.awssdk.benchmark.utils.BenchmarkUtils.awaitCountdownLatchUninterruptibly;
+import static software.amazon.awssdk.benchmark.utils.BenchmarkUtils.countDownUponCompletion;
+
+/**
+ * Using aws-crt-client to test against local mock https server.
+ */
+@State(Scope.Benchmark)
+@Warmup(iterations = 3, time = 15, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Fork(2) // To reduce difference between each run
+@BenchmarkMode(Mode.Throughput)
+public class AwsCrtClientNonTLSBenchmark implements SdkHttpClientBenchmark {
+
+    private MockServer mockServer;
+    private SdkAsyncHttpClient sdkHttpClient;
+    private ProtocolRestJsonAsyncClient client;
+
+    @Setup(Level.Trial)
+    public void setup() throws Exception {
+        mockServer = new MockServer();
+        mockServer.start();
+
+        sdkHttpClient = AwsCrtAsyncHttpClient.builder()
+                .verifyPeer(false)
+                .build();
+
+        client = ProtocolRestJsonAsyncClient.builder()
+                .endpointOverride(mockServer.getHttpUri())
+                .httpClient(sdkHttpClient)
+                .build();
+
+        // Making sure the request actually succeeds
+        client.allTypes().join();
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDown() throws Exception {
+        mockServer.stop();
+        client.close();
+        sdkHttpClient.close();
+    }
+
+    @Override
+    @Benchmark
+    @OperationsPerInvocation(CONCURRENT_CALLS)
+    public void concurrentApiCall(Blackhole blackhole) {
+        CountDownLatch countDownLatch = new CountDownLatch(CONCURRENT_CALLS);
+        for (int i = 0; i < CONCURRENT_CALLS; i++) {
+            countDownUponCompletion(blackhole, client.allTypes(), countDownLatch);
+        }
+
+        awaitCountdownLatchUninterruptibly(countDownLatch, 10, TimeUnit.SECONDS);
+
+    }
+
+    @Override
+    @Benchmark
+    public void sequentialApiCall(Blackhole blackhole) {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownUponCompletion(blackhole, client.allTypes(), countDownLatch);
+        awaitCountdownLatchUninterruptibly(countDownLatch, 1, TimeUnit.SECONDS);
+    }
+
+    public static void main(String... args) throws Exception {
+        Options opt = new OptionsBuilder()
+                .include(AwsCrtClientNonTLSBenchmark.class.getSimpleName())
+                .addProfiler(StackProfiler.class)
+                .build();
+        Collection<RunResult> run = new Runner(opt).run();
+    }
+}
