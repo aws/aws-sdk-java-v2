1a9658d1318ebd134775442af48ed12133c4f5f7
Matthew Miller <millem@amazon.com>
2019-12-03 13:24:08 -0800

    Reap unhealthy and idle HTTP/2 connections.
    1. Periodically PING HTTP/2 connections and close them if the service doesn't respond within a certain period of time.
    2. Close HTTP/2 connections that have had no streams checked out for a certain period of time.

--- http-clients/netty-nio-client/src/test/java/software/amazon/awssdk/http/nio/netty/internal/http2/MultiplexedChannelRecordTest.java
+++ http-clients/netty-nio-client/src/test/java/software/amazon/awssdk/http/nio/netty/internal/http2/MultiplexedChannelRecordTest.java
@@ -15,59 +15,177 @@
 
 package software.amazon.awssdk.http.nio.netty.internal.http2;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static software.amazon.awssdk.http.nio.netty.internal.ChannelAttributeKey.PING_TRACKER;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.handler.codec.http2.Http2FrameCodecBuilder;
+import io.netty.handler.codec.http2.Http2MultiplexHandler;
 import io.netty.util.concurrent.DefaultPromise;
 import io.netty.util.concurrent.Promise;
-import io.netty.util.concurrent.ScheduledFuture;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import software.amazon.awssdk.http.Protocol;
+import software.amazon.awssdk.http.nio.netty.internal.ChannelAttributeKey;
 import software.amazon.awssdk.http.nio.netty.internal.MockChannel;
 
 public class MultiplexedChannelRecordTest {
+    private EventLoopGroup loopGroup;
+    private MockChannel channel;
 
-    private static EventLoopGroup loopGroup;
-    private static Channel channel;
-
-    @BeforeClass
-    public static void setup() throws Exception {
+    @Before
+    public void setup() throws Exception {
         loopGroup = new NioEventLoopGroup(4);
         channel = new MockChannel();
     }
 
-    @AfterClass
-    public static void teardown() {
+    @After
+    public void teardown() {
         loopGroup.shutdownGracefully().awaitUninterruptibly();
         channel.close();
     }
 
     @Test
-    public void pingInflight_reusableShouldBeFalse() throws Exception {
+    public void nullIdleTimeoutSeemsToDisableReaping() throws InterruptedException {
+        EmbeddedChannel channel = newHttp2Channel();
+        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 1, null);
+
+        Promise<Channel> streamPromise = channel.eventLoop().newPromise();
+        record.acquireStream(streamPromise);
+
+        channel.runPendingTasks();
+
+        assertThat(streamPromise.isSuccess()).isTrue();
+        assertThat(channel.isOpen()).isTrue();
+
+        record.closeAndReleaseChild(streamPromise.getNow());
+
+        assertThat(channel.isOpen()).isTrue();
+
+        Thread.sleep(1_000);
+        channel.runPendingTasks();
+
+        assertThat(channel.isOpen()).isTrue();
+    }
+
+    @Test
+    public void recordsWithoutReservedStreamsAreClosedAfterTimeout() throws InterruptedException {
+        int idleTimeoutMillis = 1000;
+        EmbeddedChannel channel = newHttp2Channel();
+        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 1, Duration.ofMillis(idleTimeoutMillis));
+
+        Promise<Channel> streamPromise = channel.eventLoop().newPromise();
+        record.acquireStream(streamPromise);
+
+        channel.runPendingTasks();
+
+        assertThat(streamPromise.isSuccess()).isTrue();
+        assertThat(channel.isOpen()).isTrue();
+
+        record.closeAndReleaseChild(streamPromise.getNow());
+
+        assertThat(channel.isOpen()).isTrue();
+
+        Thread.sleep(idleTimeoutMillis * 2);
+        channel.runPendingTasks();
+
+        assertThat(channel.isOpen()).isFalse();
+    }
+
+    @Test
+    public void recordsWithReservedStreamsAreNotClosedAfterTimeout() throws InterruptedException {
+        int idleTimeoutMillis = 1000;
+        EmbeddedChannel channel = newHttp2Channel();
+        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 2, Duration.ofMillis(idleTimeoutMillis));
+
+        Promise<Channel> streamPromise = channel.eventLoop().newPromise();
+        Promise<Channel> streamPromise2 = channel.eventLoop().newPromise();
+        record.acquireStream(streamPromise);
+        record.acquireStream(streamPromise2);
+
+        channel.runPendingTasks();
+
+        assertThat(streamPromise.isSuccess()).isTrue();
+        assertThat(streamPromise2.isSuccess()).isTrue();
+        assertThat(channel.isOpen()).isTrue();
+
+        record.closeAndReleaseChild(streamPromise.getNow());
+
+        assertThat(channel.isOpen()).isTrue();
+
+        Thread.sleep(idleTimeoutMillis * 2);
+        channel.runPendingTasks();
+
+        assertThat(channel.isOpen()).isTrue();
+    }
+
+    @Test
+    public void acquireRequestResetsCloseTimer() throws InterruptedException {
+        int idleTimeoutMillis = 1000;
+        EmbeddedChannel channel = newHttp2Channel();
+        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 2, Duration.ofMillis(idleTimeoutMillis));
+
+        for (int i = 0; i < 20; ++i) {
+            Thread.sleep(idleTimeoutMillis / 10);
+            channel.runPendingTasks();
+
+            Promise<Channel> streamPromise = channel.eventLoop().newPromise();
+            assertThat(record.acquireStream(streamPromise)).isTrue();
+            channel.runPendingTasks();
+
+            assertThat(streamPromise.isSuccess()).isTrue();
+            assertThat(channel.isOpen()).isTrue();
+
+            record.closeAndReleaseChild(streamPromise.getNow());
+            channel.runPendingTasks();
+        }
+
+        assertThat(channel.isOpen()).isTrue();
+
+        Thread.sleep(idleTimeoutMillis * 2);
+        channel.runPendingTasks();
+
+        assertThat(channel.isOpen()).isFalse();
+    }
+
+    @Test
+    public void idleTimerDoesNotApplyBeforeFirstChannelIsCreated() throws InterruptedException {
+        int idleTimeoutMillis = 1000;
+        EmbeddedChannel channel = newHttp2Channel();
+        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 2, Duration.ofMillis(idleTimeoutMillis));
+
+        Thread.sleep(idleTimeoutMillis * 2);
+        channel.runPendingTasks();
+
+        assertThat(channel.isOpen()).isTrue();
+    }
+
+    @Test
+    public void availableStream0_reusableShouldBeFalse() {
         loopGroup.register(channel).awaitUninterruptibly();
         Promise<Channel> channelPromise = new DefaultPromise<>(loopGroup.next());
         channelPromise.setSuccess(channel);
 
-        channel.attr(PING_TRACKER).set(new PingTracker(() -> Mockito.mock(ScheduledFuture.class)));
-        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 8);
+        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 0, Duration.ofSeconds(10));
 
         assertThat(record.acquireStream(null)).isFalse();
     }
 
-    @Test
-    public void availableStream0_reusableShouldBeFalse() throws Exception {
-        loopGroup.register(channel).awaitUninterruptibly();
-        Promise<Channel> channelPromise = new DefaultPromise<>(loopGroup.next());
-        channelPromise.setSuccess(channel);
+    private EmbeddedChannel newHttp2Channel() {
+        EmbeddedChannel channel = new EmbeddedChannel(Http2FrameCodecBuilder.forClient().build(),
+                                                      new Http2MultiplexHandler(new NoOpHandler()));
+        channel.attr(ChannelAttributeKey.PROTOCOL_FUTURE).set(CompletableFuture.completedFuture(Protocol.HTTP2));
+        return channel;
+    }
 
-        MultiplexedChannelRecord record = new MultiplexedChannelRecord(channel, 0);
-
-        assertThat(record.acquireStream(null)).isFalse();
+    private static class NoOpHandler extends ChannelInitializer<Channel> {
+        @Override
+        protected void initChannel(Channel ch) { }
     }
 }
