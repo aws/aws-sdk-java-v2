0ad021426aed89639e6e61e6c5ebb26876dce146
Ben Maizels <bmaizels@amazon.com>
2020-01-09 15:49:30 -0800

    Bugfix: s3 asynchronous client GetBucketPolicy now works and does not always through an XML parsing error

--- test/protocol-tests/src/test/java/software/amazon/awssdk/protocol/tests/QueryRequestTransformTest.java
+++ test/protocol-tests/src/test/java/software/amazon/awssdk/protocol/tests/QueryRequestTransformTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ import software.amazon.awssdk.services.protocolquery.model.IdempotentOperationRe
 import java.net.URI;
 import java.util.List;
 
-public class QueryRequestTransformTest {
+public class QueryRequestTransformTest extends ProtocolTestBase {
 
     @Rule
     public WireMockRule wireMock = new WireMockRule(0);
