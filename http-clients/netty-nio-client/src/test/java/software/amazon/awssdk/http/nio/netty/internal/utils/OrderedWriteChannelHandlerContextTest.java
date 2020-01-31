28ed143a6fce54d240570c41a6744f41908227fe
Matthew Miller <millem@amazon.com>
2020-01-31 13:06:41 -0800

    Fixed an issue with Transcribe Streaming's startStreamingTranscription API that could cause signature mismatch exceptions.
    Because of https://github.com/netty/netty/issues/7783, writes performed by the event-loop thread can 'skip ahead' of writes
    performed off of the event-loop thread. This can cause events to be sent to Transcribe out-of-order, if some are performed
    on the event loop. This manifests as a signature mismatch exception because the signature of the last message is included in
    the signature.
    
    This change introduces a ChannelHandlerContext implementation that makes sure we're off of the event loop before performing a
    write. If we're on the event loop, it schedules a task for the write to be performed instead of performing the task immediately.
    
    This change isn't finalized yet, and still requires additional testing.

---
+++ http-clients/netty-nio-client/src/test/java/software/amazon/awssdk/http/nio/netty/internal/utils/OrderedWriteChannelHandlerContextTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.http.nio.netty.internal.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+public class OrderedWriteChannelHandlerContextTest {
+    @Test
+    public void wrapOnlyHappensOnce() {
+        EmbeddedChannel channel = new EmbeddedChannel(new NoOpHandler());
+        ChannelHandlerContext ctx = channel.pipeline().context(NoOpHandler.class);
+        ChannelHandlerContext wrapped = OrderedWriteChannelHandlerContext.wrap(ctx);
+        ChannelHandlerContext wrapped2 = OrderedWriteChannelHandlerContext.wrap(wrapped);
+
+        assertThat(ctx).isNotSameAs(wrapped);
+        assertThat(wrapped).isSameAs(wrapped2);
+    }
+
+    private static class NoOpHandler implements ChannelHandler {
+        @Override
+        public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+
+        }
+
+        @Override
+        public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+        }
+    }
+
+}
