f7b9605b25e7044b9bfa9bd2815570acbbb66d0a
Bret Ambrose <bretambrose@gmail.com>
2020-07-15 14:15:37 -0700

    Checkstyle

--- test/http-client-tests/src/main/java/software/amazon/awssdk/http/H1ServerBehaviorTestBase.java
+++ test/http-client-tests/src/main/java/software/amazon/awssdk/http/H1ServerBehaviorTestBase.java
@@ -45,7 +45,6 @@ import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
-
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
