7134917bb3de5a6f9d35cf6e3cd35a876b0f11b5
Zoe Wang <zoewang@amazon.com>
2020-09-08 14:19:54 -0700

    Add waiter generator task

---
+++ codegen/src/main/java/software/amazon/awssdk/codegen/poet/waiters/WaitersClassSpec.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.codegen.poet.waiters;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.TypeSpec;
+import software.amazon.awssdk.codegen.model.intermediate.IntermediateModel;
+import software.amazon.awssdk.codegen.model.service.WaiterDefinition;
+import software.amazon.awssdk.codegen.poet.ClassSpec;
+
+public class WaitersClassSpec implements ClassSpec {
+
+    private final String operationName;
+    private final WaiterDefinition waiterDefinition;
+    private final IntermediateModel model;
+
+    public WaitersClassSpec(IntermediateModel model, String operationName, WaiterDefinition waiterDefinition) {
+        this.model = model;
+        this.operationName = operationName;
+        this.waiterDefinition = waiterDefinition;
+    }
+
+    @Override
+    public TypeSpec poetSpec() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ClassName className() {
+        throw new UnsupportedOperationException();
+    }
+}
