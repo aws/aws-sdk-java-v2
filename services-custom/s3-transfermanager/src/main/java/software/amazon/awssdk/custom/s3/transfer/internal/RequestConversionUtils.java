43bb1447292c29f4e50e49639362989d672799a0
Zoe Wang <zoewang@amazon.com>
2021-01-08 16:30:02 -0800

    Fix build

--- services-custom/s3-transfermanager/src/main/java/software/amazon/awssdk/custom/s3/transfer/internal/RequestConversionUtils.java
+++ services-custom/s3-transfermanager/src/main/java/software/amazon/awssdk/custom/s3/transfer/internal/RequestConversionUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -46,6 +46,7 @@ final class RequestConversionUtils {
                                 .ifNoneMatch(getObjectRequest.ifNoneMatch())
                                 .ifUnmodifiedSince(getObjectRequest.ifUnmodifiedSince())
                                 .key(getObjectRequest.key())
+                                .expectedBucketOwner(getObjectRequest.expectedBucketOwner())
                                 .partNumber(getObjectRequest.partNumber())
                                 .range(getObjectRequest.range())
                                 .requestPayer(getObjectRequest.requestPayerAsString())
