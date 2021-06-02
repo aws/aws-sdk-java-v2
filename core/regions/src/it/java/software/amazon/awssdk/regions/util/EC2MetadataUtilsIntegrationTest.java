be5388e8218d20de46d35d4c6f17e94ff39f59d7
Matthew Miller <millem@amazon.com>
2021-06-02 14:09:17 -0700

    Moved Jackson from being an external SDK dependency to an internal dependency.
    Jackson-databind was removed from being a dependency (except in the code generator). Jackson-core and
    Jackson-dataformat-cbor were moved to shaded dependencies. This was done because Jackson versions
    are not 100% compatible between minor versions and keeping them up to date were problematic for SDK
    customers.
    
    Customers will see a change in artifact sizes. Customers which do not use Jackson-databind outside of
    the SDK today will see a ~1 MB artifact size decrease. Customers which do use Jackson-databind outside
    of the SDK will see a ~0.5 MB artifact size increase.
    
    This change required breaking inter-module (protected) APIs, so it should not be released outside of an
    SDK minor version bump. Customers will not be able to use older client versions with this version of
    the core libraries.
    
    Change summary:
    1. Added 'jackson-core' and 'jackson-dataformat-cbor' which contain the SDK's shaded versions with the
       Jackson packages of the same names.
    2. Removed 'aws-ion-protocol' module. This was not used by any public AWS services and can be re-added when
       service support is needed.
    3. Added 'json-utils', a library for reading and parsing JSON.

--- core/regions/src/it/java/software/amazon/awssdk/regions/util/EC2MetadataUtilsIntegrationTest.java
+++ core/regions/src/it/java/software/amazon/awssdk/regions/util/EC2MetadataUtilsIntegrationTest.java
@@ -49,39 +49,6 @@ public class EC2MetadataUtilsIntegrationTest {
         System.clearProperty(SdkSystemSetting.AWS_EC2_METADATA_SERVICE_ENDPOINT.property());
     }
 
-    @Test
-    public void testIamInfo() {
-        EC2MetadataUtils.IamInfo info = EC2MetadataUtils
-                .getIamInstanceProfileInfo();
-
-        Assert.assertEquals("Success", info.code);
-        Assert.assertNull(info.message);
-        Assert.assertEquals("2014-04-07T08:18:41Z", info.lastUpdated);
-        Assert.assertEquals("foobar", info.instanceProfileArn);
-        Assert.assertEquals("moobily", info.instanceProfileId);
-    }
-
-    @Test
-    public void testIamCredentials() {
-        Map<String, EC2MetadataUtils.IamSecurityCredential> map = EC2MetadataUtils
-                .getIamSecurityCredentials();
-
-        Assert.assertEquals(2, map.size());
-
-        for (Map.Entry<String, EC2MetadataUtils.IamSecurityCredential> entry : map
-                .entrySet()) {
-
-            Assert.assertNotNull(entry.getKey());
-            Assert.assertNotNull(entry.getValue().code);
-            Assert.assertNotNull(entry.getValue().lastUpdated);
-            Assert.assertEquals("AWS-HMAC", entry.getValue().type);
-            Assert.assertEquals("foobar", entry.getValue().accessKeyId);
-            Assert.assertEquals("moobily", entry.getValue().secretAccessKey);
-            Assert.assertEquals("beebop", entry.getValue().token);
-            Assert.assertNotNull(entry.getValue().expiration);
-        }
-    }
-
     @Test(expected = SdkClientException.class)
     public void ec2MetadataDisabled_shouldThrowException() {
         try {
@@ -90,26 +57,6 @@ public class EC2MetadataUtilsIntegrationTest {
         } finally {
             System.clearProperty(SdkSystemSetting.AWS_EC2_METADATA_DISABLED.property());
         }
-    }
-
-    @Test
-    public void testInstanceInfo() {
-        EC2MetadataUtils.InstanceInfo info = EC2MetadataUtils.getInstanceInfo();
-        Assert.assertEquals("2014-08-07T22:07:46Z", info.getPendingTime());
-        Assert.assertEquals("m1.small", info.getInstanceType());
-        Assert.assertEquals("ami-a49665cc", info.getImageId());
-        Assert.assertEquals("i-6b2de041", info.getInstanceId());
-        Assert.assertEquals("foo", info.getBillingProducts()[0]);
-        Assert.assertEquals("x86_64", info.getArchitecture());
-        Assert.assertEquals("599169622985", info.getAccountId());
-        Assert.assertEquals("aki-919dcaf8", info.getKernelId());
-        Assert.assertEquals("baz", info.getRamdiskId());
-        Assert.assertEquals("us-east-1", info.getRegion());
-        Assert.assertEquals("2010-08-31", info.getVersion());
-        Assert.assertEquals("us-east-1b", info.getAvailabilityZone());
-        Assert.assertEquals("10.201.215.38", info.getPrivateIp());
-        Assert.assertEquals("bar", info.getDevpayProductCodes()[0]);
-        Assert.assertEquals("qaz", info.getMarketplaceProductCodes()[0]);
     }
 
     @Test
