f6e485accb490298fb18ddd17fa9c8464810b423
Michael Li <mchlia@amazon.com>
2021-07-14 16:22:23 -0400

    Updating sdk-core tests with object to mimic batch request entry

--- core/sdk-core/src/test/java/software/amazon/awssdk/core/internal/batchutilities/BatchBufferTest.java
+++ core/sdk-core/src/test/java/software/amazon/awssdk/core/internal/batchutilities/BatchBufferTest.java
@@ -29,14 +29,14 @@ import org.testng.Assert;
 
 public class BatchBufferTest {
 
-    private BatchBuffer<String, String, List<String>> buffer;
+    private BatchBuffer<String, String, List<RequestWithId>> buffer;
     private String destination;
     private int currentId;
 
-    BiFunction<Map<String, String>, String, CompletableFuture<List<String>>> batchingFunction =
+    BiFunction<Map<String, String>, String, CompletableFuture<List<RequestWithId>>> batchingFunction =
         (requestEntryMap, destination) -> {
-            List<String> entries = new ArrayList<>(requestEntryMap.size());
-            requestEntryMap.forEach((key, value) -> entries.add(value));
+            List<RequestWithId> entries = new ArrayList<>(requestEntryMap.size());
+            requestEntryMap.forEach((key, value) -> entries.add(new RequestWithId(Integer.parseInt(key), value)));
             return CompletableFuture.supplyAsync(() -> {
                 try {
                     Thread.sleep(150);
@@ -47,14 +47,34 @@ public class BatchBufferTest {
             });
         };
 
-    Function<List<String>, Map<String, String>> unpackResponseFunction =
+    Function<List<RequestWithId>, Map<String, String>> unpackResponseFunction =
         requestBatchResponse -> {
             Map<String, String> mappedResponses = new HashMap<>();
-            for (int i = 0; i < requestBatchResponse.size(); i++) {
-                mappedResponses.put(Integer.toString(currentId++), requestBatchResponse.get(i));
+            for (RequestWithId requestWithId : requestBatchResponse) {
+                mappedResponses.put(Integer.toString(currentId++), requestWithId.getMessage());
             }
             return mappedResponses;
         };
+
+    //Object to mimic a batch request entry
+    private static class RequestWithId {
+
+        private final int id;
+        private final String message;
+
+        public RequestWithId(int id, String message) {
+            this.id = id;
+            this.message = message;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 
     @Before
     public void beforeEachBufferTest() {
@@ -67,7 +87,7 @@ public class BatchBufferTest {
     public void sendTenRequestsTest() {
         String[] requests = new String[10];
         for (int i = 0; i < requests.length; i++) {
-            requests[i] = Integer.toString(i);
+            requests[i] = "Message " + i;
         }
         List<CompletableFuture<String>> responses = new ArrayList<>();
         for (String request : requests) {
@@ -75,7 +95,7 @@ public class BatchBufferTest {
         }
         CompletableFuture.allOf(responses.toArray(new CompletableFuture[0])).join();
         for (int i = 0; i < requests.length; i++) {
-            Assert.assertEquals(responses.get(i).join(), Integer.toString(i));
+            Assert.assertEquals(responses.get(i).join(), "Message " + i);
         }
     }
 
@@ -83,15 +103,14 @@ public class BatchBufferTest {
     public void sendTwentyRequestsTest() {
         String[] requests = new String[20];
         for (int i = 0; i < requests.length; i++) {
-            requests[i] = Integer.toString(i);
+            requests[i] = "Message " + i;
         }
         List<CompletableFuture<String>> responses = new ArrayList<>();
         for (String request : requests) {
             responses.add(buffer.sendRequest(request, destination));
         }
-        CompletableFuture.allOf(responses.toArray(new CompletableFuture[0])).join();
-        for (CompletableFuture<String> response: responses) {
-            System.out.println("Message body:" + response.join());
+        for (int i = 0; i < requests.length; i++) {
+            Assert.assertEquals(responses.get(i).join(), "Message " + i);
         }
     }
 }
