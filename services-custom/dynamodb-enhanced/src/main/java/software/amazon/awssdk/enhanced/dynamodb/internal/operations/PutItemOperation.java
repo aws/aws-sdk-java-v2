023ebb4ba45c44003fd65da30df0185d2d6c4e89
Dongie Agnir <261310+dagnir@users.noreply.github.com>
2021-09-07 10:47:00 -0700

    Implement support for ReturnValuesOnConditionCheckFailure (#2688)
    * Implement return values on TransactWriteItems
    
    This change gives customer the ability to specify
    ReturnValuesOnConditionCheckFailure on the TransactWriteItems operation, i.e.
    for PutItem, DeleteItem, UpdateItem, and ConditionCheck.
    
    In order to support this feature, new Transact*EnhancedRequest objects have been
    introduced because ReturnValuesOnConditionCheckFailure does not make any sense
    for the non-TransactWrite operations. Conversely, other options on the
    non-transact versions like ReturnConsumedCapacity do not make sense for the
    Transact* versions.
    
    As a result of "splitting" off the TransactWrite versions of the operations, the
    methods on TransactWriteItemsEnhancedRequest that take the non-transact versions
    have been deprecated.
    
    Fixes #2283
    
    * Review comments

--- services-custom/dynamodb-enhanced/src/main/java/software/amazon/awssdk/enhanced/dynamodb/internal/operations/PutItemOperation.java
+++ services-custom/dynamodb-enhanced/src/main/java/software/amazon/awssdk/enhanced/dynamodb/internal/operations/PutItemOperation.java
@@ -16,6 +16,7 @@
 package software.amazon.awssdk.enhanced.dynamodb.internal.operations;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 import software.amazon.awssdk.annotations.SdkInternalApi;
@@ -27,6 +28,7 @@ import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.enhanced.dynamodb.extensions.WriteModification;
 import software.amazon.awssdk.enhanced.dynamodb.internal.extensions.DefaultDynamoDbExtensionContext;
 import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
+import software.amazon.awssdk.enhanced.dynamodb.model.TransactPutItemEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
@@ -36,6 +38,7 @@ import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 import software.amazon.awssdk.services.dynamodb.model.PutRequest;
 import software.amazon.awssdk.services.dynamodb.model.TransactWriteItem;
 import software.amazon.awssdk.services.dynamodb.model.WriteRequest;
+import software.amazon.awssdk.utils.Either;
 
 @SdkInternalApi
 public class PutItemOperation<T>
@@ -43,13 +46,21 @@ public class PutItemOperation<T>
                TransactableWriteOperation<T>,
                TableOperation<T, PutItemRequest, PutItemResponse, Void> {
 
-    private final PutItemEnhancedRequest<T> request;
+    private final Either<PutItemEnhancedRequest<T>, TransactPutItemEnhancedRequest<T>> request;
 
     private PutItemOperation(PutItemEnhancedRequest<T> request) {
-        this.request = request;
+        this.request = Either.left(request);
+    }
+
+    private PutItemOperation(TransactPutItemEnhancedRequest<T> request) {
+        this.request = Either.right(request);
     }
 
     public static <T> PutItemOperation<T> create(PutItemEnhancedRequest<T> request) {
+        return new PutItemOperation<>(request);
+    }
+
+    public static <T> PutItemOperation<T> create(TransactPutItemEnhancedRequest<T> request) {
         return new PutItemOperation<>(request);
     }
 
@@ -68,7 +79,8 @@ public class PutItemOperation<T>
         tableMetadata.primaryPartitionKey();
 
         boolean alwaysIgnoreNulls = true;
-        Map<String, AttributeValue> itemMap = tableSchema.itemToMap(this.request.item(), alwaysIgnoreNulls);
+        T item = request.map(PutItemEnhancedRequest::item, TransactPutItemEnhancedRequest::item);
+        Map<String, AttributeValue> itemMap = tableSchema.itemToMap(item, alwaysIgnoreNulls);
 
         WriteModification transformation =
             extension != null ? extension.beforeWrite(
@@ -138,28 +150,35 @@ public class PutItemOperation<T>
                                                        DynamoDbEnhancedClientExtension dynamoDbEnhancedClientExtension) {
         PutItemRequest putItemRequest = generateRequest(tableSchema, operationContext, dynamoDbEnhancedClientExtension);
 
-        Put put = Put.builder()
-                     .item(putItemRequest.item())
-                     .tableName(putItemRequest.tableName())
-                     .conditionExpression(putItemRequest.conditionExpression())
-                     .expressionAttributeValues(putItemRequest.expressionAttributeValues())
-                     .expressionAttributeNames(putItemRequest.expressionAttributeNames())
-                     .build();
+        Put.Builder builder = Put.builder()
+                                 .item(putItemRequest.item())
+                                 .tableName(putItemRequest.tableName())
+                                 .conditionExpression(putItemRequest.conditionExpression())
+                                 .expressionAttributeValues(putItemRequest.expressionAttributeValues())
+                                 .expressionAttributeNames(putItemRequest.expressionAttributeNames());
+
+        request.right()
+               .map(TransactPutItemEnhancedRequest::returnValuesOnConditionCheckFailureAsString)
+               .ifPresent(builder::returnValuesOnConditionCheckFailure);
 
         return TransactWriteItem.builder()
-                                .put(put)
+                                .put(builder.build())
                                 .build();
     }
 
     private PutItemRequest.Builder addExpressionsIfExist(PutItemRequest.Builder requestBuilder,
                                                          WriteModification transformation) {
+
+        Expression originalConditionExpression = request.map(r -> Optional.ofNullable(r.conditionExpression()),
+                                                             r -> Optional.ofNullable(r.conditionExpression()))
+                                                        .orElse(null);
         Expression mergedConditionExpression;
 
         if (transformation != null && transformation.additionalConditionalExpression() != null) {
-            mergedConditionExpression = Expression.join(this.request.conditionExpression(),
+            mergedConditionExpression = Expression.join(originalConditionExpression,
                                                         transformation.additionalConditionalExpression(), " AND ");
         } else {
-            mergedConditionExpression = this.request.conditionExpression();
+            mergedConditionExpression = originalConditionExpression;
         }
 
         if (mergedConditionExpression != null) {
@@ -177,5 +196,4 @@ public class PutItemOperation<T>
         }
         return requestBuilder;
     }
-
 }
---
+++ services-custom/dynamodb-enhanced/src/main/java/software/amazon/awssdk/enhanced/dynamodb/model/TransactUpdateItemEnhancedRequest.java
@@ -0,0 +1,239 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.enhanced.dynamodb.model;
+
+import java.util.Objects;
+import software.amazon.awssdk.annotations.SdkPublicApi;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.Expression;
+import software.amazon.awssdk.services.dynamodb.model.ReturnValuesOnConditionCheckFailure;
+
+/**
+ * Defines parameters used to update an item to a DynamoDb table using the
+ * {@link DynamoDbEnhancedClient#transactWriteItems(TransactWriteItemsEnhancedRequest)} or
+ * {@link DynamoDbEnhancedAsyncClient#transactWriteItems(TransactWriteItemsEnhancedRequest)}
+ * operation.
+ * <p>
+ * A valid request object must contain the item that should be written to the table.
+ *
+ * @param <T> The type of the modelled object.
+ */
+@SdkPublicApi
+public class TransactUpdateItemEnhancedRequest<T> {
+
+    private final T item;
+    private final Boolean ignoreNulls;
+    private final Expression conditionExpression;
+    private final String returnValuesOnConditionCheckFailure;
+
+    private TransactUpdateItemEnhancedRequest(Builder<T> builder) {
+        this.item = builder.item;
+        this.ignoreNulls = builder.ignoreNulls;
+        this.conditionExpression = builder.conditionExpression;
+        this.returnValuesOnConditionCheckFailure = builder.returnValuesOnConditionCheckFailure;
+    }
+
+    /**
+     * Creates a newly initialized builder for the request object.
+     *
+     * @param itemClass the class that items in this table map to
+     * @param <T>       The type of the modelled object, corresponding to itemClass
+     * @return a UpdateItemEnhancedRequest builder
+     */
+    public static <T> Builder<T> builder(Class<? extends T> itemClass) {
+        return new Builder<>();
+    }
+
+    /**
+     * Returns a builder initialized with all existing values on the request object.
+     */
+    public Builder<T> toBuilder() {
+        return new Builder<T>().item(item)
+                               .ignoreNulls(ignoreNulls)
+                               .conditionExpression(conditionExpression)
+                               .returnValuesOnConditionCheckFailure(returnValuesOnConditionCheckFailure);
+    }
+
+    /**
+     * Returns the item for this update operation request.
+     */
+    public T item() {
+        return item;
+    }
+
+    /**
+     * Returns if the update operation should ignore attributes with null values, or false if it has not been set.
+     */
+    public Boolean ignoreNulls() {
+        return ignoreNulls;
+    }
+
+    /**
+     * Returns the condition {@link Expression} set on this request object, or null if it doesn't exist.
+     */
+    public Expression conditionExpression() {
+        return conditionExpression;
+    }
+
+    /**
+     * Returns what values to return if the condition check fails.
+     * <p>
+     * If the service returns an enum value that is not available in the current SDK version,
+     * {@link #returnValuesOnConditionCheckFailure} will return
+     * {@link ReturnValuesOnConditionCheckFailure#UNKNOWN_TO_SDK_VERSION}. The raw value returned by the service is
+     * available from {@link #returnValuesOnConditionCheckFailureAsString}.
+     *
+     * @return What values to return on condition check failure.
+     */
+    public ReturnValuesOnConditionCheckFailure returnValuesOnConditionCheckFailure() {
+        return ReturnValuesOnConditionCheckFailure.fromValue(returnValuesOnConditionCheckFailure);
+    }
+
+    /**
+     * Returns what values to return if the condition check fails.
+     * <p>
+     * If the service returns an enum value that is not available in the current SDK version,
+     * {@link #returnValuesOnConditionCheckFailure} will return
+     * {@link ReturnValuesOnConditionCheckFailure#UNKNOWN_TO_SDK_VERSION}. The raw value returned by the service is
+     * available from {@link #returnValuesOnConditionCheckFailureAsString}.
+     *
+     * @return What values to return on condition check failure.
+     */
+    public String returnValuesOnConditionCheckFailureAsString() {
+        return returnValuesOnConditionCheckFailure;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TransactUpdateItemEnhancedRequest<?> that = (TransactUpdateItemEnhancedRequest<?>) o;
+
+        if (!Objects.equals(item, that.item)) {
+            return false;
+        }
+        if (!Objects.equals(ignoreNulls, that.ignoreNulls)) {
+            return false;
+        }
+        if (!Objects.equals(conditionExpression, that.conditionExpression)) {
+            return false;
+        }
+        return Objects.equals(returnValuesOnConditionCheckFailure, that.returnValuesOnConditionCheckFailure);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(item);
+        result = 31 * result + Objects.hashCode(ignoreNulls);
+        result = 31 * result + Objects.hashCode(conditionExpression);
+        result = 31 * result + Objects.hashCode(returnValuesOnConditionCheckFailure);
+        return result;
+    }
+
+    /**
+     * A builder that is used to create a request with the desired parameters.
+     * <p>
+     * <b>Note</b>: A valid request builder must define an item.
+     */
+    public static final class Builder<T> {
+        private T item;
+        private Boolean ignoreNulls;
+        private Expression conditionExpression;
+        private String returnValuesOnConditionCheckFailure;
+
+        private Builder() {
+        }
+
+        /**
+         * Sets if the update operation should ignore attributes with null values. By default, the value is false.
+         * <p>
+         * If set to true, any null values in the Java object will be ignored and not be updated on the persisted
+         * record. This is commonly referred to as a 'partial update'.
+         * If set to false, null values in the Java object will cause those attributes to be removed from the persisted
+         * record on update.
+         *
+         * @param ignoreNulls the boolean value
+         * @return a builder of this type
+         */
+        public Builder<T> ignoreNulls(Boolean ignoreNulls) {
+            this.ignoreNulls = ignoreNulls;
+            return this;
+        }
+
+        /**
+         * Defines a logical expression on an item's attribute values which, if evaluating to true,
+         * will allow the update operation to succeed. If evaluating to false, the operation will not succeed.
+         * <p>
+         * See {@link Expression} for condition syntax and examples.
+         *
+         * @param conditionExpression a condition written as an {@link Expression}
+         * @return a builder of this type
+         */
+        public Builder<T> conditionExpression(Expression conditionExpression) {
+            this.conditionExpression = conditionExpression;
+            return this;
+        }
+
+        /**
+         * Sets the item to write to DynamoDb. Required.
+         *
+         * @param item the item to write
+         * @return a builder of this type
+         */
+        public Builder<T> item(T item) {
+            this.item = item;
+            return this;
+        }
+
+        /**
+         * Use <code>ReturnValuesOnConditionCheckFailure</code> to get the item attributes if the <code>ConditionCheck</code>
+         * condition fails. For <code>ReturnValuesOnConditionCheckFailure</code>, the valid values are: NONE and
+         * ALL_OLD.
+         *
+         * @param returnValuesOnConditionCheckFailure What values to return on condition check failure.
+         * @return a builder of this type
+         */
+        public Builder<T> returnValuesOnConditionCheckFailure(
+            ReturnValuesOnConditionCheckFailure returnValuesOnConditionCheckFailure) {
+            this.returnValuesOnConditionCheckFailure = returnValuesOnConditionCheckFailure == null ? null :
+                                                       returnValuesOnConditionCheckFailure.toString();
+            return this;
+        }
+
+        /**
+         * Use <code>ReturnValuesOnConditionCheckFailure</code> to get the item attributes if the <code>ConditionCheck</code>
+         * condition fails. For <code>ReturnValuesOnConditionCheckFailure</code>, the valid values are: NONE and
+         * ALL_OLD.
+         *
+         * @param returnValuesOnConditionCheckFailure What values to return on condition check failure.
+         * @return a builder of this type
+         */
+        public Builder<T> returnValuesOnConditionCheckFailure(String returnValuesOnConditionCheckFailure) {
+            this.returnValuesOnConditionCheckFailure = returnValuesOnConditionCheckFailure;
+            return this;
+        }
+
+        public TransactUpdateItemEnhancedRequest<T> build() {
+            return new TransactUpdateItemEnhancedRequest<>(this);
+        }
+    }
+}
