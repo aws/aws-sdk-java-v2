9e919119b5670a32c358fc778f61b147007c4b0c
Bennett Lynch <lyncbenn@amazon.com>
2021-11-19 22:45:59 -0800

    Enable BlockHound for integration tests and stability tests
    This PR enables [BlockHound](https://github.com/reactor/BlockHound)
    for the SDK integration test and stability tests. BlockHound can be used
    to detect blocking behavior in threads that must be treated as
    non-blocking, like Netty's EventLoop threads.
    
    If BlockHound detects an illegal blocking operation being performed, it
    will throw a BlockingOperationError. Doing so in an integration test
    would typically fail the integration test and allow us to investigate
    the behavior.
    
    This PR enables BlockHound by leveraging BlockHound's provided
    [BlockHoundTestExecutionListener](https://github
    .com/reactor/BlockHound/blob/2071ca8dbb06eba9ffa65596f327e3182d3b9730
    /junit-platform/src/main/java/reactor/blockhound/junit/platform
    /BlockHoundTestExecutionListener.java), which uses @AutoService to
    automatically register with JUnit 5's ServiceLoader support:
    
    https://junit.org/junit5/docs/current/user-guide/#launcher-api-listeners-custom

---
+++ test/stability-tests/src/it/java/software/amazon/awssdk/stability/tests/BlockHoundIntegrationTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.stability.tests;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import reactor.blockhound.BlockingOperationError;
+import software.amazon.awssdk.utils.Logger;
+
+public class BlockHoundIntegrationTest {
+    private static final Logger log = Logger.loggerFor(BlockHoundIntegrationTest.class);
+
+    AtomicReference<Throwable> throwableReference;
+    CountDownLatch latch;
+    EventLoopGroup bossGroup;
+    EventLoopGroup workerGroup;
+    Socket clientSocket;
+    Channel serverChannel;
+
+    @Before
+    public void setup() {
+        throwableReference = new AtomicReference<>();
+        latch = new CountDownLatch(1);
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
+    }
+
+    @After
+    public void teardown() throws Exception {
+        if (clientSocket != null) {
+            clientSocket.close();
+        }
+        if (serverChannel != null) {
+            serverChannel.close();
+        }
+        workerGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
+    }
+
+    @Test
+    public void testBlockHoundIntegration() throws Exception {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                 .channel(NioServerSocketChannel.class)
+                 .childHandler(new ChannelDuplexHandler() {
+                     @Override
+                     public void channelActive(ChannelHandlerContext ctx) {
+                         log.info(() -> "Preparing to sleep on the EventLoop to test if BlockHound is installed");
+                         try {
+                             Thread.sleep(1000);
+                             log.info(() -> "BlockHound does not appear to be successfully installed");
+                         } catch (Throwable t) {
+                             log.info(() -> "BlockHound is successfully installed", t);
+                             throwableReference.set(t);
+                         }
+                         latch.countDown();
+                         ctx.fireChannelActive();
+                     }
+                 });
+
+        int port = getUnusedPort();
+        serverChannel = bootstrap.bind(port).sync().channel();
+        clientSocket = new Socket("localhost", port);
+
+        latch.await(5, TimeUnit.SECONDS);
+        assertThat(throwableReference.get())
+            .withFailMessage("BlockHound does not appear to be successfully installed. "
+                             + "Ensure that BlockHoundTestExecutionListener is available on "
+                             + "the class path and correctly registered with JUnit: "
+                             + "https://github.com/reactor/BlockHound/blob/master/docs/supported_testing_frameworks.md")
+            .isInstanceOf(BlockingOperationError.class);
+    }
+
+    public static int getUnusedPort() throws IOException {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            socket.setReuseAddress(true);
+            return socket.getLocalPort();
+        }
+    }
+}
