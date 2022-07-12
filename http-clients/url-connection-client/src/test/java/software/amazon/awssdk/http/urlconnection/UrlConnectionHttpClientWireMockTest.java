cfacbd8d5248b9257c5e229c4caffb9c6910f69e
Zoe Wang <33073555+zoewangg@users.noreply.github.com>
2022-07-12 14:58:04 -0700

    Set Accept header for UrlConnectionHttpClient (#3294)

--- http-clients/url-connection-client/src/test/java/software/amazon/awssdk/http/urlconnection/UrlConnectionHttpClientWireMockTest.java
+++ http-clients/url-connection-client/src/test/java/software/amazon/awssdk/http/urlconnection/UrlConnectionHttpClientWireMockTest.java
@@ -14,17 +14,23 @@
  */
 package software.amazon.awssdk.http.urlconnection;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static software.amazon.awssdk.http.Header.ACCEPT;
 import static software.amazon.awssdk.http.SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES;
 
+import java.io.IOException;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
 import org.junit.After;
 import org.junit.Test;
+import software.amazon.awssdk.http.HttpExecuteRequest;
+import software.amazon.awssdk.http.HttpExecuteResponse;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.SdkHttpClientTestSuite;
+import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
 import software.amazon.awssdk.utils.AttributeMap;
 
 public final class UrlConnectionHttpClientWireMockTest extends SdkHttpClientTestSuite {
@@ -49,6 +55,43 @@ public final class UrlConnectionHttpClientWireMockTest extends SdkHttpClientTest
     public void connectionsAreNotReusedOn5xxErrors() {
         // We cannot support this because the URL connection client doesn't allow us to disable connection reuse
     }
+
+    // https://bugs.openjdk.org/browse/JDK-8163921
+    @Test
+    public void noAcceptHeader_shouldSet() throws IOException {
+        SdkHttpClient client = createSdkHttpClient();
+
+        stubForMockRequest(200);
+
+        SdkHttpFullRequest req = mockSdkRequest("http://localhost:" + mockServer.port(), SdkHttpMethod.POST);
+        HttpExecuteResponse rsp = client.prepareRequest(HttpExecuteRequest.builder()
+                                                                          .request(req)
+                                                                          .contentStreamProvider(req.contentStreamProvider()
+                                                                                                    .orElse(null))
+                                                                          .build())
+                                        .call();
+
+        mockServer.verify(postRequestedFor(urlPathEqualTo("/")).withHeader(ACCEPT, equalTo("*/*")));
+    }
+
+    @Test
+    public void hasAcceptHeader_shouldNotOverride() throws IOException {
+        SdkHttpClient client = createSdkHttpClient();
+
+        stubForMockRequest(200);
+
+        SdkHttpFullRequest req = mockSdkRequest("http://localhost:" + mockServer.port(), SdkHttpMethod.POST);
+        req = req.toBuilder().putHeader(ACCEPT, "text/html").build();
+        HttpExecuteResponse rsp = client.prepareRequest(HttpExecuteRequest.builder()
+                                                                          .request(req)
+                                                                          .contentStreamProvider(req.contentStreamProvider()
+                                                                                                    .orElse(null))
+                                                                          .build())
+                                        .call();
+
+        mockServer.verify(postRequestedFor(urlPathEqualTo("/")).withHeader(ACCEPT, equalTo("text/html")));
+    }
+
 
     @After
     public void reset() {
