e2250def47acc028a39e4b8ca6e93fb2521e14a6
Matthew Miller <millem@amazon.com>
2022-08-29 16:21:47 -0700

    Pass endpoint override to S3 CRT Async HTTP client. This is the first part of fixing endpoint overrides in transfer manager. The next part will be in the CRT. (#3384)

--- services/s3/src/main/java/software/amazon/awssdk/services/s3/internal/crt/S3CrtAsyncHttpClient.java
+++ services/s3/src/main/java/software/amazon/awssdk/services/s3/internal/crt/S3CrtAsyncHttpClient.java
@@ -16,6 +16,7 @@
 package software.amazon.awssdk.services.s3.internal.crt;
 
 import static software.amazon.awssdk.services.s3.internal.crt.S3InternalSdkHttpExecutionAttribute.OPERATION_NAME;
+import static software.amazon.awssdk.utils.FunctionalUtils.invokeSafely;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -91,13 +92,18 @@ public final class S3CrtAsyncHttpClient implements SdkAsyncHttpClient {
         S3MetaRequestOptions requestOptions = new S3MetaRequestOptions()
             .withHttpRequest(httpRequest)
             .withMetaRequestType(requestType)
-            .withResponseHandler(responseHandler);
+            .withResponseHandler(responseHandler)
+            .withEndpoint(getEndpoint(uri));
 
         try (S3MetaRequest s3MetaRequest = crtS3Client.makeMetaRequest(requestOptions)) {
             closeResourcesWhenComplete(executeFuture, s3MetaRequest, responseHandler);
         }
 
         return executeFuture;
+    }
+
+    private static URI getEndpoint(URI uri) {
+        return invokeSafely(() -> new URI(uri.getScheme(), null, uri.getHost(), uri.getPort(), null, null, null));
     }
 
     @Override
