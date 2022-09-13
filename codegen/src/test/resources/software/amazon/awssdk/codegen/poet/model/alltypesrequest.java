1cbbbab7d6cef0245e02a0d9a0677dc479953888
Zoe Wang <33073555+zoewangg@users.noreply.github.com>
2022-09-13 15:02:58 -0700

    Fix javadoc to make it build with JDK 17 (#3415)
    * Fix javadoc to make it work with JDK17
    
    * Fix build
    
    * Skip Javadoc generation for tests profiles and service modules
    
    * Fix source
    
    * Update buildspec
    
    * Fix provide feedback button
    
    * Exclude test modules
    
    * Fix exclusion
    
    * Fix buildspec formatting

--- codegen/src/test/resources/software/amazon/awssdk/codegen/poet/model/alltypesrequest.java
+++ codegen/src/test/resources/software/amazon/awssdk/codegen/poet/model/alltypesrequest.java
@@ -1748,16 +1748,21 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
         /**
          * Sets the value of the ListOfStructs property for this object.
          *
-         * This is a convenience method that creates an instance of the {@link List<SimpleStruct>.Builder} avoiding the
-         * need to create one manually via {@link List<SimpleStruct>#builder()}.
+         * This is a convenience method that creates an instance of the
+         * {@link software.amazon.awssdk.services.jsonprotocoltests.model.SimpleStruct.Builder} avoiding the need to
+         * create one manually via
+         * {@link software.amazon.awssdk.services.jsonprotocoltests.model.SimpleStruct#builder()}.
          *
-         * When the {@link Consumer} completes, {@link List<SimpleStruct>.Builder#build()} is called immediately and its
-         * result is passed to {@link #listOfStructs(List<SimpleStruct>)}.
+         * <p>
+         * When the {@link Consumer} completes,
+         * {@link software.amazon.awssdk.services.jsonprotocoltests.model.SimpleStruct.Builder#build()} is called
+         * immediately and its result is passed to {@link #listOfStructs(List<SimpleStruct>)}.
          *
          * @param listOfStructs
-         *        a consumer that will call methods on {@link List<SimpleStruct>.Builder}
+         *        a consumer that will call methods on
+         *        {@link software.amazon.awssdk.services.jsonprotocoltests.model.SimpleStruct.Builder}
          * @return Returns a reference to this object so that method calls can be chained together.
-         * @see #listOfStructs(List<SimpleStruct>)
+         * @see #listOfStructs(java.util.Collection<SimpleStruct>)
          */
         Builder listOfStructs(Consumer<SimpleStruct.Builder>... listOfStructs);
 
@@ -1956,6 +1961,7 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
          * This is a convenience method that creates an instance of the {@link StructWithTimestamp.Builder} avoiding the
          * need to create one manually via {@link StructWithTimestamp#builder()}.
          *
+         * <p>
          * When the {@link Consumer} completes, {@link StructWithTimestamp.Builder#build()} is called immediately and
          * its result is passed to {@link #structWithNestedTimestampMember(StructWithTimestamp)}.
          *
@@ -1993,6 +1999,7 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
          * This is a convenience method that creates an instance of the {@link StructWithNestedBlobType.Builder}
          * avoiding the need to create one manually via {@link StructWithNestedBlobType#builder()}.
          *
+         * <p>
          * When the {@link Consumer} completes, {@link StructWithNestedBlobType.Builder#build()} is called immediately
          * and its result is passed to {@link #structWithNestedBlob(StructWithNestedBlobType)}.
          *
@@ -2047,6 +2054,7 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
          * This is a convenience method that creates an instance of the {@link RecursiveStructType.Builder} avoiding the
          * need to create one manually via {@link RecursiveStructType#builder()}.
          *
+         * <p>
          * When the {@link Consumer} completes, {@link RecursiveStructType.Builder#build()} is called immediately and
          * its result is passed to {@link #recursiveStruct(RecursiveStructType)}.
          *
@@ -2074,6 +2082,7 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
          * This is a convenience method that creates an instance of the {@link BaseType.Builder} avoiding the need to
          * create one manually via {@link BaseType#builder()}.
          *
+         * <p>
          * When the {@link Consumer} completes, {@link BaseType.Builder#build()} is called immediately and its result is
          * passed to {@link #polymorphicTypeWithSubTypes(BaseType)}.
          *
@@ -2101,6 +2110,7 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
          * This is a convenience method that creates an instance of the {@link SubTypeOne.Builder} avoiding the need to
          * create one manually via {@link SubTypeOne#builder()}.
          *
+         * <p>
          * When the {@link Consumer} completes, {@link SubTypeOne.Builder#build()} is called immediately and its result
          * is passed to {@link #polymorphicTypeWithoutSubTypes(SubTypeOne)}.
          *
@@ -2150,6 +2160,7 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
          * This is a convenience method that creates an instance of the {@link Underscore_Name_Type.Builder} avoiding
          * the need to create one manually via {@link Underscore_Name_Type#builder()}.
          *
+         * <p>
          * When the {@link Consumer} completes, {@link Underscore_Name_Type.Builder#build()} is called immediately and
          * its result is passed to {@link #underscore_Name_Type(Underscore_Name_Type)}.
          *
@@ -2186,6 +2197,7 @@ public final class AllTypesRequest extends JsonProtocolTestsRequest implements
          * This is a convenience method that creates an instance of the {@link AllTypesUnionStructure.Builder} avoiding
          * the need to create one manually via {@link AllTypesUnionStructure#builder()}.
          *
+         * <p>
          * When the {@link Consumer} completes, {@link AllTypesUnionStructure.Builder#build()} is called immediately and
          * its result is passed to {@link #allTypesUnionStructure(AllTypesUnionStructure)}.
          *
