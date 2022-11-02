b6f9c8705e46c653516d8a5c8b539bb69cfcb50d
Jonathan M. Henson <henso@amazon.com>
2022-11-01 19:49:34 -0700

    Added client metrics for the CRT HTTP SPI implementation. (#3482)
    * Added client metrics for the CRT HTTP SPI implementation.
    
    * fix build error in crt request executor with correct import statement.
    
    * Added concurrency acquire duration metrics to crt http SPI implementation.
    
    * simplify conditional.
    
    * Made http exceptions io exceptions so they can be retried.
    
    * Updated the exception mapping to be in the correct adapter.

--- http-clients/aws-crt-client/src/main/java/software/amazon/awssdk/http/crt/internal/CrtRequestExecutor.java
+++ http-clients/aws-crt-client/src/main/java/software/amazon/awssdk/http/crt/internal/CrtRequestExecutor.java
@@ -15,16 +15,27 @@
 
 package software.amazon.awssdk.http.crt.internal;
 
+import static software.amazon.awssdk.http.HttpMetric.AVAILABLE_CONCURRENCY;
+import static software.amazon.awssdk.http.HttpMetric.CONCURRENCY_ACQUIRE_DURATION;
+import static software.amazon.awssdk.http.HttpMetric.LEASED_CONCURRENCY;
+import static software.amazon.awssdk.http.HttpMetric.MAX_CONCURRENCY;
+import static software.amazon.awssdk.http.HttpMetric.PENDING_CONCURRENCY_ACQUIRES;
+
 import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import software.amazon.awssdk.annotations.SdkInternalApi;
 import software.amazon.awssdk.crt.CrtRuntimeException;
 import software.amazon.awssdk.crt.http.HttpClientConnection;
+import software.amazon.awssdk.crt.http.HttpClientConnectionManager;
+import software.amazon.awssdk.crt.http.HttpManagerMetrics;
 import software.amazon.awssdk.crt.http.HttpRequest;
 import software.amazon.awssdk.crt.http.HttpStreamResponseHandler;
 import software.amazon.awssdk.http.SdkCancellationException;
 import software.amazon.awssdk.http.async.AsyncExecuteRequest;
 import software.amazon.awssdk.http.async.SdkAsyncHttpResponseHandler;
+import software.amazon.awssdk.metrics.MetricCollector;
+import software.amazon.awssdk.metrics.NoOpMetricCollector;
 import software.amazon.awssdk.http.crt.internal.request.CrtRequestAdapter;
 import software.amazon.awssdk.http.crt.internal.response.CrtResponseAdapter;
 import software.amazon.awssdk.utils.Logger;
@@ -34,20 +45,43 @@ public final class CrtRequestExecutor {
     private static final Logger log = Logger.loggerFor(CrtRequestExecutor.class);
 
     public CompletableFuture<Void> execute(CrtRequestContext executionContext) {
+        // go ahead and get a reference to the metricCollector since multiple futures will
+        // need it regardless.
+        MetricCollector metricCollector = executionContext.metricCollector();
+        boolean shouldPublishMetrics = metricCollector != null && !(metricCollector instanceof NoOpMetricCollector);
+
+        long acquireStartTime = 0;
+
+        if (shouldPublishMetrics) {
+            // go ahead and get acquireStartTime for the concurrency timer as early as possible,
+            // so it's as accurate as possible, but only do it in a branch since clock_gettime()
+            // results in a full sys call barrier (multiple mutexes and a hw interrupt).
+            acquireStartTime = System.nanoTime();
+        }
+
         CompletableFuture<Void> requestFuture = createExecutionFuture(executionContext.sdkRequest());
 
         // When a Connection is ready from the Connection Pool, schedule the Request on the connection
         CompletableFuture<HttpClientConnection> httpClientConnectionCompletableFuture =
             executionContext.crtConnPool().acquireConnection();
 
+        long finalAcquireStartTime = acquireStartTime;
+
         httpClientConnectionCompletableFuture.whenComplete((crtConn, throwable) -> {
             AsyncExecuteRequest asyncRequest = executionContext.sdkRequest();
+
             // If we didn't get a connection for some reason, fail the request
             if (throwable != null) {
                 reportFailure(new IOException("An exception occurred when acquiring a connection", throwable),
                               requestFuture,
                               asyncRequest.responseHandler());
                 return;
+            }
+
+            if (shouldPublishMetrics) {
+                long acquireCompletionTime = System.nanoTime();
+                Duration acquireTimeTaken = Duration.ofNanos(acquireCompletionTime - finalAcquireStartTime);
+                metricCollector.reportMetric(CONCURRENCY_ACQUIRE_DURATION, acquireTimeTaken);
             }
 
             HttpRequest crtRequest = CrtRequestAdapter.toCrtRequest(executionContext);
@@ -64,6 +98,18 @@ public final class CrtRequestExecutor {
             }
         });
 
+        requestFuture.whenComplete((obj, err) -> {
+            if (shouldPublishMetrics) {
+                HttpClientConnectionManager connManager = executionContext.crtConnPool();
+                HttpManagerMetrics managerMetrics = connManager.getManagerMetrics();
+                // currently this executor only handles HTTP 1.1. Until H2 is added, the max concurrency settings are 1:1 with TCP
+                // connections. When H2 is added, this code needs to be updated to handle stream multiplexing
+                metricCollector.reportMetric(MAX_CONCURRENCY, connManager.getMaxConnections());
+                metricCollector.reportMetric(AVAILABLE_CONCURRENCY, (int) managerMetrics.getAvailableConcurrency());
+                metricCollector.reportMetric(LEASED_CONCURRENCY, (int) managerMetrics.getLeasedConcurrency());
+                metricCollector.reportMetric(PENDING_CONCURRENCY_ACQUIRES, (int) managerMetrics.getPendingConcurrencyAcquires());
+            }
+        });
         return requestFuture;
     }
 
@@ -91,7 +137,7 @@ public final class CrtRequestExecutor {
     /**
      * Notify the provided response handler and future of the failure.
      */
-    private void reportFailure(Throwable cause,
+    private void reportFailure(IOException cause,
                                CompletableFuture<Void> executeFuture,
                                SdkAsyncHttpResponseHandler responseHandler) {
         try {
