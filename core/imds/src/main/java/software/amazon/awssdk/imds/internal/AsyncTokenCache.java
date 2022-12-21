021ced042f483e77706180f3fefc30b2d4fb38bc
Olivier Lepage-Applin <olapplin@amazon.com>
2022-12-21 11:52:24 -0500

    use list of waiting future in  AsyncTokeCache

--- core/imds/src/main/java/software/amazon/awssdk/imds/internal/AsyncTokenCache.java
+++ core/imds/src/main/java/software/amazon/awssdk/imds/internal/AsyncTokenCache.java
@@ -15,10 +15,10 @@
 
 package software.amazon.awssdk.imds.internal;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 import software.amazon.awssdk.annotations.SdkInternalApi;
 import software.amazon.awssdk.utils.CompletableFutureUtils;
@@ -26,55 +26,96 @@ import software.amazon.awssdk.utils.CompletableFutureUtils;
 @SdkInternalApi
 final class AsyncTokenCache implements Supplier<CompletableFuture<Token>> {
 
+    /**
+     * The currently cached value.
+     * Only modified through synchronized block, under the refreshLock.
+     */
     private volatile Token cachedToken;
-    private final Supplier<CompletableFuture<Token>> supplier;
-    private final Object refreshLock = new Object();
 
-    // TODO [DRAFT] add the ability to pass in the execution service
-    private final ExecutorService executorService = ForkJoinPool.commonPool();
+    /**
+     * The asynchronous operation that is used to refresh the token.
+     */
+    private final Supplier<CompletableFuture<Token>> supplier;
+
+    /**
+     * The collection of future that are waiting for the refresh call to complete. If a call to {@link AsyncTokenCache#get()}
+     * is made while the token request is happening, a future will be added to this collection. All future in this collection
+     * are completed once the token request is done.
+     */
+    private Collection<CompletableFuture<Token>> waitingFutures;
+
+    /**
+     * Indicates if the token refresh request is currently running or not.
+     */
+    private final AtomicBoolean refreshRunning;
+
+    private final Object refreshLock = new Object();
 
     AsyncTokenCache(Supplier<CompletableFuture<Token>> supplier) {
         this.supplier = supplier;
+        this.waitingFutures = new ArrayList<>();
+        this.refreshRunning = new AtomicBoolean(false);
     }
 
     /**
      * @return an already completed future if the cached value is still fresh. If the cached value was stale, the returned future
      * that will be completed once the refresh has been completed.
+     * In the case where multiple call to <pre>get</pre> are made while the token is expired, all CompletableFuture returned
+     * will be
+     * completed once the
      */
     @Override
     public CompletableFuture<Token> get() {
-        CompletableFuture<Token> returnFuture = new CompletableFuture<>();
-        CompletableFuture.runAsync(() -> refreshIfNeededAndComplete(returnFuture), executorService);
-        return returnFuture;
+        Token currentValue = cachedToken;
+        if (!needsRefresh(currentValue)) {
+            return CompletableFuture.completedFuture(currentValue);
+        }
+        synchronized (refreshLock) { // Make sure the value wasn't refreshed while we were waiting for the lock.
+            currentValue = cachedToken;
+            if (!needsRefresh(currentValue)) {
+                return CompletableFuture.completedFuture(currentValue);
+            }
+            CompletableFuture<Token> result = new CompletableFuture<>();
+            waitingFutures.add(result);
+            if (!refreshRunning.get()) {
+                CompletableFuture<Token> tokenRequest = startRefresh();
+                CompletableFutureUtils.forwardExceptionTo(result, tokenRequest);
+            }
+            return result;
+        }
     }
 
-    private void refreshIfNeededAndComplete(CompletableFuture<Token> parentFuture) {
-        if (needsRefresh()) {
+    private CompletableFuture<Token> startRefresh() {
+        CompletableFuture<Token> tokenRequest = supplier.get();
+        refreshRunning.set(true); // After supplier.get(), in case that throws an exception
+        tokenRequest.whenComplete((token, throwable) -> {
+            Collection<CompletableFuture<Token>> toComplete;
             synchronized (refreshLock) {
-                // the value might have been refreshed while waiting on the lock,
-                // if so, refresh it only if it has already expired agan.
-                if (needsRefresh()) {
-                    try {
-                        CompletableFuture<Token> tokenFuture = supplier.get();
-                        CompletableFutureUtils.forwardExceptionTo(parentFuture, tokenFuture);
-                        cachedToken = tokenFuture.get();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        parentFuture.completeExceptionally(e);
-                    } catch (ExecutionException e) {
-                        Throwable completionError = e.getCause() == null ? e : e.getCause();
-                        parentFuture.completeExceptionally(completionError);
-                    } catch (Exception e) {
-                        parentFuture.completeExceptionally(e);
-                    }
+                // Instead of completing the waiting future while holding the lock, we copy the list reference and
+                // release the lock before completing them. This is just in case someone (naughty) is doing something
+                // blocking on the complete calls. It's not good that they're doing that, but at least
+                // it won't block other threads trying to acquire the lock.
+                toComplete = waitingFutures;
+                waitingFutures = new ArrayList<>();
+                refreshRunning.set(false);
+                if (token != null) {
+                    cachedToken = token;
                 }
             }
-        }
-        parentFuture.complete(cachedToken);
+
+            toComplete.forEach(future -> {
+                if (throwable == null) {
+                    future.complete(token);
+                } else {
+                    future.completeExceptionally(throwable);
+                }
+            });
+        });
+        return tokenRequest;
     }
 
-    private boolean needsRefresh() {
-        return cachedToken == null || cachedToken.isExpired();
+    private boolean needsRefresh(Token token) {
+        return token == null || token.isExpired();
     }
 
 }
