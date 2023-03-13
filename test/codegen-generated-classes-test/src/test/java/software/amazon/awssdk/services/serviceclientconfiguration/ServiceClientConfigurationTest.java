507b1e31295ff49c2c80f7c9d5385ca75a718a9a
hdavidh <hdavidh@amazon.com>
2023-03-13 14:19:58 -0700

    Add tests

--- test/codegen-generated-classes-test/src/test/java/software/amazon/awssdk/services/serviceclientconfiguration/ServiceClientConfigurationTest.java
+++ test/codegen-generated-classes-test/src/test/java/software/amazon/awssdk/services/serviceclientconfiguration/ServiceClientConfigurationTest.java
@@ -19,20 +19,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
 import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.protocolrestxml.ProtocolRestXmlAsyncClient;
 import software.amazon.awssdk.services.protocolrestxml.ProtocolRestXmlClient;
 
 public class ServiceClientConfigurationTest {
 
     @Test
-    public void serviceClientConfiguration_shouldReturnCorrectRegion() {
+    public void syncClient_serviceClientConfiguration_shouldReturnCorrectRegion() {
         ProtocolRestXmlClient client = ProtocolRestXmlClient.builder()
                                                             .region(Region.ME_SOUTH_1)
-                                                            .credentialsProvider(StaticCredentialsProvider.create(
-                                                                AwsBasicCredentials.create("foo", "bar")))
                                                             .build();
 
         String region = client.serviceClientConfiguration().region();
@@ -40,7 +37,7 @@ public class ServiceClientConfigurationTest {
     }
 
     @Test
-    public void serviceClientConfiguration_shouldReturnCorrectClientOverrideConfigurationFields() {
+    public void syncClient_serviceClientConfiguration_shouldReturnCorrectClientOverrideConfigurationFields() {
         ClientOverrideConfiguration overrideConfiguration = ClientOverrideConfiguration.builder()
                                                                                        .apiCallAttemptTimeout(Duration.ofSeconds(30))
                                                                                        .apiCallTimeout(Duration.ofSeconds(90))
@@ -60,9 +57,53 @@ public class ServiceClientConfigurationTest {
     }
 
     @Test
-    public void serviceClientConfiguration_withoutOverrideConfiguration_shouldReturnEmptyFields () {
+    public void syncClient_serviceClientConfiguration_withoutOverrideConfiguration_shouldReturnEmptyFields () {
         ProtocolRestXmlClient client = ProtocolRestXmlClient.builder()
                                                             .build();
+
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().toString()).isEqualTo(
+            "ClientOverrideConfiguration(headers={}, executionInterceptors=[], advancedOptions={})");
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().apiCallAttemptTimeout()).isNotPresent();
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().apiCallTimeout()).isNotPresent();
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().retryPolicy()).isNotPresent();
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().defaultProfileFile()).isNotPresent();
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().metricPublishers()).isEmpty();
+    }
+
+    @Test
+    public void asyncClient_serviceClientConfiguration_shouldReturnCorrectRegion() {
+        ProtocolRestXmlAsyncClient client = ProtocolRestXmlAsyncClient.builder()
+                                                                      .region(Region.ME_SOUTH_1)
+                                                                      .build();
+
+        String region = client.serviceClientConfiguration().region();
+        assertThat(region).isEqualTo("me-south-1");
+    }
+
+    @Test
+    public void asyncClient_serviceClientConfiguration_shouldReturnCorrectClientOverrideConfigurationFields() {
+        ClientOverrideConfiguration overrideConfiguration = ClientOverrideConfiguration.builder()
+                                                                                       .apiCallAttemptTimeout(Duration.ofSeconds(30))
+                                                                                       .apiCallTimeout(Duration.ofSeconds(90))
+                                                                                       .retryPolicy(c -> c.numRetries(4))
+                                                                                       .build();
+
+        ProtocolRestXmlAsyncClient client = ProtocolRestXmlAsyncClient.builder()
+                                                                      .overrideConfiguration(overrideConfiguration)
+                                                                      .build();
+
+
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().apiCallAttemptTimeout().get()).isEqualTo(Duration.ofSeconds(30));
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().apiCallTimeout().get()).isEqualTo(Duration.ofSeconds(90));
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().retryPolicy().get().numRetries()).isEqualTo(4);
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().defaultProfileFile()).isNotPresent();
+        assertThat(client.serviceClientConfiguration().overrideConfiguration().metricPublishers()).isEmpty();
+    }
+
+    @Test
+    public void asyncClient_serviceClientConfiguration_withoutOverrideConfiguration_shouldReturnEmptyFields () {
+        ProtocolRestXmlAsyncClient client = ProtocolRestXmlAsyncClient.builder()
+                                                                      .build();
 
         assertThat(client.serviceClientConfiguration().overrideConfiguration().toString()).isEqualTo(
             "ClientOverrideConfiguration(headers={}, executionInterceptors=[], advancedOptions={})");
