05ef6343db282bad78ccab37d8ff12cecad6d1f5
David Ho <70000000+davidh44@users.noreply.github.com>
2023-03-28 09:10:04 -0700

    Fix serviceclientconfig build issues (#3864)
    * Make serviceClientConfiguration()  default and throw unsupported operation exception
    
    * Update client interface method
    
    * Temporarily disable Poet Client tests
    
    * remove unused imports
    
    * Fix build issue
    
    * Update codegen files and re-enable tests
    
    * remove unnecessary method implementation from test client

--- codegen/src/main/java/software/amazon/awssdk/codegen/poet/client/SyncClientInterface.java
+++ codegen/src/main/java/software/amazon/awssdk/codegen/poet/client/SyncClientInterface.java
@@ -17,7 +17,6 @@ package software.amazon.awssdk.codegen.poet.client;
 
 import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
-import static javax.lang.model.element.Modifier.ABSTRACT;
 import static javax.lang.model.element.Modifier.DEFAULT;
 import static javax.lang.model.element.Modifier.FINAL;
 import static javax.lang.model.element.Modifier.PUBLIC;
@@ -534,7 +533,8 @@ public class SyncClientInterface implements ClassSpec {
     protected MethodSpec serviceClientConfigMethod() {
         return MethodSpec.methodBuilder("serviceClientConfiguration")
                          .addAnnotation(Override.class)
-                         .addModifiers(PUBLIC, ABSTRACT)
+                         .addModifiers(PUBLIC, DEFAULT)
+                         .addStatement("throw new $T()", UnsupportedOperationException.class)
                          .returns(new PoetExtension(model).getServiceConfigClass())
                          .build();
     }
--- test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/enhanced/dynamodb/V2TestDynamoDbBaseClient.java
+++ test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/enhanced/dynamodb/V2TestDynamoDbBaseClient.java
@@ -17,7 +17,6 @@ package software.amazon.awssdk.benchmark.enhanced.dynamodb;
 
 import org.openjdk.jmh.infra.Blackhole;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbServiceClientConfiguration;
 
 abstract class V2TestDynamoDbBaseClient implements DynamoDbClient {
     protected final Blackhole bh;
@@ -34,10 +33,4 @@ abstract class V2TestDynamoDbBaseClient implements DynamoDbClient {
     @Override
     public void close() {
     }
-
-    @Override
-    public DynamoDbServiceClientConfiguration serviceClientConfiguration() {
-        return null;
-    }
-
 }
