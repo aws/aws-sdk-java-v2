e6e13a9f9f53fa693546cbf1a35fc1f08b1b657a
Jaykumar Gosar <gosar@amazon.com>
2023-05-10 12:26:00 -0700

    Minor updates to identity-spi
    * Made interfaces @SdkPublicApi
    * Added test for equals and hashCode

--- core/identity-spi/src/main/java/software/amazon/awssdk/identity/spi/IdentityProperty.java
+++ core/identity-spi/src/main/java/software/amazon/awssdk/identity/spi/IdentityProperty.java
@@ -17,7 +17,7 @@ package software.amazon.awssdk.identity.spi;
 
 import java.util.Objects;
 import software.amazon.awssdk.annotations.Immutable;
-import software.amazon.awssdk.annotations.SdkProtectedApi;
+import software.amazon.awssdk.annotations.SdkPublicApi;
 import software.amazon.awssdk.annotations.ThreadSafe;
 import software.amazon.awssdk.utils.ToString;
 import software.amazon.awssdk.utils.Validate;
@@ -26,7 +26,7 @@ import software.amazon.awssdk.utils.Validate;
  * A strongly-typed property for input to an {@link IdentityProvider}.
  * @param <T> The type of the attribute.
  */
-@SdkProtectedApi
+@SdkPublicApi
 @Immutable
 @ThreadSafe
 public final class IdentityProperty<T> {
