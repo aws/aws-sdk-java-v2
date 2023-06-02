7ee3bca6d158a4c6ab69fc6779ef664bc7c10f2e
Jaykumar Gosar <5666661+gosar@users.noreply.github.com>
2023-06-02 13:19:17 -0700

    Add AuthScheme sub-interfaces (#4041)
    * Add AuthScheme sub-interfaces
    
    * Add create method to AuthScheme sub-interfaces

---
+++ core/http-auth-event-stream/src/main/java/software/amazon/awssdk/http/auth/eventstream/AwsV4EventStreamHttpAuthScheme.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.http.auth.eventstream;
+
+import software.amazon.awssdk.annotations.SdkPublicApi;
+import software.amazon.awssdk.http.auth.spi.HttpAuthScheme;
+import software.amazon.awssdk.http.auth.spi.IdentityProviderConfiguration;
+import software.amazon.awssdk.identity.spi.AwsCredentialsIdentity;
+import software.amazon.awssdk.identity.spi.IdentityProvider;
+
+/**
+ * The <a href="https://smithy.io/2.0/aws/aws-auth.html#aws-auth-sigv4-trait">aws.auth#sigv4</a>
+ * auth scheme, which uses a {@link AwsCredentialsIdentity} and {@link AwsV4EventStreamHttpSigner}.
+ */
+@SdkPublicApi
+public interface AwsV4EventStreamHttpAuthScheme extends HttpAuthScheme<AwsCredentialsIdentity> {
+
+    /**
+     * Retrieve the scheme ID.
+     */
+    @Override
+    default String schemeId() {
+        return "aws.auth#sigv4";
+    }
+
+    /**
+     * Retrieve the {@link AwsCredentialsIdentity} based {@link IdentityProvider} associated with this authentication scheme.
+     */
+    @Override
+    default IdentityProvider<AwsCredentialsIdentity> identityProvider(IdentityProviderConfiguration providers) {
+        return providers.identityProvider(AwsCredentialsIdentity.class);
+    }
+
+    /**
+     * Retrieve the {@link AwsV4EventStreamHttpSigner} associated with this authentication scheme.
+     */
+    @Override
+    default AwsV4EventStreamHttpSigner signer() {
+        return AwsV4EventStreamHttpSigner.create();
+    }
+
+    /**
+     * Get a default implementation of a {@link AwsV4EventStreamHttpAuthScheme}
+     */
+    static AwsV4EventStreamHttpAuthScheme create() {
+        return new AwsV4EventStreamHttpAuthScheme() {
+        };
+    }
+}
