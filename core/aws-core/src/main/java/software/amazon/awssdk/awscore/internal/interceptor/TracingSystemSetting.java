1450a63e07fd5e2d92c86e526f2dcb7d328d13c3
Raviteja Duggirala <dug@amazon.com>
2023-06-06 16:36:14 -0700

    Fixing checkstyle error

--- core/aws-core/src/main/java/software/amazon/awssdk/awscore/internal/interceptor/TracingSystemSetting.java
+++ core/aws-core/src/main/java/software/amazon/awssdk/awscore/internal/interceptor/TracingSystemSetting.java
@@ -12,6 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 package software.amazon.awssdk.awscore.internal.interceptor;
 
 import software.amazon.awssdk.annotations.SdkInternalApi;
