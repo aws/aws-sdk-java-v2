60ccd300fe4dd4bfcaf3ea6e1b0f6d3bebea7c74
Zoe Wang <33073555+zoewangg@users.noreply.github.com>
2023-06-09 14:05:36 -0700

    Checkstyle fix

--- http-clients/apache-client/src/main/java/software/amazon/awssdk/http/apache/internal/conn/IdleConnectionReaper.java
+++ http-clients/apache-client/src/main/java/software/amazon/awssdk/http/apache/internal/conn/IdleConnectionReaper.java
@@ -19,7 +19,6 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 import java.util.WeakHashMap;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
