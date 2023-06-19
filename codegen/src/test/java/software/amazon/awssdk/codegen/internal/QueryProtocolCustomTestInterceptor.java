67f47d5be431228ef6036fa014020f7e469dda6c
hdavidh <hdavidh@amazon.com>
2023-06-19 14:20:09 -0700

    Add codegen tests

---
+++ codegen/src/test/java/software/amazon/awssdk/codegen/internal/QueryProtocolCustomTestInterceptor.java
@@ -0,0 +1,12 @@
+package software.amazon.awssdk.codegen.internal;
+
+import software.amazon.awssdk.annotations.SdkInternalApi;
+import software.amazon.awssdk.codegen.poet.builder.BuilderClassTest;
+
+/**
+ * Empty no-op test interceptor for query protocols to view generated code in test-query-sync-client-builder-class.java and
+ * test-query-async-client-builder-class.java and validate in {@link BuilderClassTest}.
+ */
+@SdkInternalApi
+public class QueryProtocolCustomTestInterceptor {
+}
