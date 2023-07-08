8c0abc2fbde797628a531da00df9c60f6923ec27
hdavidh <hdavidh@amazon.com>
2023-07-07 17:07:35 -0700

    CompressionAsyncRequestBodyTest

--- core/sdk-core/src/test/java/software/amazon/awssdk/core/internal/async/CompressionAsyncRequestBodyTest.java
+++ core/sdk-core/src/test/java/software/amazon/awssdk/core/internal/async/CompressionAsyncRequestBodyTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.awssdk.core.internal.async;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,7 +47,8 @@ import software.amazon.awssdk.http.async.SimpleSubscriber;
 public class CompressionAsyncRequestBodyTest {
     private static final Compressor compressor = new GzipCompressor();;
     public static final String CRLF = "\r\n";
-    private final static String testString = "JamboJamboJamboJamboJambo";
+    private final static String testString =
+        "RequestCompressionTest-RequestCompressionTest-RequestCompressionTest-RequestCompressionTest-RequestCompressionTest";
 
     private static byte[] compressedData = compressor.compress(testString.getBytes(StandardCharsets.UTF_8));
     private static String expectedTestString =
