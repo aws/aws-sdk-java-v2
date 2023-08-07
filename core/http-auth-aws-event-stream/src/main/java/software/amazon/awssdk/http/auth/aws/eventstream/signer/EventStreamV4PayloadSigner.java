dc3f312e9bf907cd3f8ddd7ac2b01c825ec6d129
Hayden Baker <haydebak@amazon.com>
2023-08-07 15:07:48 -0700

    Implement payload signing for aws event-streams

--- core/http-auth-aws-event-stream/src/main/java/software/amazon/awssdk/http/auth/aws/eventstream/signer/EventStreamV4PayloadSigner.java
+++ core/http-auth-aws-event-stream/src/main/java/software/amazon/awssdk/http/auth/aws/eventstream/signer/EventStreamV4PayloadSigner.java
@@ -13,27 +13,48 @@
  * permissions and limitations under the License.
  */
 
-package software.amazon.awssdk.http.auth.aws.internal.signer;
+package software.amazon.awssdk.http.auth.aws.eventstream.signer;
 
 import java.nio.ByteBuffer;
+import java.time.Clock;
 import org.reactivestreams.Publisher;
 import software.amazon.awssdk.annotations.SdkProtectedApi;
 import software.amazon.awssdk.http.ContentStreamProvider;
+import software.amazon.awssdk.http.auth.aws.eventstream.internal.SigV4DataFramePublisher;
+import software.amazon.awssdk.http.auth.aws.signer.CredentialScope;
 import software.amazon.awssdk.http.auth.aws.signer.V4Context;
 import software.amazon.awssdk.http.auth.aws.signer.V4PayloadSigner;
+import software.amazon.awssdk.identity.spi.AwsCredentialsIdentity;
 
 /**
- * A default implementation of a payload signer that is a no-op, since payloads are most commonly unsigned.
+ * An implementation which supports async signing of event-stream payloads.
  */
 @SdkProtectedApi
-public class DefaultV4PayloadSigner implements V4PayloadSigner {
+public class EventStreamV4PayloadSigner implements V4PayloadSigner {
+
+    private final AwsCredentialsIdentity credentials;
+    private final CredentialScope credentialScope;
+    private final Clock signingClock;
+
+    public EventStreamV4PayloadSigner(AwsCredentialsIdentity credentials, CredentialScope credentialScope, Clock signingClock) {
+        this.credentials = credentials;
+        this.credentialScope = credentialScope;
+        this.signingClock = signingClock;
+    }
+
     @Override
     public ContentStreamProvider sign(ContentStreamProvider payload, V4Context v4Context) {
-        return payload;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Publisher<ByteBuffer> sign(Publisher<ByteBuffer> payload, V4Context v4Context) {
-        return payload;
+        return new SigV4DataFramePublisher(
+            payload,
+            credentials,
+            credentialScope,
+            v4Context.getSignature(),
+            signingClock
+        );
     }
 }
