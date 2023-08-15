ba89f477aa0e3570a9c86b312c86ea243407d489
Dongie Agnir <dongie@amazon.com>
2023-08-15 14:47:56 -0700

    Fix test

--- test/codegen-generated-classes-test/src/test/java/software/amazon/awssdk/services/metrics/CoreMetricsTest.java
+++ test/codegen-generated-classes-test/src/test/java/software/amazon/awssdk/services/metrics/CoreMetricsTest.java
@@ -198,7 +198,7 @@ public class CoreMetricsTest {
             .isGreaterThanOrEqualTo(Duration.ZERO);
         assertThat(capturedCollection.metricValues(CoreMetric.RETRY_COUNT)).containsExactly(0);
         assertThat(capturedCollection.metricValues(CoreMetric.SERVICE_ENDPOINT).get(0)).isEqualTo(URI.create(
-            "https://customresponsemetadata.us-west-2.amazonaws.com"));
+            "https://protocolrestjson.amazonaws.com"));
 
         assertThat(capturedCollection.children()).hasSize(1);
         MetricCollection attemptCollection = capturedCollection.children().get(0);
