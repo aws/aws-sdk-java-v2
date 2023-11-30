97d6d11cf5bc035b2a675921cd6b57b79e9d4694
Olivier L Applin <olapplin@amazon.com>
2023-11-30 13:35:46 -0500

    Update core/sdk-core/src/main/java/software/amazon/awssdk/core/client/config/ClientOverrideConfiguration.java
    Co-authored-by: Manuel Sugawara <sugmanue@amazon.com>

--- core/sdk-core/src/main/java/software/amazon/awssdk/core/client/config/ClientOverrideConfiguration.java
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/client/config/ClientOverrideConfiguration.java
@@ -706,7 +706,7 @@ public final class ClientOverrideConfiguration
 
         @Override
         public Map<String, List<String>> headers() {
-            Map<String, List<String>> option = Validate.getOrDefault(config.option(ADDITIONAL_HTTP_HEADERS), HashMap::new);
+            Map<String, List<String>> option = Validate.getOrDefault(config.option(ADDITIONAL_HTTP_HEADERS), Collections::emptyMap);
             return CollectionUtils.unmodifiableMapOfLists(option);
         }
 
