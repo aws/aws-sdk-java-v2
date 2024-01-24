dae1b10e75c22d87c1e2f7ae87e918b6b8287d6c
Zoe Wang <33073555+zoewangg@users.noreply.github.com>
2024-01-23 19:15:41 -0800

    Generate upgrade-sdk-depenencies recipe

---
+++ migration-tool/src/main/resources/scripts/generate_upgrade_sdk_dependencies_recipe.py
@@ -0,0 +1,86 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License.
+#  A copy of the License is located at
+#
+#   http://aws.amazon.com/apache2.0
+#
+#  or in the "license" file accompanying this file. This file is distributed
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#  express or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+import os
+import re
+from scripts.utils import find_sdk_version
+
+MAPPING_FILE_NAME = 'upgrade-sdk-dependencies.yml'
+RESOURCES_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, "../resources"))))
+RECIPE_ROOT_DIR =os.path.join(
+    RESOURCES_ROOT_DIR,
+    'META-INF/rewrite'
+)
+SERVICE_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, "../../../../")))),
+    'services'
+)
+
+def load_all_service_modules():
+    service_mapping = {}
+    for s in [s for s in os.listdir(SERVICE_DIR) if os.path.isdir(os.path.join(SERVICE_DIR, s))]:
+         service_mapping[s] = find_v1_equivalent(s)
+    return service_mapping
+
+def find_v1_equivalent(s):
+    return "aws-java-sdk-" + s
+
+def write_recipe_yml_file(service_mapping):
+    filename = os.path.join(RECIPE_ROOT_DIR, MAPPING_FILE_NAME)
+
+    with open(filename, 'w') as f:
+        write_copy_right_header(f)
+        write_recipe_metadata(f)
+        for s in service_mapping:
+            write_recipe(f, s, service_mapping)
+    return filename
+
+def write_recipe_metadata(f):
+    f.write('''---
+type: specs.openrewrite.org/v1beta/recipe
+name: software.amazon.awssdk.UpgradeSdkDependencies
+displayName: Change Maven dependency groupId, artifactId and/or the version example
+recipeList:
+''')
+
+def write_recipe(f, s, service_mapping):
+    change_dependency_group_id_and_artifact_id = '''
+- org.openrewrite.maven.ChangeDependencyGroupIdAndArtifactId:
+    oldGroupId: com.amazonaws
+    oldArtifactId: {0}
+    newGroupId: software.amazon.awssdk
+    newArtifactId: {1}
+    newVersion: {2}'''
+    version = find_sdk_version()
+    f.write(change_dependency_group_id_and_artifact_id.format(service_mapping[s], s, version))
+
+
+def write_copy_right_header(f):
+    f.write('''#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://aws.amazon.com/apache2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+''')
+
+def generate_upgrade_sdk_dependencies_recipe():
+    service_mapping = load_all_service_modules()
+    write_recipe_yml_file(service_mapping)
---
+++ migration-tool/src/test/java/software/amazon/awssdk/recipe/UpgradeSdkDependenciesTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.recipe;
+
+import static org.openrewrite.maven.Assertions.pomXml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+public class UpgradeSdkDependenciesTest implements RewriteTest {
+
+    private static String sdkVersion;
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        sdkVersion = getVersion();
+    }
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        try (InputStream stream = getClass().getResourceAsStream("/META-INF/rewrite/upgrade-sdk-dependencies.yml")) {
+            spec.recipe(stream, "software.amazon.awssdk.UpgradeSdkDependencies");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getVersion() throws IOException {
+        Path root = Paths.get(".").normalize().toAbsolutePath();
+        Path pomFile = root.resolve("pom.xml");
+        Optional<String> versionString =
+            Files.readAllLines(pomFile)
+                 .stream().filter(l -> l.contains("<version>")).findFirst();
+
+        if (!versionString.isPresent()) {
+            throw new AssertionError("No version is found");
+        }
+
+        String string = versionString.get().trim();
+        String substring = string.substring(9, string.indexOf('/') - 1);
+        return substring;
+    }
+
+    @Test
+    void standardClient_shouldChangeDependencyGroupIdAndArtifactId() throws IOException {
+        getVersion();
+        String currentVersion = getVersion();
+        rewriteRun(
+            pomXml(
+                "                  <project>\n"
+                + "                      <groupId>com.test.app</groupId>\n"
+                + "                      <artifactId>my-app</artifactId>\n"
+                + "                      <version>1</version>\n"
+                + "                      <dependencies>\n"
+                + "                          <dependency>\n"
+                + "                              <groupId>com.amazonaws</groupId>\n"
+                + "                              <artifactId>aws-java-sdk-sqs</artifactId>\n"
+                + "                              <version>1.12.100</version>\n"
+                + "                          </dependency>\n"
+                + "                      </dependencies>\n"
+                + "                  </project>",
+                String.format("                  <project>\n"
+                              + "                      <groupId>com.test.app</groupId>\n"
+                              + "                      <artifactId>my-app</artifactId>\n"
+                              + "                      <version>1</version>\n"
+                              + "                      <dependencies>\n"
+                              + "                          <dependency>\n"
+                              + "                              <groupId>software.amazon.awssdk</groupId>\n"
+                              + "                              <artifactId>sqs</artifactId>\n"
+                              + "                              <version>%s</version>\n"
+                              + "                          </dependency>\n"
+                              + "                      </dependencies>\n"
+                              + "                  </project>", currentVersion)
+
+            )
+        );
+    }
+}
