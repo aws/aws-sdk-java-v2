f3d56d1a9e0bfc60de2d767ee685ec701a91fdd6
hdavidh <hdavidh@amazon.com>
2024-02-08 13:49:50 -0800

    update javadocs

--- services/s3/src/main/java/software/amazon/awssdk/services/s3/internal/multipart/MultipartS3AsyncClient.java
+++ services/s3/src/main/java/software/amazon/awssdk/services/s3/internal/multipart/MultipartS3AsyncClient.java
@@ -43,7 +43,7 @@ import software.amazon.awssdk.utils.Validate;
 
 /**
  * An {@link S3AsyncClient} that automatically converts PUT, COPY requests to their respective multipart call. CRC32 will be
- * enabled for the PUT and COPY requests.
+ * enabled for the PUT and COPY requests, unless the the checksum is specified or checksum validation is disabled.
  * Note: GET is not yet supported.
  *
  * @see MultipartConfiguration
