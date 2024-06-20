bb3b3cc74dd62339178abd7e78625008629609ff
Zoe Wang <33073555+zoewangg@users.noreply.github.com>
2024-06-20 15:55:12 -0700

    Change auth type getters to fluent getters

--- migration-tool/src/test/java/software/amazon/awssdk/migration/recipe/ChangeAuthTypesTest.java
+++ migration-tool/src/test/java/software/amazon/awssdk/migration/recipe/ChangeAuthTypesTest.java
@@ -75,20 +75,24 @@ public class ChangeAuthTypesTest implements RewriteTest {
     void defaultCredentialsProviderChain_getInstance_isRewritten() {
         rewriteRun(
             java(
-                "import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;\n"
+                "import com.amazonaws.auth.AWSCredentials;\n"
+                + "import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;\n"
                 + "\n"
                 + "public class Example {\n"
                 + "    public static void main(String[] args) {\n"
                 + "        DefaultAWSCredentialsProviderChain credentials = DefaultAWSCredentialsProviderChain.getInstance();\n"
+                + "        AWSCredentials awsCredentials = credentials.getCredentials();\n"
                 + "    }\n"
                 + "}\n",
-                "import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;\n"
+                "import software.amazon.awssdk.auth.credentials.AwsCredentials;\n"
+                + "import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;\n"
                 + "\n"
                 + "public class Example {\n"
                 + "    public static void main(String[] args) {\n"
                 + "        DefaultCredentialsProvider credentials = DefaultCredentialsProvider.create();\n"
+                + "        AwsCredentials awsCredentials = credentials.resolveCredentials();\n"
                 + "    }\n"
-                + "}\n"
+                + "}"
             )
         );
     }
@@ -98,20 +102,53 @@ public class ChangeAuthTypesTest implements RewriteTest {
     void instanceCredentialsProvider_getInstance_isRewritten() {
         rewriteRun(
             java(
-                "import com.amazonaws.auth.InstanceProfileCredentialsProvider;\n"
+                "import com.amazonaws.auth.AWSCredentials;\n"
+                + "import com.amazonaws.auth.InstanceProfileCredentialsProvider;\n"
                 + "\n"
                 + "public class Example {\n"
                 + "    public static void main(String[] args) {\n"
                 + "        InstanceProfileCredentialsProvider credentials = InstanceProfileCredentialsProvider.getInstance();\n"
+                + "        AWSCredentials awsCredentials = credentials.getCredentials();\n"
                 + "    }\n"
                 + "}\n",
-                "import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;\n"
+                "import software.amazon.awssdk.auth.credentials.AwsCredentials;\n"
+                + "import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;\n"
                 + "\n"
                 + "public class Example {\n"
                 + "    public static void main(String[] args) {\n"
                 + "        InstanceProfileCredentialsProvider credentials = InstanceProfileCredentialsProvider.create();\n"
+                + "        AwsCredentials awsCredentials = credentials.resolveCredentials();\n"
                 + "    }\n"
-                + "}\n"
+                + "}"
+            )
+        );
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_8})
+    void staticCredentials_getter_isRewritten() {
+        rewriteRun(
+            java("import com.amazonaws.auth.AWSCredentials;\n"
+                + "import com.amazonaws.auth.BasicSessionCredentials;\n"
+                + "\n"
+                + "public class Example {\n"
+                + "    public static void main(String[] args) {\n"
+                + "        BasicSessionCredentials credentials = new BasicSessionCredentials(\"foo\", \"bar\", \"123\");\n"
+                + "        String accessKeyId = credentials.getAWSAccessKeyId();\n"
+                + "        String secreteKey = credentials.getAWSSecretKey();\n"
+                + "        String sessionToken = credentials.getSessionToken();\n"
+                + "    }\n"
+                + "}\n",
+                "import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;\n"
+                + "\n"
+                + "public class Example {\n"
+                + "    public static void main(String[] args) {\n"
+                + "        AwsSessionCredentials credentials = new AwsSessionCredentials(\"foo\", \"bar\", \"123\");\n"
+                + "        String accessKeyId = credentials.getAWSAccessKeyId();\n"
+                + "        String secreteKey = credentials.getAWSSecretKey();\n"
+                + "        String sessionToken = credentials.sessionToken();\n"
+                + "    }\n"
+                + "}"
             )
         );
     }
