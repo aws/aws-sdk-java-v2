0bb54f871e5fc4bcbdbf032731a5101f36ec6905
John Viegas <joviegas@amazon.com>
2024-10-31 11:35:07 -0700

    Handled comment to make it private

--- core/checksums/src/main/java/software/amazon/awssdk/checksums/internal/CrcCloneOnMarkChecksum.java
+++ core/checksums/src/main/java/software/amazon/awssdk/checksums/internal/CrcCloneOnMarkChecksum.java
@@ -48,7 +48,7 @@ public final class CrcCloneOnMarkChecksum extends BaseCrcChecksum {
     }
 
     // Function to clone any Checksum that implements Cloneable
-    private static Checksum cloneIfCloneable(Checksum checksum) {
+    private Checksum cloneIfCloneable(Checksum checksum) {
         if (checksum instanceof Cloneable) {
             try {
                 Method cloneMethod = checksum.getClass().getMethod("clone");
