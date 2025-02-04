c6823a351b15ab06cd9489fee4d5c174475118db
Ran Vaknin <RanVaknin@users.noreply.github.com>
2025-02-04 15:01:47 -0800

    Fix checkstyle

--- services/cloudfront/src/main/java/software/amazon/awssdk/services/cloudfront/internal/utils/SigningUtils.java
+++ services/cloudfront/src/main/java/software/amazon/awssdk/services/cloudfront/internal/utils/SigningUtils.java
@@ -179,7 +179,7 @@ public final class SigningUtils {
         if (expirationDate == null) {
             throw SdkClientException.create("Expiration date must be provided to sign CloudFront URLs");
         }
-        if (resourceUrl == null){
+        if (resourceUrl == null) {
             throw SdkClientException.create("Resource URL must be provided to sign CloudFront URLs");
         }
         String policyResource = policyResourceUrl != null ? policyResourceUrl : resourceUrl;
