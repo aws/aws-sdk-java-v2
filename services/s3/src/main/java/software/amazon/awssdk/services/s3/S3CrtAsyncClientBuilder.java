57ec7c0ed19bec45ce6cc26965bb3931bdfcb91e
yibole <yibole@amazon.com>
2025-02-17 17:39:02 -0800

    added disableS3ExpressSessionAuth field

--- services/s3/src/main/java/software/amazon/awssdk/services/s3/S3CrtAsyncClientBuilder.java
+++ services/s3/src/main/java/software/amazon/awssdk/services/s3/S3CrtAsyncClientBuilder.java
@@ -352,6 +352,15 @@ public interface S3CrtAsyncClientBuilder extends SdkBuilder<S3CrtAsyncClientBuil
      */
     S3CrtAsyncClientBuilder futureCompletionExecutor(Executor futureCompletionExecutor);
 
+    /**
+     * Configure whether to disable this client's usage of Session Auth for S3Express buckets and reverts to using conventional
+     * SigV4 for those.
+     *
+     * @param disableS3ExpressSessionAuth whether S3ExpressSessionAuth should be disabled
+     * @return an instance of this builder
+     */
+    S3CrtAsyncClientBuilder disableS3ExpressSessionAuth(Boolean disableS3ExpressSessionAuth);
+
 
     @Override
     S3AsyncClient build();
