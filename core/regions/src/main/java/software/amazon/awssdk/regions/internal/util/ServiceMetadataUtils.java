c8d628c8932b69ebfeae0a9d516b9cb0195a7024
Olivier Lepage-Applin <olapplin@amazon.com>
2025-05-07 15:52:44 -0400

    checkstyle

--- core/regions/src/main/java/software/amazon/awssdk/regions/internal/util/ServiceMetadataUtils.java
+++ core/regions/src/main/java/software/amazon/awssdk/regions/internal/util/ServiceMetadataUtils.java
@@ -23,9 +23,9 @@ import software.amazon.awssdk.regions.PartitionMetadata;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.regions.ServiceEndpointKey;
 import software.amazon.awssdk.utils.Pair;
-import software.amazon.awssdk.utils.uri.SdkUri;
 import software.amazon.awssdk.utils.StringUtils;
 import software.amazon.awssdk.utils.Validate;
+import software.amazon.awssdk.utils.uri.SdkUri;
 
 @SdkInternalApi
 public class ServiceMetadataUtils {
@@ -39,7 +39,8 @@ public class ServiceMetadataUtils {
                                   String endpointPrefix,
                                   String region,
                                   String dnsSuffix) {
-        return SdkUri.getInstance().create(StringUtils.replaceEach(hostname, SEARCH_LIST, new String[] {endpointPrefix, region, dnsSuffix }));
+        return SdkUri.getInstance().create(
+            StringUtils.replaceEach(hostname, SEARCH_LIST, new String[] {endpointPrefix, region, dnsSuffix }));
     }
 
     public static Region signingRegion(ServiceEndpointKey key,
