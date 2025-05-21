b8dcec98901d55b378bf6fc7fe796f609ee7c8ba
Alex Woods <alexwoo@amazon.com>
2025-05-21 13:16:31 -0700

    Fix CompletableFuture hangs when RetryStrategy/MetricsCollector raise errors

--- core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/pipeline/stages/AsyncRetryableStage.java
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/http/pipeline/stages/AsyncRetryableStage.java
@@ -75,7 +75,11 @@ public final class AsyncRetryableStage<OutputT> implements RequestPipeline<SdkHt
 
         public CompletableFuture<Response<OutputT>> execute() {
             CompletableFuture<Response<OutputT>> future = new CompletableFuture<>();
-            attemptFirstExecute(future);
+            try {
+                attemptFirstExecute(future);
+            } catch (Throwable t) {
+                future.completeExceptionally(t);
+            }
             return future;
         }
 
@@ -126,6 +130,9 @@ public final class AsyncRetryableStage<OutputT> implements RequestPipeline<SdkHt
 
                 retryableStageHelper.recordAttemptSucceeded();
                 future.complete(response);
+            }).exceptionally(t -> {
+                future.completeExceptionally(t);
+                return null;
             });
         }
 
@@ -149,7 +156,11 @@ public final class AsyncRetryableStage<OutputT> implements RequestPipeline<SdkHt
 
         private void maybeRetryExecute(CompletableFuture<Response<OutputT>> future, Exception exception) {
             retryableStageHelper.setLastException(exception);
-            maybeAttemptExecute(future);
+            try {
+                maybeAttemptExecute(future);
+            } catch (Throwable t) {
+                future.completeExceptionally(t);
+            }
         }
     }
 }
