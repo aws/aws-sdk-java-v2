ec73eb697de04425cbebf3b83082ea2e27e7dc65
hdavidh <hdavidh@amazon.com>
2025-06-27 16:27:17 -0700

    Add model files and test classes

---
+++ test/protocol-tests/src/test/java/software/amazon/awssdk/protocol/tests/SmithyRpcV2CborQueryCompatibleProtocolTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.protocol.tests;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import software.amazon.awssdk.protocol.ProtocolTestSuiteLoader;
+import software.amazon.awssdk.protocol.model.TestCase;
+import software.amazon.awssdk.protocol.runners.ProtocolTestRunner;
+
+@RunWith(Parameterized.class)
+public class SmithyRpcV2CborQueryCompatibleProtocolTest extends ProtocolTestBase {
+
+    private static final ProtocolTestSuiteLoader TEST_SUITE_LOADER = new ProtocolTestSuiteLoader();
+    private static ProtocolTestRunner testRunner;
+
+    @Parameterized.Parameter
+    public TestCase testCase;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static List<TestCase> data() throws IOException {
+        return TEST_SUITE_LOADER.load("smithy-rpcv2-querycompatible-suite.json");
+    }
+
+    @BeforeClass
+    public static void setupFixture() {
+        testRunner = new ProtocolTestRunner("/models/query-compatible-rpcv2-2025-06-20-intermediate.json");
+    }
+
+    @Test
+    public void runProtocolTest() throws Exception {
+        testRunner.runTest(testCase);
+    }
+}
