25ac7aae6a00db2ec873c1ecb62556fa4bf2d96d
jencymaryjoseph <35571282+jencymaryjoseph@users.noreply.github.com>
2025-06-30 14:39:54 -0700

    Format PresignedUrlManager.java

--- services/s3/src/main/java/software/amazon/awssdk/services/s3/presignedurl/PresignedUrlManager.java
+++ services/s3/src/main/java/software/amazon/awssdk/services/s3/presignedurl/PresignedUrlManager.java
@@ -26,8 +26,7 @@ import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.presignedurl.model.PresignedUrlGetObjectRequest;
 
 /**
- * Interface for executing S3 operations using presigned URLs. This can be accessed using
- * {@link S3Client#presignedUrlManager()}.
+ * Interface for executing S3 operations using presigned URLs. This can be accessed using {@link S3Client#presignedUrlManager()}.
  */
 @SdkPublicApi
 public interface PresignedUrlManager {
@@ -36,9 +35,8 @@ public interface PresignedUrlManager {
      * Downloads an S3 object using a presigned URL.
      * </p>
      * <p>
-     * This operation uses a presigned URL that contains all necessary authentication information,
-     * eliminating the need for AWS credentials at request time. The presigned URL must be valid
-     * and not expired.
+     * This operation uses a presigned URL that contains all necessary authentication information, eliminating the need for AWS
+     * credentials at request time. The presigned URL must be valid and not expired.
      * </p>
      * <dl>
      * <dt>Range Requests</dt>
@@ -58,27 +56,24 @@ public interface PresignedUrlManager {
      * </dd>
      * </dl>
      *
-     * @param request
-     *        The presigned URL request containing the URL and optional range parameters
-     * @param responseTransformer
-     *        Transforms the response to the desired return type. See
-     *        {@link software.amazon.awssdk.core.sync.ResponseTransformer} for pre-built implementations
-     *        like downloading to a file or converting to bytes.
-     * @param <ReturnT>
-     *        The type of the transformed response
+     * @param request             The presigned URL request containing the URL and optional range parameters
+     * @param responseTransformer Transforms the response to the desired return type. See
+     *                            {@link software.amazon.awssdk.core.sync.ResponseTransformer} for pre-built implementations like
+     *                            downloading to a file or converting to bytes.
+     * @param <ReturnT>           The type of the transformed response
      * @return The transformed result of the ResponseTransformer
-     * @throws software.amazon.awssdk.services.s3.model.NoSuchKeyException
-     *         The specified object does not exist
-     * @throws software.amazon.awssdk.services.s3.model.InvalidObjectStateException
-     *         Object is archived and must be restored before retrieval
-     * @throws software.amazon.awssdk.core.exception.SdkClientException
-     *         If any client side error occurs such as network failures or invalid presigned URL
-     * @throws S3Exception
-     *         Base class for all S3 service exceptions. Unknown exceptions will be thrown as an instance of this type.
+     * @throws software.amazon.awssdk.services.s3.model.NoSuchKeyException          The specified object does not exist
+     * @throws software.amazon.awssdk.services.s3.model.InvalidObjectStateException Object is archived and must be restored before
+     *                                                                              retrieval
+     * @throws software.amazon.awssdk.core.exception.SdkClientException             If any client side error occurs such as
+     *                                                                              network failures or invalid presigned URL
+     * @throws S3Exception                                                          Base class for all S3 service exceptions.
+     *                                                                              Unknown exceptions will be thrown as an
+     *                                                                              instance of this type.
      */
     default <ReturnT> ReturnT getObject(PresignedUrlGetObjectRequest request,
-                                ResponseTransformer<GetObjectResponse, ReturnT> responseTransformer) throws NoSuchKeyException,
-                                                                                                            InvalidObjectStateException, SdkClientException, S3Exception {
+                                        ResponseTransformer<GetObjectResponse, ReturnT> responseTransformer) throws NoSuchKeyException,
+                                                                                                                    InvalidObjectStateException, SdkClientException, S3Exception {
         throw new UnsupportedOperationException();
     }
 
