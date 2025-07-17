065c036ea6e2d9ec452572675061eef8c7c3e074
Alex Woods <alexwoo@amazon.com>
2025-07-17 16:18:33 -0700

    Update validation message

--- codegen/src/main/java/software/amazon/awssdk/codegen/naming/DefaultNamingStrategy.java
+++ codegen/src/main/java/software/amazon/awssdk/codegen/naming/DefaultNamingStrategy.java
@@ -507,9 +507,8 @@ public class DefaultNamingStrategy implements NamingStrategy {
                     ValidationErrorSeverity.DANGER,
                     String.format(
                         "Encountered a name or identifier that the customer will see (%s in the %s) with an underscore. "
-                        + "This isn't idiomatic in Java. Please either remove the underscores or apply the "
-                        + "'underscoresInNameBehavior' customization for this service (Supported "
-                        + "'underscoresInNameBehavior' values: %s).", name, location, supportedBehaviors)
+                        + "This isn't idiomatic in Java. Please either remove the underscores",
+                        name, location, supportedBehaviors)
                 ));
             }
             if (behavior != UnderscoresInNameBehavior.ALLOW) {
