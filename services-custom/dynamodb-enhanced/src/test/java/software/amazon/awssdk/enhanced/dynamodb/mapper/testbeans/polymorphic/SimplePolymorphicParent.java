33405b23bd97a577508eddfccfa60bb83676f15c
Ana Satirbasa <Ana-Maria.Satirbasa@endava.com>
2025-07-17 10:10:57 +0300

    DynamoDB Enhanced Client Polymorphic Types Support

---
+++ services-custom/dynamodb-enhanced/src/test/java/software/amazon/awssdk/enhanced/dynamodb/mapper/testbeans/polymorphic/SimplePolymorphicParent.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.enhanced.dynamodb.mapper.testbeans.polymorphic;
+
+import java.util.Objects;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSubtypeDiscriminator;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSupertype;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSupertype.Subtype;
+
+@DynamoDbBean
+@DynamoDbSupertype( {
+    @Subtype(discriminatorValue = "one", subtypeClass = SimplePolymorphicChildOne.class),
+    @Subtype(discriminatorValue = "two", subtypeClass = SimplePolymorphicChildTwo.class)
+})
+public abstract class SimplePolymorphicParent {
+    String type;
+
+    @DynamoDbSubtypeDiscriminator
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SimplePolymorphicParent that = (SimplePolymorphicParent) o;
+        return Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(type);
+    }
+}
