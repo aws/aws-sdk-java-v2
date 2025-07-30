ccc2455a001a9c6f83d48ee603166ecd2f3a2e0c
John Viegas <joviegas@amazon.com>
2025-07-30 13:48:13 -0700

    Refactored test

--- services/dynamodb/src/test/java/software/amazon/awssdk/services/dynamodb/DynamoDbDefaultRetryFunctionalTest.java
+++ services/dynamodb/src/test/java/software/amazon/awssdk/services/dynamodb/DynamoDbDefaultRetryFunctionalTest.java
@@ -26,8 +26,9 @@ import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import java.net.URI;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.SdkSystemSetting;
@@ -41,20 +42,8 @@ import software.amazon.awssdk.testutils.EnvironmentVariableHelper;
 @WireMockTest
 class DynamoDbDefaultRetryFunctionalTest {
 
-    private EnvironmentVariableHelper environmentVariableHelper;
+    private final EnvironmentVariableHelper environmentVariableHelper = new EnvironmentVariableHelper();
     private DynamoDbClient dynamoDbClient;
-
-    @BeforeEach
-    void setup(WireMockRuntimeInfo wm) {
-        environmentVariableHelper = new EnvironmentVariableHelper();
-
-        dynamoDbClient = DynamoDbClient.builder()
-                                       .endpointOverride(URI.create(wm.getHttpBaseUrl()))
-                                       .credentialsProvider(StaticCredentialsProvider.create(
-                                           AwsBasicCredentials.create("test", "test")))
-                                       .region(Region.US_EAST_1)
-                                       .build();
-    }
 
     @AfterEach
     void tearDown() {
@@ -64,47 +53,49 @@ class DynamoDbDefaultRetryFunctionalTest {
         }
     }
 
-    @Test
-    void listTables_whenNoRetryPolicySet_shouldAttempt9Times(WireMockRuntimeInfo wm) {
+    @ParameterizedTest
+    @ValueSource(strings = {"adaptive", "legacy", "standard"})
+    void listTables_whenRetryModeSet_shouldAttempt9Times(String retryMode, WireMockRuntimeInfo wm) {
+        // Set the retry mode environment variable
+        environmentVariableHelper.set(SdkSystemSetting.AWS_RETRY_MODE.environmentVariable(), retryMode);
+
+        // Build the DynamoDB client here instead of setup so that environment variable options gets picked for each tests
+        dynamoDbClient = DynamoDbClient.builder()
+                                       .endpointOverride(URI.create(wm.getHttpBaseUrl()))
+                                       .credentialsProvider(StaticCredentialsProvider.create(
+                                           AwsBasicCredentials.create("akid", "skid")))
+                                       .region(Region.US_EAST_1)
+                                       .build();
+
         stubFor(post(anyUrl())
                     .willReturn(aResponse().withStatus(503)));
+
         assertThatExceptionOfType(DynamoDbException.class)
             .isThrownBy(() -> dynamoDbClient.listTables());
+
         int actualAttempts = wm.getWireMock().getAllServeEvents().size();
         assertThat(actualAttempts)
-            .as("Default retry mode should result in 9 total attempts (1 initial + 8 retries)")
+            .as("Retry mode '%s' should result in 9 total attempts (1 initial + 8 retries)", retryMode)
             .isEqualTo(9);
     }
 
     @Test
-    void listTables_whenRetryModeSetToStandard_shouldAttempt10Times(WireMockRuntimeInfo wm) {
-        environmentVariableHelper.set(SdkSystemSetting.AWS_RETRY_MODE.environmentVariable(), "standard");
-        dynamoDbClient.close();
+    void listTables_whenUsingDefaultRetryMode_shouldAttempt9Times(WireMockRuntimeInfo wm) {
         dynamoDbClient = DynamoDbClient.builder()
                                        .endpointOverride(URI.create(wm.getHttpBaseUrl()))
                                        .credentialsProvider(StaticCredentialsProvider.create(
-                                           AwsBasicCredentials.create("test", "test")))
+                                           AwsBasicCredentials.create("akid", "skid")))
                                        .region(Region.US_EAST_1)
                                        .build();
         stubFor(post(anyUrl())
                     .willReturn(aResponse().withStatus(503)));
         assertThatExceptionOfType(DynamoDbException.class)
             .isThrownBy(() -> dynamoDbClient.listTables());
-        int actualAttempts = wm.getWireMock().getAllServeEvents().size();
-        assertThat(actualAttempts)
-            .as("Standard retry mode should result in 9 total attempts (1 initial + 8 retries)")
-            .isEqualTo(9);
-    }
 
-    @Test
-    void listTables_whenUsingDefaultRetryMode_shouldAttempt9Times(WireMockRuntimeInfo wm) {
-        stubFor(post(anyUrl())
-                    .willReturn(aResponse().withStatus(503)));
-        assertThatExceptionOfType(DynamoDbException.class)
-            .isThrownBy(() -> dynamoDbClient.listTables());
         int actualAttempts = wm.getWireMock().getAllServeEvents().size();
         assertThat(actualAttempts)
             .as("Default retry mode should result in 9 total attempts (1 initial + 8 retries)")
             .isEqualTo(9);
     }
+
 }
