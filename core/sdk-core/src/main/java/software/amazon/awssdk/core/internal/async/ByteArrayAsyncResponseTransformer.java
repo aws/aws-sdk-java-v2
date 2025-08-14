32b529fc0a4923b4b4cb0dacc50156b55b8bac17
hdavidh <hdavidh@amazon.com>
2025-08-14 11:27:38 -0700

    Add comments

--- core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/async/ByteArrayAsyncResponseTransformer.java
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/internal/async/ByteArrayAsyncResponseTransformer.java
@@ -47,6 +47,8 @@ public final class ByteArrayAsyncResponseTransformer<ResponseT> implements
     @Override
     public CompletableFuture<ResponseBytes<ResponseT>> prepare() {
         cf = new CompletableFuture<>();
+        // Using fromByteArrayUnsafe() to avoid unnecessary extra copying of byte array. The data writing has completed and the
+        // byte array will not be further modified so this is safe
         return cf.thenApply(arr -> ResponseBytes.fromByteArrayUnsafe(response, arr));
     }
 
