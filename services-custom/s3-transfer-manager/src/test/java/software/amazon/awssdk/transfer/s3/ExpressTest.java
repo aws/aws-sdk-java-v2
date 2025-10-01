3263ded8d45b4cc3856eaad45194cc63233ee1c7
Olivier Lepage-Applin <olapplin@amazon.com>
2025-10-01 10:52:45 -0400

    heap space

--- services-custom/s3-transfer-manager/src/test/java/software/amazon/awssdk/transfer/s3/ExpressTest.java
+++ services-custom/s3-transfer-manager/src/test/java/software/amazon/awssdk/transfer/s3/ExpressTest.java
@@ -45,7 +45,7 @@ public class ExpressTest {
 
     int maxInflightDownloads = 1;
     String bucket = "hagrid-test-3--use2-az2--x-s3";
-    long bufferSize = 256 * 1024 * 1024;
+    long bufferSize = 5L * 1024 * 1024 * 1024;
     long partSize = 5L * 1024 * 1024 * 1024;
     int chunkSize = (int) (bufferSize / maxInflightDownloads);
 
