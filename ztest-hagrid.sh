6aaefe9695aa168cb9f7ad0d0c92d397bb5ca56e
Olivier Lepage-Applin <olapplin@amazon.com>
2025-10-03 11:29:09 -0400

    typo

--- ztest-hagrid.sh
+++ ztest-hagrid.sh
@@ -4,7 +4,7 @@ dl='getHagridFile'
 ul='uploadHagridFile'
 
 key='1TB'
-path="/mnt/ebs-volume//$key"
+path="/mnt/ebs-volume/$key"
 
 mvn test -pl :s3-transfer-manager \
     -Dtest=software.amazon.awssdk.transfer.s3.HagridTest#$ul \
