8fd45c6e7973053906e95791252bbf549b005856
Olivier Lepage-Applin <olapplin@amazon.com>
2025-10-10 10:46:17 -0400

    ul

--- ztest-hagrid.sh
+++ ztest-hagrid.sh
@@ -7,6 +7,6 @@ key='48TB'
 path="/mnt/raid0/$key"
 
 mvn test -pl :s3-transfer-manager \
-    -Dtest=software.amazon.awssdk.transfer.s3.HagridTest#$dl \
+    -Dtest=software.amazon.awssdk.transfer.s3.HagridTest#$ul \
     -Dtestpath=$path \
     -Dtestkey=$key
