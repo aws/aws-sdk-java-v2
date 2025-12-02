740b8638f0f26381340338ee20a1a380691dea5c
Zoe Wang <33073555+zoewangg@users.noreply.github.com>
2025-12-02 12:53:08 -0800

    Remove Lazy on identityProviders

--- core/identity-spi/src/main/java/software/amazon/awssdk/identity/spi/internal/DefaultIdentityProviders.java
+++ core/identity-spi/src/main/java/software/amazon/awssdk/identity/spi/internal/DefaultIdentityProviders.java
@@ -17,8 +17,11 @@ package software.amazon.awssdk.identity.spi.internal;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import software.amazon.awssdk.annotations.Immutable;
 import software.amazon.awssdk.annotations.SdkInternalApi;
 import software.amazon.awssdk.identity.spi.Identity;
@@ -35,24 +38,20 @@ import software.amazon.awssdk.utils.Validate;
 @Immutable
 @SdkInternalApi
 public final class DefaultIdentityProviders implements IdentityProviders {
-    /**
-     * TODO(sra-identity-auth): Currently, some customers assume we won't interact with the identity providers when we create
-     * the client. This isn't true - we need to call identityType. To TEMPORARILY work around those customer's tests failing,
-     * this is marked lazy. Once we fully migrate over to the SRA as the default code path, we should remove this lazy and
-     * ticket everyone in live who is making those bad assumptions.
-     */
-    private final Lazy<Map<Class<?>, IdentityProvider<?>>> identityProviders;
+    // /**
+    //  * TODO(sra-identity-auth): Currently, some customers assume we won't interact with the identity providers when we create
+    //  * the client. This isn't true - we need to call identityType. To TEMPORARILY work around those customer's tests failing,
+    //  * this is marked lazy. Once we fully migrate over to the SRA as the default code path, we should remove this lazy and
+    //  * ticket everyone in live who is making those bad assumptions.
+    //  */
+    private final Map<Class<?>, IdentityProvider<?>> identityProviders;
     private final List<IdentityProvider<?>> identityProvidersList;
 
     private DefaultIdentityProviders(BuilderImpl builder) {
         this.identityProvidersList = new ArrayList<>(builder.identityProviders);
-        this.identityProviders = new Lazy<>(() -> {
-            Map<Class<?>, IdentityProvider<?>> result = new HashMap<>();
-            for (IdentityProvider<?> identityProvider : identityProvidersList) {
-                result.put(identityProvider.identityType(), identityProvider);
-            }
-            return result;
-        });
+        this.identityProviders = identityProvidersList.stream().collect(
+            Collectors.toMap(identityProvider -> identityProvider.identityType(),
+                             Function.identity()));
     }
 
     public static Builder builder() {
@@ -61,7 +60,7 @@ public final class DefaultIdentityProviders implements IdentityProviders {
 
     @Override
     public <T extends Identity> IdentityProvider<T> identityProvider(Class<T> identityType) {
-        return (IdentityProvider<T>) identityProviders.getValue().get(identityType);
+        return (IdentityProvider<T>) identityProviders.get(identityType);
     }
 
     @Override
