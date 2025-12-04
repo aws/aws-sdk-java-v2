f7d027279ddaaaf21b53d983582031a6e9ed50eb
Zoe Wang <33073555+zoewangg@users.noreply.github.com>
2025-12-04 15:26:30 -0800

    Update codegen to automatically add chunked encoding signer property … (#6551)
    * Update codegen to automatically add chunked encoding signer property for streaming operations with http checksum trait
    
    * Address feedback
    
    * Remove unncessary s3 control interceptor that enables signing properties since they are handled in codegen now
    
    * Add changelog entry
    
    * Rename variable names and add comments to improve readability

---
+++ codegen/src/test/resources/software/amazon/awssdk/codegen/poet/auth/scheme/s3control-test-auth-scheme-default-provider.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package software.amazon.awssdk.services.s3control.auth.scheme.internal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import software.amazon.awssdk.annotations.Generated;
+import software.amazon.awssdk.annotations.SdkInternalApi;
+import software.amazon.awssdk.http.auth.aws.signer.AwsV4HttpSigner;
+import software.amazon.awssdk.http.auth.spi.scheme.AuthSchemeOption;
+import software.amazon.awssdk.services.s3control.auth.scheme.S3ControlAuthSchemeParams;
+import software.amazon.awssdk.services.s3control.auth.scheme.S3ControlAuthSchemeProvider;
+
+@Generated("software.amazon.awssdk:codegen")
+@SdkInternalApi
+public final class DefaultS3ControlAuthSchemeProvider implements S3ControlAuthSchemeProvider {
+    private static final DefaultS3ControlAuthSchemeProvider DEFAULT = new DefaultS3ControlAuthSchemeProvider();
+
+    private DefaultS3ControlAuthSchemeProvider() {
+    }
+
+    public static DefaultS3ControlAuthSchemeProvider create() {
+        return DEFAULT;
+    }
+
+    @Override
+    public List<AuthSchemeOption> resolveAuthScheme(S3ControlAuthSchemeParams params) {
+        List<AuthSchemeOption> options = new ArrayList<>();
+        options.add(AuthSchemeOption.builder().schemeId("aws.auth#sigv4")
+                                    .putSignerProperty(AwsV4HttpSigner.SERVICE_SIGNING_NAME, "s3")
+                                    .putSignerProperty(AwsV4HttpSigner.REGION_NAME, params.region().id())
+                                    .putSignerProperty(AwsV4HttpSigner.DOUBLE_URL_ENCODE, false)
+                                    .putSignerProperty(AwsV4HttpSigner.NORMALIZE_PATH, false).build());
+        return Collections.unmodifiableList(options);
+    }
+}
