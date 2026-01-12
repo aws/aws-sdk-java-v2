d6c80d22f7763598bd922eecd2c5e546670a8a4f
Dongie Agnir <dongie@amazon.com>
2026-01-12 10:53:42 -0800

    Reintroduce the protected API for backcompat

---
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/pagination/async/PaginatedItemsPublisher.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.core.pagination.async;
+
+import java.util.Iterator;
+import java.util.function.Function;
+import org.reactivestreams.Subscriber;
+import software.amazon.awssdk.annotations.SdkProtectedApi;
+import software.amazon.awssdk.core.async.SdkPublisher;
+import software.amazon.awssdk.core.internal.pagination.async.ItemsSubscription;
+
+/**
+ * A publisher to request for a stream of paginated items. The class can be used to request data for paginated items
+ * across multiple pages.
+ *
+ * @param <ResponseT> The type of a single response page
+ * @param <ItemT> The type of paginated member in a response page
+ */
+@SdkProtectedApi
+public final class PaginatedItemsPublisher<ResponseT, ItemT> implements SdkPublisher<ItemT> {
+
+    private final AsyncPageFetcher<ResponseT> nextPageFetcher;
+
+    private final Function<ResponseT, Iterator<ItemT>> getIteratorFunction;
+
+    private final boolean isLastPage;
+
+    private PaginatedItemsPublisher(BuilderImpl builder) {
+        this.nextPageFetcher = builder.nextPageFetcher;
+        this.getIteratorFunction = builder.iteratorFunction;
+        this.isLastPage = builder.isLastPage;
+    }
+
+    public static Builder builder() {
+        return new BuilderImpl();
+    }
+
+    @Override
+    public void subscribe(Subscriber<? super ItemT> subscriber) {
+        subscriber.onSubscribe(isLastPage ? new EmptySubscription(subscriber)
+                                          : ItemsSubscription.builder()
+                                                             .subscriber(subscriber)
+                                                             .nextPageFetcher(nextPageFetcher)
+                                                             .iteratorFunction(getIteratorFunction)
+                                                             .build());
+    }
+
+    public interface Builder {
+        Builder nextPageFetcher(AsyncPageFetcher nextPageFetcher);
+
+        Builder iteratorFunction(Function iteratorFunction);
+
+        Builder isLastPage(boolean isLastPage);
+
+        PaginatedItemsPublisher build();
+    }
+
+    private static final class BuilderImpl implements Builder {
+        private AsyncPageFetcher nextPageFetcher;
+        private Function iteratorFunction;
+        private boolean isLastPage;
+
+        @Override
+        public Builder nextPageFetcher(AsyncPageFetcher nextPageFetcher) {
+            this.nextPageFetcher = nextPageFetcher;
+            return this;
+        }
+
+        @Override
+        public Builder iteratorFunction(Function iteratorFunction) {
+            this.iteratorFunction = iteratorFunction;
+            return this;
+        }
+
+        @Override
+        public Builder isLastPage(boolean isLastPage) {
+            this.isLastPage = isLastPage;
+            return this;
+        }
+
+        @Override
+        public PaginatedItemsPublisher build() {
+            return new PaginatedItemsPublisher(this);
+        }
+    }
+}
---
+++ core/sdk-core/src/test/java/software/amazon/awssdk/core/pagination/async/PaginatedItemsPublisherTckTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.core.pagination.async;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.tck.PublisherVerification;
+import org.reactivestreams.tck.TestEnvironment;
+
+/**
+ * TCK verification test for {@link PaginatedItemsPublisher}.
+ */
+public class PaginatedItemsPublisherTckTest extends PublisherVerification<Long> {
+
+    public PaginatedItemsPublisherTckTest() {
+        super(new TestEnvironment());
+    }
+
+    @Override
+    public Publisher<Long> createPublisher(long l) {
+        Function<List<Long>, Iterator<Long>> getIterator = response -> response != null ? response.iterator()
+                                                                                        : Collections.emptyIterator();
+
+        return PaginatedItemsPublisher.builder()
+                                      .nextPageFetcher(new PageFetcher(l, 5))
+                                      .iteratorFunction(getIterator)
+                                      .isLastPage(false)
+                                      .build();
+    }
+
+    @Override
+    public Publisher<Long> createFailedPublisher() {
+        // It's not possible to initialize PaginatedItemsPublisher to a failed
+        // state since we can only reach a failed state if we fail to fulfill a
+        // request, e.g. because the service returned an error response.
+
+        // return null to skip related tests
+        return null;
+    }
+
+    /**
+     * Simple {@link AsyncPageFetcher} that returns lists of longs as pages.
+     */
+    private static class PageFetcher implements AsyncPageFetcher<List<Long>> {
+        private final long maxVal;
+        private final long step;
+
+        private PageFetcher(long maxVal, long step) {
+            this.maxVal = maxVal;
+            this.step = step;
+        }
+
+        @Override
+        public boolean hasNextPage(List<Long> oldPage) {
+            return (lastElement(oldPage)) < maxVal - 1;
+        }
+
+        @Override
+        public CompletableFuture<List<Long>> nextPage(List<Long> oldPage) {
+            long i = lastElement(oldPage) + 1;
+            long j = Math.min(i + step, maxVal);
+            List<Long> stream = LongStream.range(i, j).boxed().collect(Collectors.toList());
+            return CompletableFuture.completedFuture(stream);
+        }
+
+        private long lastElement(List<Long> s) {
+            // first page is always null
+            if (s == null) return -1;
+            return s.get(s.size() - 1);
+        }
+    }
+}
