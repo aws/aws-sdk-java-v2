dd51ae6dc076eaa589466352d67eefbb7e7463cc
John Viegas <70235430+joviegas@users.noreply.github.com>
2026-06-17 13:32:38 -0700

    feat(sdk-core): Add SdkWarmUpProvider SPI and CannedResponseHttpClient for CRaC auto-priming (#7042)
    * feat(sdk-core): add SdkWarmUpProvider SPI for CRaC auto-priming
    
    * Handled review comments
    
    * Add CannedResponseHttpClient for Crac/Auto priming feature

---
+++ core/sdk-core/src/main/java/software/amazon/awssdk/core/crac/SdkWarmUpProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.core.crac;
+
+import software.amazon.awssdk.annotations.SdkProtectedApi;
+import software.amazon.awssdk.annotations.ThreadSafe;
+
+/**
+ * Service Provider Interface for warming up an SDK service's request path before a Coordinated Restore at Checkpoint
+ * (CRaC) checkpoint. The SDK discovers implementations on the classpath with {@link java.util.ServiceLoader}. An
+ * implementation registers itself in the
+ * {@code META-INF/services/software.amazon.awssdk.core.crac.SdkWarmUpProvider} resource.
+ *
+ * <p>
+ * Implementations must be thread safe.
+ */
+@ThreadSafe
+@SdkProtectedApi
+public interface SdkWarmUpProvider {
+
+    /**
+     * Exercises the service's request path so the Just-In-Time compiled code is captured in a CRaC snapshot.
+     */
+    void warmUp();
+}
