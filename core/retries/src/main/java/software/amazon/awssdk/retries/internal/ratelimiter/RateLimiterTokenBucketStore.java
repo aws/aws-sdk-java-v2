lineaje-labs-gos/aws-aws-sdk-java-v2

Add acquireAsync
This commit replaces tryAcquire with acquireAsync to better support
async codepaths. acquireAsync returns a CompletableFuture that is
completed when a token is successfully acquired; unlike tryAcquire, the
caller is not expected to do any of their own waiting once future is
complete.

core/retries/src/main/java/software/amazon/awssdk/retries/internal/ratelimiter/RateLimiterTokenBucketStore.java
@@ -15,8 +15,10 @@
 
 package software.amazon.awssdk.retries.internal.ratelimiter;
 
+import java.util.concurrent.ScheduledExecutorService;
 import software.amazon.awssdk.annotations.SdkInternalApi;
 import software.amazon.awssdk.annotations.ToBuilderIgnoreField;
+import software.amazon.awssdk.utils.SdkAutoCloseable;
 import software.amazon.awssdk.utils.Validate;
 import software.amazon.awssdk.utils.builder.CopyableBuilder;
 import software.amazon.awssdk.utils.builder.ToCopyableBuilder;
@@ -27,17 +29,25 @@ import software.amazon.awssdk.utils.cache.lru.LruCache;
  */
 @SdkInternalApi
 public final class RateLimiterTokenBucketStore
-    implements ToCopyableBuilder<RateLimiterTokenBucketStore.Builder, RateLimiterTokenBucketStore> {
+    implements ToCopyableBuilder<RateLimiterTokenBucketStore.Builder, RateLimiterTokenBucketStore>, SdkAutoCloseable {
     private static final int MAX_ENTRIES = 128;
     private static final RateLimiterClock DEFAULT_CLOCK = new SystemClock();
     private final LruCache<String, RateLimiterTokenBucket> scopeToTokenBucket;
     private final RateLimiterClock clock;
+    private final ScheduledExecutorService scheduler;
 
     private RateLimiterTokenBucketStore(Builder builder) {
         this.clock = Validate.paramNotNull(builder.clock, "clock");
-        this.scopeToTokenBucket = LruCache.<String, RateLimiterTokenBucket>builder(x -> new RateLimiterTokenBucket(clock))
+        this.scheduler = Validate.paramNotNull(builder.scheduler, "scheduler");
+        this.scopeToTokenBucket = LruCache.<String, RateLimiterTokenBucket>builder(
+            x -> new RateLimiterTokenBucket(clock, scheduler))
                                           .maxSize(MAX_ENTRIES)
                                           .build();
+    }
+
+    @Override
+    public void close() {
+        scheduler.shutdownNow();
     }
 
     public RateLimiterTokenBucket tokenBucketForScope(String scope) {
@@ -56,6 +66,7 @@ public final class RateLimiterTokenBucketStore
 
     public static class Builder implements CopyableBuilder<Builder, RateLimiterTokenBucketStore> {
         private RateLimiterClock clock;
+        private ScheduledExecutorService scheduler;
 
         Builder() {
             this.clock = DEFAULT_CLOCK;
@@ -67,6 +78,11 @@ public final class RateLimiterTokenBucketStore
 
         public Builder clock(RateLimiterClock clock) {
             this.clock = clock;
+            return this;
+        }
+
+        public Builder scheduler(ScheduledExecutorService scheduler) {
+            this.scheduler = scheduler;
             return this;
         }
 

core/retries/src/test/java/software/amazon/awssdk/retries/internal/ratelimiter/RateLimiterTokenBucketStoreTest.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.retries.internal.ratelimiter;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.concurrent.ScheduledExecutorService;
+import org.junit.jupiter.api.Test;
+
+public class RateLimiterTokenBucketStoreTest {
+    @Test
+    void close_closesScheduler() {
+        ScheduledExecutorService mockScheduler = mock(ScheduledExecutorService.class);
+        RateLimiterTokenBucketStore store = RateLimiterTokenBucketStore.builder()
+                                                                       .clock(new SystemClock())
+                                                                       .scheduler(mockScheduler)
+                                                                       .build();
+
+        store.close();
+
+        verify(mockScheduler).shutdownNow();
+    }
+}

core/retries/src/test/java/software/amazon/awssdk/retries/internal/ratelimiter/RateLimiterTokenBucketTest.java
@@ -16,44 +16,132 @@
 package software.amazon.awssdk.retries.internal.ratelimiter;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.within;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class RateLimiterTokenBucketTest {
     private static MutableClock clock = null;
-    private static RateLimiterTokenBucket tokenBucket = null;
     private static final double EPSILON = 0.0001;
+    private static ScheduledExecutorService scheduler = null;
+    private static RateLimiterTokenBucket tokenBucket = null;
 
     @BeforeAll
     static void setup() {
         clock = new MutableClock();
-        tokenBucket = new RateLimiterTokenBucket(clock);
+        scheduler = mock(ScheduledExecutorService.class);
+        tokenBucket = new RateLimiterTokenBucket(clock, scheduler);
     }
 
-    @ParameterizedTest
-    @MethodSource("parameters")
-    void testCase(TestCase testCase) {
+    @Test
+    void acquireAsync_notifierNotRunning_schedules() {
+        ScheduledExecutorService testScheduler = mock(ScheduledExecutorService.class);
+        RateLimiterTokenBucket testBucket = new RateLimiterTokenBucket(new MutableClock(), testScheduler);
+
+        testBucket.acquireAsync();
+        verify(testScheduler).schedule(any(Runnable.class), eq(0L), any(TimeUnit.class));
+    }
+
+    @Test
+    void acquireAsync_notifierRunning_doesNotSchedule() {
+        ScheduledExecutorService testScheduler = mock(ScheduledExecutorService.class);
+        RateLimiterTokenBucket testBucket = new RateLimiterTokenBucket(new MutableClock(), testScheduler);
+        testBucket.acquireAsync();
+        verify(testScheduler).schedule(any(Runnable.class), eq(0L), any(TimeUnit.class));
+
+        testBucket.acquireAsync();
+        verifyNoMoreInteractions(testScheduler);
+    }
+
+    @Test
+    void acquireAsync_notifierNotRunning_schedulerThrows_completesFuture() {
+        ScheduledExecutorService testScheduler = mock(ScheduledExecutorService.class);
+        RateLimiterTokenBucket testBucket = new RateLimiterTokenBucket(new MutableClock(), testScheduler);
+        when(testScheduler.schedule(any(Runnable.class), eq(0L), any(TimeUnit.class)))
+            .thenThrow(new RejectedExecutionException("rejected"));
+
+        assertThatThrownBy(testBucket.acquireAsync()::join).satisfies(t -> {
+            Throwable cause = t.getCause();
+            assertThat(cause).isExactlyInstanceOf(RuntimeException.class);
+            assertThat(cause).hasMessage("Unable to initiate token acquire");
+            assertThat(cause.getCause()).hasMessage("rejected");
+        });
+
+        assertThat(testBucket.waiting()).isEmpty();
+    }
+
+    @Test
+    void acquireAsync_notifierRunning_schedulerThrows_completesFuture() {
+        ScheduledExecutorService testScheduler = mock(ScheduledExecutorService.class);
+        RateLimiterTokenBucket testBucket = new RateLimiterTokenBucket(new MutableClock(), testScheduler);
+
+        // Enable the bucket
+        testBucket.updateRateAfterThrottling();
+
+        when(testScheduler.schedule(any(Runnable.class), anyLong(), any()))
+            .thenAnswer(i -> {
+                Runnable command = i.getArgument(0);
+                Long delay = i.getArgument(1);
+                if (delay == 0L) {
+                    command.run();
+                    return null;
+                }
+                throw new RejectedExecutionException("rejected");
+            });
+
+        assertThatThrownBy(testBucket.acquireAsync()::join).satisfies(t -> {
+            Throwable cause = t.getCause();
+            assertThat(cause).isExactlyInstanceOf(RuntimeException.class);
+            assertThat(cause).hasMessage("Unable to initiate token acquire");
+            assertThat(cause.getCause()).hasMessage("rejected");
+        });
+
+        assertThat(testBucket.waiting()).isEmpty();
+    }
+
+    @Test
+    void sendingRateEndToEndTest() {
+        for (TestCase sendingRateTestCase : sendingRateTestCases()) {
+            assertSendingRateTestCase(sendingRateTestCase);
+        }
+    }
+
+    void assertSendingRateTestCase(TestCase testCase) {
         clock.setCurrent(testCase.givenTimestamp);
         RateLimiterUpdateResponse res;
-        tokenBucket.tryAcquire();
+
         if (testCase.throttleResponse) {
             res = tokenBucket.updateRateAfterThrottling();
         } else {
             res = tokenBucket.updateRateAfterSuccess();
         }
         double measuredTxRate = res.measuredTxRate();
-        assertThat(measuredTxRate).isCloseTo(testCase.expectMeasuredTxRate, within(EPSILON));
+        assertThat(measuredTxRate)
+            .as("%s: Measured TX rate", testCase)
+            .isCloseTo(testCase.expectMeasuredTxRate, within(EPSILON));
         double fillRate = res.fillRate();
-        assertThat(fillRate).isCloseTo(testCase.expectFillRate, within(EPSILON));
+        assertThat(fillRate)
+            .as("%s: Fill rate", testCase)
+            .isCloseTo(testCase.expectFillRate, within(EPSILON));
     }
 
-
-    static Collection<TestCase> parameters() {
+    static Collection<TestCase> sendingRateTestCases() {
+        // Note: Test cases are not independent. Each case depends on the state of the bucket being correctly updated from the
+        // previous test.
         return Arrays.asList(
             new TestCase()
                 .givenSuccessResponse()
@@ -174,6 +262,15 @@ class RateLimiterTokenBucketTest {
             return this;
         }
 
+        @Override
+        public String toString() {
+            return "TestCase{" +
+                   "throttleResponse=" + throttleResponse +
+                   ", givenTimestamp=" + givenTimestamp +
+                   ", expectMeasuredTxRate=" + expectMeasuredTxRate +
+                   ", expectFillRate=" + expectFillRate +
+                   '}';
+        }
     }
 
     static class MutableClock implements RateLimiterClock {

aws/aws-sdk-java-v2

core/retries/src/main/java/software/amazon/awssdk/retries/internal/ratelimiter/RateLimiterTokenBucketStore.java
@@ -15,8 +15,10 @@
 
 package software.amazon.awssdk.retries.internal.ratelimiter;
 
+import java.util.concurrent.ScheduledExecutorService;
 import software.amazon.awssdk.annotations.SdkInternalApi;
 import software.amazon.awssdk.annotations.ToBuilderIgnoreField;
+import software.amazon.awssdk.utils.SdkAutoCloseable;
 import software.amazon.awssdk.utils.Validate;
 import software.amazon.awssdk.utils.builder.CopyableBuilder;
 import software.amazon.awssdk.utils.builder.ToCopyableBuilder;
@@ -27,17 +29,25 @@ import software.amazon.awssdk.utils.cache.lru.LruCache;
  */
 @SdkInternalApi
 public final class RateLimiterTokenBucketStore
-    implements ToCopyableBuilder<RateLimiterTokenBucketStore.Builder, RateLimiterTokenBucketStore> {
+    implements ToCopyableBuilder<RateLimiterTokenBucketStore.Builder, RateLimiterTokenBucketStore>, SdkAutoCloseable {
     private static final int MAX_ENTRIES = 128;
     private static final RateLimiterClock DEFAULT_CLOCK = new SystemClock();
     private final LruCache<String, RateLimiterTokenBucket> scopeToTokenBucket;
     private final RateLimiterClock clock;
+    private final ScheduledExecutorService scheduler;
 
     private RateLimiterTokenBucketStore(Builder builder) {
         this.clock = Validate.paramNotNull(builder.clock, "clock");
-        this.scopeToTokenBucket = LruCache.<String, RateLimiterTokenBucket>builder(x -> new RateLimiterTokenBucket(clock))
+        this.scheduler = Validate.paramNotNull(builder.scheduler, "scheduler");
+        this.scopeToTokenBucket = LruCache.<String, RateLimiterTokenBucket>builder(
+            x -> new RateLimiterTokenBucket(clock, scheduler))
                                           .maxSize(MAX_ENTRIES)
                                           .build();
+    }
+
+    @Override
+    public void close() {
+        scheduler.shutdownNow();
     }
 
     public RateLimiterTokenBucket tokenBucketForScope(String scope) {
@@ -56,6 +66,7 @@ public final class RateLimiterTokenBucketStore
 
     public static class Builder implements CopyableBuilder<Builder, RateLimiterTokenBucketStore> {
         private RateLimiterClock clock;
+        private ScheduledExecutorService scheduler;
 
         Builder() {
             this.clock = DEFAULT_CLOCK;
@@ -63,10 +74,16 @@ public final class RateLimiterTokenBucketStore
 
         Builder(RateLimiterTokenBucketStore store) {
             this.clock = store.clock;
+            this.scheduler = store.scheduler;
         }
 
         public Builder clock(RateLimiterClock clock) {
             this.clock = clock;
+            return this;
+        }
+
+        public Builder executor(ScheduledExecutorService scheduler) {
+            this.scheduler = scheduler;
             return this;
         }
 

core/retries/src/test/java/software/amazon/awssdk/retries/internal/ratelimiter/RateLimiterTokenBucketStoreTest.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.retries.internal.ratelimiter;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import org.junit.jupiter.api.Test;
+
+public class RateLimiterTokenBucketStoreTest {
+    @Test
+    void close_closesScheduler() {
+        ScheduledExecutorService scheduler = mock(ScheduledExecutorService.class);
+        RateLimiterTokenBucketStore store = RateLimiterTokenBucketStore.builder()
+                                                                       .clock(new SystemClock())
+                                                                       .executor(scheduler)
+                                                                       .build();
+
+        store.close();
+
+        verify(scheduler).shutdownNow();
+    }
+}

core/retries/src/test/java/software/amazon/awssdk/retries/internal/ratelimiter/RateLimiterTokenBucketTest.java
@@ -16,44 +16,161 @@
 package software.amazon.awssdk.retries.internal.ratelimiter;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.within;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.Collection;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class RateLimiterTokenBucketTest {
-    private static MutableClock clock = null;
-    private static RateLimiterTokenBucket tokenBucket = null;
     private static final double EPSILON = 0.0001;
+    private MutableClock clock = null;
+    private ScheduledExecutorService scheduler = null;
+    private RateLimiterTokenBucket tokenBucket = null;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         clock = new MutableClock();
-        tokenBucket = new RateLimiterTokenBucket(clock);
+        scheduler = mock(ScheduledExecutorService.class);
+        tokenBucket = new RateLimiterTokenBucket(clock, scheduler);
     }
 
-    @ParameterizedTest
-    @MethodSource("parameters")
-    void testCase(TestCase testCase) {
+    @Test
+    void acquireAsync_bucketClosed_futureCompletedExceptionally() {
+        tokenBucket.close();
+        CompletableFuture<Void> f = tokenBucket.acquireAsync();
+        assertThatThrownBy(f::join).satisfies(t -> {
+            Throwable cause = t.getCause();
+            assertThat(cause).isExactlyInstanceOf(IllegalStateException.class);
+            assertThat(cause).hasMessage("Rate limiter bucket is closed");
+        });
+    }
+
+    @Test
+    void acquireAsync_notEnabled_doesNotScheduleTask() {
+        CompletableFuture<Void> f = tokenBucket.acquireAsync();
+        assertThat(f).isCompleted();
+        verifyNoInteractions(scheduler);
+    }
+
+    @Test
+    void acquireAsync_enabled_schedulesTask() {
+        tokenBucket.updateRateAfterThrottling();
+
+        tokenBucket.acquireAsync();
+        verify(scheduler).schedule(any(Runnable.class), anyLong(), any(TimeUnit.class));
+    }
+
+
+    @Test
+    void acquireAsync_scheduleFails_completesFutureExceptionally() {
+        tokenBucket.updateRateAfterThrottling();
+
+        doThrow(new RejectedExecutionException("no")).when(scheduler).schedule(any(Runnable.class),
+                                                                               anyLong(),
+                                                                               any(TimeUnit.class));
+
+        CompletableFuture<Void> f = tokenBucket.acquireAsync();
+        assertThatThrownBy(f::join).satisfies(t -> {
+            Throwable cause = t.getCause();
+            assertThat(cause).hasMessage("Unable to initiate token acquire");
+        });
+    }
+
+    @Test
+    void close_completesAllPendingFutures() {
+        // enable throttling so futures actually get queued instead of being completed immediately
+        tokenBucket.updateRateAfterThrottling();
+
+        List<CompletableFuture<Void>> futures = IntStream.range(0, 10)
+                                                         .mapToObj(i -> tokenBucket.acquireAsync())
+                                                         .collect(Collectors.toList());
+
+        tokenBucket.close();
+
+        assertThat(futures).allSatisfy(f -> {
+            assertThatThrownBy(f::join).satisfies(t -> {
+                Throwable cause = t.getCause();
+                assertThat(cause).isExactlyInstanceOf(IllegalStateException.class);
+                assertThat(cause).hasMessage("Rate limiter bucket is closed");
+            });
+        });
+
+        assertThat(tokenBucket.waiting()).isEmpty();
+    }
+
+    @Test
+    void close_doesShutDownExecutor() {
+        tokenBucket.close();
+        verifyNoInteractions(scheduler);
+    }
+    
+    @Test
+    void doNotify_scheduleRejected_failsFuture() {
+        tokenBucket.updateRateAfterThrottling();
+
+        // Empty bucket at default rate of 0.5 tokens per second should be 2seconds
+        when(scheduler.schedule(any(Runnable.class), eq(2000L), eq(TimeUnit.MILLISECONDS)))
+            .thenThrow(new RejectedExecutionException("no"));
+
+        // 0L is the initial schedule from acquireAsync, capture the doNotify schedule and execute that.
+        when(scheduler.schedule(any(Runnable.class), eq(0L), any(TimeUnit.class))).thenAnswer(i -> {
+           Runnable r = i.getArgument(0);
+           r.run();
+           return null;
+        });
+
+        CompletableFuture<Void> future = tokenBucket.acquireAsync();
+        assertThatThrownBy(future::join).hasRootCauseInstanceOf(RejectedExecutionException.class);
+        assertThat(tokenBucket.waiting()).isEmpty();
+    }
+
+    @Test
+    void sendingRateEndToEndTest() {
+        for (TestCase sendingRateTestCase : sendingRateTestCases()) {
+            assertSendingRateTestCase(sendingRateTestCase);
+        }
+    }
+
+    void assertSendingRateTestCase(TestCase testCase) {
         clock.setCurrent(testCase.givenTimestamp);
         RateLimiterUpdateResponse res;
-        tokenBucket.tryAcquire();
+
         if (testCase.throttleResponse) {
             res = tokenBucket.updateRateAfterThrottling();
         } else {
             res = tokenBucket.updateRateAfterSuccess();
         }
         double measuredTxRate = res.measuredTxRate();
-        assertThat(measuredTxRate).isCloseTo(testCase.expectMeasuredTxRate, within(EPSILON));
+        assertThat(measuredTxRate)
+            .as("%s: Measured TX rate", testCase)
+            .isCloseTo(testCase.expectMeasuredTxRate, within(EPSILON));
         double fillRate = res.fillRate();
-        assertThat(fillRate).isCloseTo(testCase.expectFillRate, within(EPSILON));
+        assertThat(fillRate)
+            .as("%s: Fill rate", testCase)
+            .isCloseTo(testCase.expectFillRate, within(EPSILON));
     }
 
-
-    static Collection<TestCase> parameters() {
+    static Collection<TestCase> sendingRateTestCases() {
+        // Note: Test cases are not independent. Each case depends on the state of the bucket being correctly updated from the
+        // previous test.
         return Arrays.asList(
             new TestCase()
                 .givenSuccessResponse()
@@ -174,6 +291,15 @@ class RateLimiterTokenBucketTest {
             return this;
         }
 
+        @Override
+        public String toString() {
+            return "TestCase{" +
+                   "throttleResponse=" + throttleResponse +
+                   ", givenTimestamp=" + givenTimestamp +
+                   ", expectMeasuredTxRate=" + expectMeasuredTxRate +
+                   ", expectFillRate=" + expectFillRate +
+                   '}';
+        }
     }
 
     static class MutableClock implements RateLimiterClock {