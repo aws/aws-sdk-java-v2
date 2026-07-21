dacad55e0ce7b34b6145ccab7d573ce7ebd67fcf
Saranya Somepalli <somepal@amazon.com>
2026-07-21 09:13:34 -0700

    Remove ServiceMetadata dependency from HelpfulUnknownHostExceptionInterceptor (#7168)
    * Remove ServiceMetadata dependency from HelpfulUnknownHostExceptionInterceptor

--- core/aws-core/src/test/java/software/amazon/awssdk/awscore/interceptor/HelpfulUnknownHostExceptionInterceptorTest.java
+++ core/aws-core/src/test/java/software/amazon/awssdk/awscore/interceptor/HelpfulUnknownHostExceptionInterceptorTest.java
@@ -6,14 +6,12 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import software.amazon.awssdk.awscore.AwsExecutionAttribute;
 import software.amazon.awssdk.core.SdkRequest;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.interceptor.ExecutionAttributes;
 import software.amazon.awssdk.core.interceptor.ExecutionInterceptor;
 import software.amazon.awssdk.core.interceptor.InterceptorContext;
 import software.amazon.awssdk.core.internal.interceptor.DefaultFailedExecutionContext;
-import software.amazon.awssdk.regions.Region;
 
 public class HelpfulUnknownHostExceptionInterceptorTest {
     private static final ExecutionInterceptor INTERCEPTOR = new HelpfulUnknownHostExceptionInterceptor();
@@ -31,76 +29,26 @@ public class HelpfulUnknownHostExceptionInterceptorTest {
         exception = new IllegalArgumentException(exception);
         exception = new UnsupportedOperationException(exception);
 
-        assertThat(modifyException(exception, Region.AWS_GLOBAL)).isInstanceOf(SdkClientException.class);
+        assertThat(modifyException(exception)).isInstanceOf(SdkClientException.class);
     }
 
     @Test
-    public void modifyException_returnsGenericHelp_forGlobalRegions() {
+    public void modifyException_returnsNetworkHelp_forUnknownHostException() {
         UnknownHostException exception = new UnknownHostException();
-        assertThat(modifyException(exception, Region.AWS_GLOBAL))
+        assertThat(modifyException(exception))
             .isInstanceOf(SdkClientException.class)
             .hasMessageContaining("network");
     }
 
     @Test
-    public void modifyException_returnsGenericHelp_forUnknownServices() {
-        UnknownHostException exception = new UnknownHostException();
-        assertThat(modifyException(exception, Region.US_EAST_1, "millems-hotdog-stand"))
-            .isInstanceOf(SdkClientException.class)
-            .satisfies(t -> doesNotHaveMessageContaining(t, "global"))
-            .hasMessageContaining("network");
-    }
-
-    @Test
-    public void modifyException_returnsGenericHelp_forUnknownServicesInUnknownRegions() {
-        UnknownHostException exception = new UnknownHostException();
-        assertThat(modifyException(exception, Region.of("cn-north-99"), "millems-hotdog-stand"))
-            .isInstanceOf(SdkClientException.class)
-            .satisfies(t -> doesNotHaveMessageContaining(t, "global"))
-            .hasMessageContaining("network");
-    }
-
-    @Test
-    public void modifyException_returnsGenericHelp_forServicesRegionalizedInAllPartitions() {
-        UnknownHostException exception = new UnknownHostException();
-        assertThat(modifyException(exception, Region.US_EAST_1, "dynamodb"))
-            .isInstanceOf(SdkClientException.class)
-            .satisfies(t -> doesNotHaveMessageContaining(t, "global"))
-            .hasMessageContaining("network");
-    }
-
-    @Test
-    public void modifyException_returnsGenericGlobalRegionHelp_forServicesGlobalInSomePartitionOtherThanTheClientPartition() {
-        UnknownHostException exception = new UnknownHostException();
-        assertThat(modifyException(exception, Region.of("cn-north-99"), "iam"))
-            .isInstanceOf(SdkClientException.class)
-            .satisfies(t -> doesNotHaveMessageContaining(t, "network"))
-            .hasMessageContaining("aws-global")
-            .hasMessageContaining("aws-cn-global");
-    }
-
-    @Test
-    public void modifyException_returnsSpecificGlobalRegionHelp_forServicesGlobalInTheClientRegionPartition() {
-        UnknownHostException exception = new UnknownHostException();
-        assertThat(modifyException(exception, Region.of("cn-north-1"), "iam"))
-            .isInstanceOf(SdkClientException.class)
-            .satisfies(t -> doesNotHaveMessageContaining(t, "aws-global"))
-            .hasMessageContaining("aws-cn-global");
-    }
-
-    private void doesNotHaveMessageContaining(Throwable throwable, String value) {
-        assertThat(throwable.getMessage()).doesNotContain(value);
+    public void modifyException_preservesOriginalExceptionAsCause() {
+        UnknownHostException exception = new UnknownHostException("iam.us-east-1.amazonaws.com");
+        Throwable result = modifyException(exception);
+        assertThat(result).isInstanceOf(SdkClientException.class);
+        assertThat(result.getCause()).isSameAs(exception);
     }
 
     private Throwable modifyException(Throwable throwable) {
-        return modifyException(throwable, null);
-    }
-
-    private Throwable modifyException(Throwable throwable, Region clientRegion) {
-        return modifyException(throwable, clientRegion, null);
-    }
-
-    private Throwable modifyException(Throwable throwable, Region clientRegion, String serviceEndpointPrefix) {
         SdkRequest sdkRequest = Mockito.mock(SdkRequest.class);
 
         DefaultFailedExecutionContext context =
@@ -109,10 +57,6 @@ public class HelpfulUnknownHostExceptionInterceptorTest {
                                          .exception(throwable)
                                          .build();
 
-        ExecutionAttributes executionAttributes =
-            new ExecutionAttributes().putAttribute(AwsExecutionAttribute.AWS_REGION, clientRegion)
-                                     .putAttribute(AwsExecutionAttribute.ENDPOINT_PREFIX, serviceEndpointPrefix);
-
-        return INTERCEPTOR.modifyException(context, executionAttributes);
+        return INTERCEPTOR.modifyException(context, new ExecutionAttributes());
     }
 }
