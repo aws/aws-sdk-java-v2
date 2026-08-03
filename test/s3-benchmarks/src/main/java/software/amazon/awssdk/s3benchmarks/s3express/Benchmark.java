a11b11457328dcbfd757efb48d1ba462bbf0fb7a
David Ho <70000000+davidh44@users.noreply.github.com>
2026-08-03 15:19:44 -0700

    Refactor S3 Benchmarks (#7210)
    * Refactor S3 benchmarks
    
    * Remove dead code
    
    * Use Jackson and add unit tests
    
    * Refactor test

--- test/s3-benchmarks/src/main/java/software/amazon/awssdk/s3benchmarks/s3express/Benchmark.java
+++ test/s3-benchmarks/src/main/java/software/amazon/awssdk/s3benchmarks/s3express/Benchmark.java
@@ -15,42 +15,7 @@
 
 package software.amazon.awssdk.s3benchmarks.s3express;
 
-import java.util.function.Supplier;
-
-/**
- * Factory to create the benchmark
- */
 @FunctionalInterface
 public interface Benchmark {
-
-    /**
-     * The benchmark method to run
-     */
     void run();
-
-    default <T> TimedResult<T> runWithTime(Supplier<T> toRun) {
-        long start = System.currentTimeMillis();
-        T result = toRun.get();
-        long end = System.currentTimeMillis();
-        return new TimedResult<>(result, (end - start) / 1000.0);
-    }
-
-    final class TimedResult<T> {
-        private final Double latency;
-        private final T result;
-
-        public TimedResult(T result, Double latency) {
-            this.result = result;
-            this.latency = latency;
-        }
-
-        public Double latency() {
-            return latency;
-        }
-
-        public T result() {
-            return result;
-        }
-
-    }
 }
