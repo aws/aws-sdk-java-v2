452ca7fde971376f4f474bced33b4fcbb443e2ce
RanVaknin <50976344+RanVaknin@users.noreply.github.com>
2026-08-17 13:18:52 -0700

    Improve PresignedRequest throughput by avoiding URI construction

---
+++ core/aws-core/src/test/java/software/amazon/awssdk/awscore/presigner/PresignedRequestTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.awscore.presigner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import java.time.Instant;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
+import software.amazon.awssdk.http.SdkHttpRequest;
+
+class PresignedRequestTest {
+    @Test
+    void standardPortIsOmitted() throws Exception {
+        SdkHttpRequest httpRequest = requestBuilder()
+            .protocol("https")
+            .host("example.com")
+            .port(443)
+            .encodedPath("/resource")
+            .build();
+
+        assertEquivalentToUriConversion(httpRequest);
+        assertThat(presignedRequest(httpRequest).url().getPort()).isEqualTo(-1);
+        assertThat(presignedRequest(httpRequest).url()).hasToString("https://example.com/resource");
+    }
+
+    @Test
+    void customPortIsPreserved() throws Exception {
+        SdkHttpRequest httpRequest = requestBuilder()
+            .protocol("https")
+            .host("example.com")
+            .port(8443)
+            .encodedPath("/resource")
+            .build();
+
+        assertEquivalentToUriConversion(httpRequest);
+        assertThat(presignedRequest(httpRequest).url().getPort()).isEqualTo(8443);
+        assertThat(presignedRequest(httpRequest).url()).hasToString("https://example.com:8443/resource");
+    }
+
+    @Test
+    void encodedPathAndQueryValuesArePreserved() throws Exception {
+        SdkHttpRequest httpRequest = requestBuilder()
+            .protocol("https")
+            .host("example.com")
+            .encodedPath("/a%20path/%2Fvalue")
+            .putRawQueryParameter("key with space", "value/with?characters")
+            .build();
+
+        assertEquivalentToUriConversion(httpRequest);
+        assertThat(presignedRequest(httpRequest).url())
+            .hasToString("https://example.com/a%20path/%2Fvalue?key%20with%20space=value%2Fwith%3Fcharacters");
+    }
+
+    @Test
+    void ipv6HostIsPreserved() throws Exception {
+        SdkHttpRequest httpRequest = requestBuilder()
+            .uri(URI.create("https://[2001:db8::1]:8443/resource"))
+            .build();
+
+        assertEquivalentToUriConversion(httpRequest);
+        assertThat(presignedRequest(httpRequest).url())
+            .hasToString("https://[2001:db8::1]:8443/resource");
+    }
+
+    private static void assertEquivalentToUriConversion(SdkHttpRequest httpRequest) throws Exception {
+        assertThat(presignedRequest(httpRequest).url()).isEqualTo(httpRequest.getUri().toURL());
+    }
+
+    private static SdkHttpFullRequest.Builder requestBuilder() {
+        return SdkHttpFullRequest.builder().method(SdkHttpMethod.GET);
+    }
+
+    private static TestPresignedRequest presignedRequest(SdkHttpRequest httpRequest) {
+        return new TestBuilder()
+            .expiration(Instant.EPOCH)
+            .isBrowserExecutable(true)
+            .signedHeaders(Collections.singletonMap("host", Collections.singletonList(httpRequest.host())))
+            .httpRequest(httpRequest)
+            .build();
+    }
+
+    private static final class TestPresignedRequest extends PresignedRequest {
+        private TestPresignedRequest(TestBuilder builder) {
+            super(builder);
+        }
+    }
+
+    private static final class TestBuilder extends PresignedRequest.DefaultBuilder<TestBuilder> {
+        @Override
+        public TestPresignedRequest build() {
+            return new TestPresignedRequest(this);
+        }
+    }
+}
---
+++ test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/presigner/S3PresignerBenchmark.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.benchmark.presigner;
+
+import java.net.URI;
+import java.net.URL;
+import java.time.Duration;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+@State(Scope.Thread)
+public class S3PresignerBenchmark {
+    private static final String ENDPOINT = "s3.us-east-1.amazonaws.com";
+    private static final String BUCKET = "java-sdk-presigner-benchmark";
+    private static final String KEY = "reports/2026/representative object+name.txt";
+
+    private S3Presigner presigner;
+    private GetObjectPresignRequest request;
+
+    @Setup(Level.Trial)
+    public void setup() {
+        presigner = S3Presigner.builder()
+                               .region(Region.US_EAST_1)
+                               .credentialsProvider(StaticCredentialsProvider.create(
+                                     AwsBasicCredentials.create("access-key", "secret-key")))
+                               .endpointOverride(URI.create("https://" + ENDPOINT))
+                               .serviceConfiguration(S3Configuration.builder()
+                                                                    .pathStyleAccessEnabled(true)
+                                                                    .build())
+                               .build();
+
+        GetObjectRequest getObjectRequest =
+            GetObjectRequest.builder()
+                            .bucket(BUCKET)
+                            .key(KEY)
+                            .responseContentDisposition("attachment; filename=report.txt")
+                            .versionId("benchmark-version")
+                            .build();
+
+        request = GetObjectPresignRequest.builder()
+                                         .signatureDuration(Duration.ofMinutes(15))
+                                         .getObjectRequest(getObjectRequest)
+                                         .build();
+
+        validate(presigner.presignGetObject(request));
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDown() {
+        presigner.close();
+    }
+
+    @Benchmark
+    public PresignedGetObjectRequest presignGetObject() {
+        return presigner.presignGetObject(request);
+    }
+
+    private static void validate(PresignedGetObjectRequest result) {
+        URL url = result.url();
+        if (!"https".equals(url.getProtocol())
+            || !ENDPOINT.equals(url.getHost())
+            || !url.getPath().contains("/" + BUCKET + "/reports/2026/representative%20object%2Bname.txt")
+            || url.getQuery() == null
+            || !url.getQuery().contains("versionId=benchmark-version")
+            || !url.getQuery().contains("response-content-disposition=")
+            || !url.getQuery().contains("X-Amz-Signature=")) {
+            throw new IllegalStateException("S3 presigner benchmark sanity check failed: " + url);
+        }
+    }
+}
