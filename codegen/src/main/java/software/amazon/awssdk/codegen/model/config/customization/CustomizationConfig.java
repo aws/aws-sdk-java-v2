d0bb118c7af00f17cfbfbdfe56a9cca3dd0c044e
Saranya Somepalli <somepal@amazon.com>
2026-08-19 09:56:48 -0700

    Remove old interpreted endpoint rules path, merge rules2 into rules, and delete legacy runtime resources (#7269)
    * Remove old interpreted endpoint rules path, merge rules2 into rules, and delete legacy runtime resources
    
    * Minor cleanup

--- codegen/src/main/java/software/amazon/awssdk/codegen/model/config/customization/CustomizationConfig.java
+++ codegen/src/main/java/software/amazon/awssdk/codegen/model/config/customization/CustomizationConfig.java
@@ -315,11 +315,6 @@ public class CustomizationConfig {
     private boolean s3ExpressAuthSupport;
 
     /**
-     * Set to true to enable compiled endpoint rules. Currently defaults to false.
-     */
-    private boolean enableGenerateCompiledEndpointRules = false;
-
-    /**
      * Customization related to auth scheme derived from endpoints.
      */
     private EndpointAuthSchemeConfig endpointAuthSchemeConfig;
@@ -817,13 +812,7 @@ public class CustomizationConfig {
         this.useS3ExpressSessionAuth = useS3ExpressSessionAuth;
     }
 
-    public boolean isEnableGenerateCompiledEndpointRules() {
-        return enableGenerateCompiledEndpointRules;
-    }
 
-    public void setEnableGenerateCompiledEndpointRules(boolean enableGenerateCompiledEndpointRules) {
-        this.enableGenerateCompiledEndpointRules = enableGenerateCompiledEndpointRules;
-    }
 
     public Map<String, String> getSkipEndpointTests() {
         return skipEndpointTests;
