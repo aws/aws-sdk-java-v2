0a110921e4660d199807b50c8c605b5c6b825b6c
RanVaknin <50976344+RanVaknin@users.noreply.github.com>
2026-09-04 13:09:55 -0700

    Fix copyright header across the repo

--- services-custom/dynamodb-mapper/src/test/java/software/amazon/awssdk/mapper/dynamodb/ConversionToAttributeValuesTest.java
+++ services-custom/dynamodb-mapper/src/test/java/software/amazon/awssdk/mapper/dynamodb/ConversionToAttributeValuesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 package software.amazon.awssdk.mapper.dynamodb;
 
 import software.amazon.awssdk.mapper.dynamodb.internal.DynamoDBMapperModelFactory;
